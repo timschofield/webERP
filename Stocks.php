e84d92ecb4e7c2e876b4d6ba9400e01ce16de25b
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2011-03-18 05:48:23 +0000

    Trap codes with spaces
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@4513 09516a42-71f9-43be-811f-9b821cf0d246

--- Stocks.php
+++ Stocks.php
@@ -100,7 +100,7 @@ if (isset($_POST['submit'])) {
 		$Errors[$i] = 'StockID';
 		$i++;
 	}
-	if (ContainsIllegalCharacters($StockID) OR strstr($StockID,'  ')) {
+	if (ContainsIllegalCharacters($StockID) OR strpos($StockID,' ')) {
 		$InputError = 1;
 		prnMsg(_('The stock item code cannot contain any of the following characters') . " - ' & + \" \\ " . _('or a space'),'error');
 		$Errors[$i] = 'StockID';
