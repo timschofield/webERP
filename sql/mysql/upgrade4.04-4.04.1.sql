e273b539ee27e349c1a74591ef6674a98b451ec1
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2011-06-18 07:05:12 +0000

    sql script amended
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@4600 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.04-4.04.1.sql
+++ sql/mysql/upgrade4.04-4.04.1.sql
@@ -1,6 +1,3 @@
-INSERT INTO scripts (script, pagesecurity, description) VALUES ('SalesTopItemsInquiry.php', 2, 'Shows top selling items either by quantity or sales value by user selectable period range');
-INSERT INTO scripts (script, pagesecurity, description) VALUES ('SalesCategoryPeriodInquiry.php', 2, 'Shows sales by caetgory for a user selectable period range');
-INSERT INTO scripts (script, pagesecurity, description) VALUES ('SalesByTypePeriodInquiry.php', 2, 'Shows sales value by sales type by user selectable period range');
 ALTER TABLE `scripts` CHANGE `pagesecurity` `pagesecurity` INT( 11 ) NOT NULL DEFAULT '1';
 ALTER TABLE  `pctabs` ADD  `assigner` VARCHAR( 20 ) NOT NULL COMMENT  'Cash assigner for the tab' AFTER  `tablimit`;
 UPDATE pctabs SET assigner = authorizer;
