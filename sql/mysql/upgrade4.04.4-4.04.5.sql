34d155e56b2972ab1d7af3a2c1cc4d26afb2736c
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2011-06-18 22:27:44 +0000

    ricards earliest dispatch mods
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@4601 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.04.4-4.04.5.sql
+++ sql/mysql/upgrade4.04.4-4.04.5.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `custcontacts` ADD `email` VARCHAR( 55 ) NOT NULL;
+INSERT INTO config (confname, confvalue) VALUES ('WorkingDaysWeek','5');
 UPDATE config SET confvalue='4.04.5' WHERE confname='VersionNumber'; 
