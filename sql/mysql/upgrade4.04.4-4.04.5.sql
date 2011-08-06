f41baed95cad1e12c99a64ef2cf6a3e911d81546
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2011-08-06 01:53:38 +0000

    various
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@4651 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.04.4-4.04.5.sql
+++ sql/mysql/upgrade4.04.4-4.04.5.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `custcontacts` ADD `email` VARCHAR( 55 ) NOT NULL;
 INSERT INTO config (confname, confvalue) VALUES ('WorkingDaysWeek','5');
-INSERT INTO `weberpdemo`.`scripts` (`script` ,`pagesecurity` ,`description`) VALUES ('PDFQuotationPortrait.php', '2', 'Quotation printout in portrait');
+INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description`) VALUES ('PDFQuotationPortrait.php', '2', 'Quotation printout in portrait');
 UPDATE config SET confvalue='4.04.5' WHERE confname='VersionNumber'; 
