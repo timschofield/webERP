f776907f9c647f55852da1f7a872bf0c82c232fe
vvs2012 <vvs2012@09516a42-71f9-43be-811f-9b821cf0d246>
2012-02-26 14:59:18 +0000

    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@4975 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.07-4.08.sql
+++ sql/mysql/upgrade4.07-4.08.sql
@@ -4,7 +4,7 @@ INSERT INTO securitygroups (SELECT secroleid,0 FROM securityroles);
 INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description`) VALUES ('reportwriter/admin/ReportCreator.php', '15', 'Report Writer');
 INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description`) VALUES ('RecurringSalesOrdersProcess.php', '1', 'Process Recurring Sales Orders');
 
-DELETE `scripts` WHERE `script`='Z_CopyBOM.php';
+DELETE FROM `scripts` WHERE `script`='Z_CopyBOM.php';
 
 UPDATE config SET confvalue='4.08.1' WHERE confname='VersionNumber';
 
