11d8d999129dadb50b73944e856eaed97c316901
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2012-03-22 06:39:41 +0000

    fix Z_ImportStocks.php - quotes
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@5130 09516a42-71f9-43be-811f-9b821cf0d246

--- Z_ImportStocks.php
+++ Z_ImportStocks.php
@@ -288,16 +288,14 @@ if ($_FILES['userfile']['name']) { //start file processing
 		<br />
 		<a href="Z_ImportStocks.php?gettemplate=1">Get Import Template</a>
 		<br />
-		<br />
-	';
-	echo "<form ENCtype='multipart/form-data' action='Z_ImportStocks.php' method="post">";
+		<br />';
+	echo '<form ENCtype="multipart/form-data" action="Z_ImportStocks.php" method="post">';
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
-	echo "<input type='hidden' name='MAX_FILE_SIZE' value='1000000' />" .
-			_('Upload file') . ": <input name='userfile' type='file' />
-			<input type='submit' value='" . _('Send File') . "' />
-		</form>
-	";
+	echo '<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />' .
+			_('Upload file') . ': <input name="userfile" type="file" />
+			<input type="submit" value="' . _('Send File') . '" />
+		</form>';
 
 }
 
