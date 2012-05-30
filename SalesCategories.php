59c7cc2b6415381648513ac9cca1c09db7c29534
tehonu <tehonu@09516a42-71f9-43be-811f-9b821cf0d246>
2012-05-30 09:03:21 +0000

    Show only Current items. No sense to add an obsolete item to a  Sales Category (for website use only)
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@5407 09516a42-71f9-43be-811f-9b821cf0d246

--- SalesCategories.php
+++ SalesCategories.php
@@ -363,7 +363,7 @@ if($result && DB_num_rows($result)) {
 }
 
 // This query will return the stock that is available
-$sql = "SELECT stockid, description FROM stockmaster ORDER BY stockid";
+$sql = "SELECT stockid, description FROM stockmaster WHERE discontinued = 0 ORDER BY stockid";
 $result = DB_query($sql,$db);
 if($result && DB_num_rows($result)) {
 	// continue id stock id in the stockid array
