6de415c671af5ff8ed9d452e77056f7e382ceac7
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2012-07-08 10:14:15 +0000

    fix links to non-existent script
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@5483 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.08-4.09.sql
+++ sql/mysql/upgrade4.08-4.09.sql
@@ -1,4 +1,5 @@
 INSERT INTO scripts VALUES ('Z_DeleteOldPrices.php','15','Deletes all old prices');
-INSERT INTO scripts VALUES ('Z_ChangeLocationCode.php','15','Change a locations code and in all tables where the old code was used to the new code')
-UPDATE config SET confvalue='4.08.1' WHERE confname='VersionNumber';
+INSERT INTO scripts VALUES ('Z_ChangeLocationCode.php','15','Change a locations code and in all tables where the old code was used to the new code');
+DELETE FROM scripts WHERE script='Z_ChangeSupplierCode.php';
+UPDATE config SET confvalue='4.08.2' WHERE confname='VersionNumber';
 
