594151cadc6c3689867565dffb9b9a1cc524aad8
tehonu <tehonu@09516a42-71f9-43be-811f-9b821cf0d246>
2012-12-22 15:04:56 +0000

    Pak Ricard: Added script showing list of current items without a picture in webERP
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@5770 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.09-4.10.sql
+++ sql/mysql/upgrade4.09-4.10.sql
@@ -11,4 +11,5 @@ VALUES ('OrderEntryDiscountPricing', '13', 'Not a script but an authority level 
 ALTER TABLE `gltrans` ADD INDEX ( `tag` );
 INSERT INTO scripts VALUES ('CustomerPurchases.php','5','Shows the purchases a customer has made.');
 INSERT INTO scripts VALUES ('GoodsReceivedButNotInvoiced.php','15','Shows the list of Goods Received Not Yet Invoiced, both in supplier currency and home currency. Total in home curency should match the GL Account for Goods received not invoiced. Any discrepancy is due to multicurrency errors.');
+INSERT INTO scripts VALUES ('ItemsWithoutPicture.php','15','Shows the list of curent items without picture in webERP');
 UPDATE config SET confvalue='4.10.0' WHERE confname='VersionNumber';
