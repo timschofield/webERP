52f6c5f8765c7b077909bfb666ab897d3b149fa3
samudaya <samudaya@09516a42-71f9-43be-811f-9b821cf0d246>
2013-04-27 18:09:51 +0000

    Fixed the Page Navigation (Go, Previous, Next) problem. This problem occurs  there are many assets and display as several pages.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@5855 09516a42-71f9-43be-811f-9b821cf0d246

--- SelectAsset.php
+++ SelectAsset.php
@@ -116,8 +116,7 @@ echo '</td>
 		<input type="submit" name="Search" value="' . _('Search Now') . '" />
 	</div>
 	<br />
-    </div>
-	</form>';
+    </div>';
 echo '<script  type="text/javascript">defaultControl(document.forms[0].Keywords);</script>';
 
 // query for list of record(s)
@@ -218,9 +217,6 @@ if (isset($_POST['Search']) OR isset($_POST['Go']) OR isset($_POST['Next']) OR i
 /* end query for list of records */
 /* display list if there is more than one record */
 if (isset($SearchResult) AND !isset($_POST['Select'])) {
-	echo '<form action="FixedAssetItems.php" method="post">';
-    echo '<div>';
-	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 	$ListCount = DB_num_rows($SearchResult);
 	if ($ListCount > 0) {
 		// If the user hit the search button and there is more than one item to show
@@ -257,6 +253,12 @@ if (isset($SearchResult) AND !isset($_POST['Select'])) {
 
 			echo '<br /></div>';
 		}
+		echo '</form>';
+		
+		echo '<form action="FixedAssetItems.php" method="post">';
+		echo '<div>';
+		echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+	
 		echo '<table class="selection">';
 		$tableheader = '<tr>
 					<th>' . _('Asset Code') . '</th>
@@ -294,9 +296,10 @@ if (isset($SearchResult) AND !isset($_POST['Select'])) {
 		}
 		//end of while loop
 		echo '</table>';
-	} // there were records to list
-    echo '</div>
+		echo '</div>
           </form>';
+	} // there were records to list
+    
 }
 /* end display list if there is more than one record */
 include ('includes/footer.inc');
