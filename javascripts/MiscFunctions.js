ce44e4607a5422080cea3e229da3c5bb41f81cae
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2013-07-21 10:19:10 +0000

    7/21/2013 Exson: Fixed the keyChar control to avoid backspace mistake.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6120 09516a42-71f9-43be-811f-9b821cf0d246

--- javascripts/MiscFunctions.js
+++ javascripts/MiscFunctions.js
@@ -5,29 +5,14 @@ c.focus();
 function ReloadForm(fB){
 fB.click();
 }
-var kPrev = 'a';
 function rTN(event){
 	if (window.event) k=window.event.keyCode;
 	else if (event) k=event.which;
 	else return true;
 	kC=String.fromCharCode(k);
-	if ((k==null) || (k==0) || (k==8) || (k==9) || (k==13) || (k==27)){
-	       	return true;
-	}
-	else if ((("0123456789.-, ").indexOf(kC)>-1)){
-		if((".-, ").indexOf(kC)>-1){
-			if((".-, ").indexOf(kPrev)>-1){
-				return false;
-			}else{
-				kPrev = kC;
-	       	       		return true;
-			}
-		}else{
-			kPrev = kC;
-			return true;
-		}
-	}
-	else return false; 
+	if ((k==null) || (k==0) || (k==8) || (k==9) || (k==13) || (k==27)) return true;
+	else if ((("0123456789.,- ").indexOf(kC)>-1)) return true;
+	else return false;
 }
 function rTI(event){
 	if (window.event) k=window.event.keyCode;
