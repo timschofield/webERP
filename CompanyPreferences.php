239fc49742a36631be2feb4fece2859101052cb1
icedlava <icedlava@09516a42-71f9-43be-811f-9b821cf0d246>
2013-08-15 02:58:43 +0000

    Allow apostrophe in company name, update tool tip.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6219 09516a42-71f9-43be-811f-9b821cf0d246

--- CompanyPreferences.php
+++ CompanyPreferences.php
@@ -171,7 +171,7 @@ if ($InputError != 1) {
 
 echo '<tr>
 		<td>' . _('Name') . ' (' . _('to appear on reports') . '):</td>
-		<td><input '.(in_array('CoyName',$Errors) ?  'class="inputerror"' : '' ) .' tabindex="1" type="text" autofocus="autofocus" required="required" name="CoyName" value="' . stripslashes($_POST['CoyName']) . '" minlength="5" pattern="[a-zA-Z0-9_\&\-\ ]*" title="' . _('Enter the name of the business, this will appear on all reports and at the top of each screen. Only alphanumeric characters space and dashes are allowed.') . '" size="52" maxlength="50" /></td>
+		<td><input '.(in_array('CoyName',$Errors) ?  'class="inputerror"' : '' ) .' tabindex="1" type="text" autofocus="autofocus" required="required" name="CoyName" value="' . stripslashes($_POST['CoyName']) . '" minlength="5" pattern="[\w\&\'\-\s]+?" title="' . _('Enter the name of the business. This will appear on all reports and at the top of each screen. Only alphanumeric characters, space, ampersand, apostrophe and dashes are allowed') . '" size="52" maxlength="50" /></td>
 	</tr>';
 
 echo '<tr>
