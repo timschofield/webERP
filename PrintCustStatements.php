d751984fae069d3a68527fa9b61e02bb80da3e5a
icedlava <icedlava@09516a42-71f9-43be-811f-9b821cf0d246>
2013-09-06 15:48:49 +0000

    PrintCustStatements.php fix to allow selection of range with alphanumeric customer codes in length to match database definition.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6331 09516a42-71f9-43be-811f-9b821cf0d246

--- PrintCustStatements.php
+++ PrintCustStatements.php
@@ -422,9 +422,9 @@ If (isset($_POST['PrintPDF']) AND isset($_POST['FromCust']) AND $_POST['FromCust
 
         echo '<table class="selection">';
 		echo '<tr><td>' . _('Starting Customer statement to print (Customer code)'). '
-			</td><td><input type="text" maxlength="6" size="7" name="FromCust" value="1" /></td></tr>
+			</td><td><input type="text" maxlength="10" size="8" name="FromCust" value="1" /></td></tr>
 			<tr><td>' .  _('Ending Customer statement to print (Customer code)') . '</td><td>
-				<input type="text" maxlength="6" size="7" name="ToCust" value="zzzzzz" /></td></tr></table>
+				<input type="text" maxlength="10" size="8" name="ToCust" value="zzzzzz" /></td></tr></table>
 				<br /><div class="centre">
 				<input type="submit" name="PrintPDF" value="' .
                 _('Print All Statements in the Range Selected').'" />
