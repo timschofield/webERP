0a0fcfc203df93fd84e7cae10d48f3eb0a10e7ba
rchacon <rchacon@09516a42-71f9-43be-811f-9b821cf0d246>
2013-11-03 18:36:18 +0000

    Allow translate the name of the currency on CompanyPreferences.php.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6388 09516a42-71f9-43be-811f-9b821cf0d246

--- CompanyPreferences.php
+++ CompanyPreferences.php
@@ -231,6 +231,7 @@ echo '<tr>
 
 
 $result=DB_query("SELECT currabrev, currency FROM currencies",$db);
+include($PathPrefix . 'includes/CurrenciesArray.php'); // To get the currency name.
 
 echo '<tr>
 		<td>' . _('Home Currency') . ':</td>
@@ -238,9 +239,9 @@ echo '<tr>
 
 while ($myrow = DB_fetch_array($result)) {
 	if ($_POST['CurrencyDefault']==$myrow['currabrev']){
-		echo '<option selected="selected" value="'. $myrow['currabrev'] . '">' . $myrow['currency'] . '</option>';
+		echo '<option selected="selected" value="'. $myrow['currabrev'] . '">' . $CurrenciesArray[$myrow['currabrev']]['Currency'] . '</option>';
 	} else {
-		echo '<option value="' . $myrow['currabrev'] . '">' . $myrow['currency']. '</option>';
+		echo '<option value="' . $myrow['currabrev'] . '">' . $CurrenciesArray[$myrow['currabrev']]['Currency'] . '</option>';
 	}
 } //end while loop
 
