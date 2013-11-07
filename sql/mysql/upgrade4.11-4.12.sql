dad9dda26e8724927cee82e19a43b6c9728f6b10
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2013-11-07 08:15:01 +0000

    7/11/2013 Exson Revise the bin definition to NOT NULL DEFAULT '' as suggest by Tim to make it more ISO compatible.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6395 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.11-4.12.sql
+++ sql/mysql/upgrade4.11-4.12.sql
@@ -5,5 +5,6 @@ INSERT INTO config VALUES ('ShopFreightMethod','webERPCalculation');
 INSERT INTO config VALUES ('ShopPaypalCommissionAccount', '7220');
 INSERT INTO  `scripts` (`script` , `pagesecurity` , `description`) VALUES ('StockClone.php',  '11',  'Script to copy a stock item and associated properties, image, price, purchase and cost data');
 #--UPDATE config SET confvalue='4.11.0' WHERE confname='VersionNumber';
-ALTER table locstock change bin bin varchar(10) NULL; 
+ALTER table locstock change bin bin varchar(10) NOT NULL DEFAULT ''; 
+
 
