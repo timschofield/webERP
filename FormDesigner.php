17b5d77c47392755755d019ac8a644758ccfe005
rchacon <rchacon@09516a42-71f9-43be-811f-9b821cf0d246>
2013-11-13 02:10:42 +0000

    Allow translation of the subkey name in FormDesigner.php.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6400 09516a42-71f9-43be-811f-9b821cf0d246

--- FormDesigner.php
+++ FormDesigner.php
@@ -220,18 +220,18 @@ foreach ($FormDesign as $key) {
 			foreach ($key as $subkey) {
 				if ($subkey['type']=='SimpleText') {
 					echo '<tr>';
-					echo '<td>' . $subkey['name'] . '</td>';
+					echo '<td>' . _($subkey['name']) . '</td>';
 					SimpleTextLine($subkey);
 					echo '</tr>';
 				} else if ($subkey['type']=='MultiLineText') {
 					echo '<tr>';
-					echo '<td>' . $subkey['name'] . '</td>';
+					echo '<td>' . _($subkey['name']) . '</td>';
 					MultiTextLine($subkey);
 					echo '</tr>';
 				} else {
 					echo '<tr>';
 					if ($subkey['type']=='DataText') {
-						echo '<td>' . $subkey['name'] . '</td>';
+						echo '<td>' . _($subkey['name']) . '</td>';
 						DataTextLine($subkey);
 					} elseif ($subkey['type']=='StartLine') {
 						echo '<td colspan="3">' . $subkey['name'].' = ' . '</td><td><input type="text" class="number" name="StartLine" size="4" maxlength="4" value="'.$key->y.'" /></td>';
