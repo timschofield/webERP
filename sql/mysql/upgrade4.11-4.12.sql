c900fe6286893267ec5e7072326b599476e29d17
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2014-03-15 21:38:17 +0000

    add AutoSupplierNo to config table
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6610 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.11-4.12.sql
+++ sql/mysql/upgrade4.11-4.12.sql
@@ -1,3 +1,4 @@
 INSERT INTO  `systypes` (`typeid` ,`typename` ,`typeno`) VALUES ('600',  'Auto Supplier Number',  '0');
+INSERT INTO config (confname, confvalue) VALUES ('AutoSupplierNo', '0');
 UPDATE config SET confvalue='4.12' WHERE confname='VersionNumber';
 
