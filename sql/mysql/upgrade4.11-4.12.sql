43bee3828228d4a0c9c070915816ad980e3b5fa9
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2014-03-15 20:42:43 +0000

    Fix SQL so NOT specific to weberpdemo
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6609 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.11-4.12.sql
+++ sql/mysql/upgrade4.11-4.12.sql
@@ -1,3 +1,3 @@
-INSERT INTO  `weberpdemo`.`systypes` (`typeid` ,`typename` ,`typeno`) VALUES ('600',  'Auto Supplier Number',  '0');
+INSERT INTO  `systypes` (`typeid` ,`typename` ,`typeno`) VALUES ('600',  'Auto Supplier Number',  '0');
 UPDATE config SET confvalue='4.12' WHERE confname='VersionNumber';
 
