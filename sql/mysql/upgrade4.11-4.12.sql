e373cc27691ebe43918f02073ececf06995b8cd0
daintree <daintree@09516a42-71f9-43be-811f-9b821cf0d246>
2014-04-23 05:53:07 +0000

    extended modulesallowed field to cope with utilities module
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6690 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.11-4.12.sql
+++ sql/mysql/upgrade4.11-4.12.sql
@@ -5,5 +5,6 @@ INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description` ) VALUES ('PDFWOP
 INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description` ) VALUES ('PDFFGLabel.php', '11', 'Produces FG Labels');
 INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description` ) VALUES ('PDFQALabel.php', '2', 'Produces a QA label on receipt of stock');
 ALTER TABLE `woitems` ADD `comments` LONGBLOB NULL DEFAULT NULL ;
+ALTER TABLE  `www_users` CHANGE  `modulesallowed`  `modulesallowed` VARCHAR( 25 ) NOT NULL;
 UPDATE config SET confvalue='4.12' WHERE confname='VersionNumber';
 
