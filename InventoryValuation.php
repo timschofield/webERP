677b2a9187211792c2a51cde2991cf51804aea1f
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2014-08-28 15:15:31 +0000

    28/08/2014 Exson: Fixed the csv file aligned abnormal when there are commas in fields strings.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6848 09516a42-71f9-43be-811f-9b821cf0d246

--- InventoryValuation.php
+++ InventoryValuation.php
@@ -206,7 +206,8 @@ if (isset($_POST['PrintPDF'])
 
 	$CSVListing = _('Category ID') .','. _('Category Description') .','. _('Stock ID') .','. _('Description') .','. _('Decimal Places') .','. _('Qty On Hand') .','. _('Units') .','. _('Unit Cost') .','. _('Total') . "\n";
 	while ($InventoryValn = DB_fetch_row($InventoryResult, $db)) {
-		$CSVListing .= implode(',', $InventoryValn) . "\n";
+		$CSVListing .= '"';
+		$CSVListing .= implode('","', $InventoryValn) . '"' . "\n";
 	}
 	header('Content-Encoding: UTF-8');
     header('Content-type: text/csv; charset=UTF-8');
