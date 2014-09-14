6fc26e534d8cd7555336b60409ca340b19ff61b7
tehonu <tehonu@09516a42-71f9-43be-811f-9b821cf0d246>
2014-09-14 00:39:55 +0000

    Improve X label, so it's readable with more than 12 columns
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6885 09516a42-71f9-43be-811f-9b821cf0d246

--- SalesGraph.php
+++ SalesGraph.php
@@ -281,6 +281,7 @@ if ((! isset($_POST['FromPeriod']) OR ! isset($_POST['ToPeriod']))
 	}
 	$graph->SetXTickPos('none');
 	$graph->SetXTickLabelPos('none');
+	$graph->SetXLabelAngle(90);
 	$graph->SetBackgroundColor('white');
 	$graph->SetTitleColor('blue');
 	$graph->SetFileFormat('png');
