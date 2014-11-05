2e48d1fdd117bf3dcadfc6af935d126a57540ff4
tehonu <tehonu@09516a42-71f9-43be-811f-9b821cf0d246>
2014-11-05 07:05:45 +0000

    Fixed bugs in previous commit
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6960 09516a42-71f9-43be-811f-9b821cf0d246

--- PDFPriceList.php
+++ PDFPriceList.php
@@ -147,10 +147,10 @@ If (isset($_POST['PrintPDF'])
 	}
 	$PricesResult = DB_query($SQL,'','',false,false);
 
-	if (DB_error_no($db) !=0) {
+	if (DB_error_no() !=0) {
 		$Title = _('Price List') . ' - ' . _('Problem Report....');
 		include('includes/header.inc');
-		prnMsg( _('The Price List could not be retrieved by the SQL because'). ' - ' . DB_error_msg($db), 'error');
+		prnMsg( _('The Price List could not be retrieved by the SQL because'). ' - ' . DB_error_msg(), 'error');
 		echo '<br /><a href="' .$RootPath .'/index.php">' .   _('Back to the menu'). '</a>';
 		if ($debug==1) {
 			prnMsg(_('For debugging purposes the SQL used was:') . $SQL,'error');
--- sql/mysql/upgrade4.11-4.12.sql
+++ sql/mysql/upgrade4.11-4.12.sql
@@ -1,5 +1,7 @@
 INSERT INTO `config` VALUES ('InvoiceQuantityDefault','1');
 
+ALTER TABLE `stockcategory` ADD INDEX `CategoryDescription` (`categorydescription`);
+
 UPDATE config SET confvalue='4.12' WHERE confname='VersionNumber';
 
 
