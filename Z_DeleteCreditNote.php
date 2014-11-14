ad6c0206fe2f511af43ca6f99e5d9697c09db2d7
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2014-11-14 11:36:18 +0000

    14/11/14 Exson: Fixed the foreign key constrained failure bug in Z_DeleteCreditNote.php.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6980 09516a42-71f9-43be-811f-9b821cf0d246

--- Z_DeleteCreditNote.php
+++ Z_DeleteCreditNote.php
@@ -57,8 +57,8 @@ $Result = DB_Txn_Begin(); /* commence a database transaction */
 
 /*Now delete the custallocns */
 
-$SQL = "DELETE custallocns FROM custallocns
-        WHERE transid_allocto ='" . $IDDebtorTrans . "'";
+$SQL = "DELETE FROM custallocns
+        WHERE transid_allocfrom ='" . $IDDebtorTrans . "'";
 
 $DbgMsg = _('The SQL that failed was');
 $ErrMsg = _('The custallocns record could not be deleted') . ' - ' . _('the sql server returned the following error');
