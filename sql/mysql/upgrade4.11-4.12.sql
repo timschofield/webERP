97c963fbf9b50ddb78f10fb593340b4c229891e6
tehonu <tehonu@09516a42-71f9-43be-811f-9b821cf0d246>
2014-11-18 02:23:21 +0000

    Added script UserLocations.php. Does the same thing as LocationUsers.php but the other way around. Selecting an user, can maintain the locations she is authorized to.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@6992 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.11-4.12.sql
+++ sql/mysql/upgrade4.11-4.12.sql
@@ -1,6 +1,6 @@
 INSERT INTO `config` VALUES ('InvoiceQuantityDefault','1');
 ALTER TABLE  `www_users` ADD  `dashboard` TINYINT NOT NULL DEFAULT  '0';
+
+INSERT INTO  `scripts` VALUES ('UserLocations.php',  '15',  'Location User Maintenance');
+
 UPDATE config SET confvalue='4.12' WHERE confname='VersionNumber';
-
-
-
