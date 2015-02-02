91ec0d97196ea9f50c0d561574d32ee9f04b32dd
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2015-02-02 15:48:39 +0000

    2/2/15 Exson: Remove suppliercontact table and related constrained key since it's not necessary and the duplicated foreign key made installation choked.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7114 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.11.4-4.12.sql
+++ sql/mysql/upgrade4.11.4-4.12.sql
@@ -166,24 +166,6 @@ CREATE TABLE IF NOT EXISTS `supplierdiscounts` (
   KEY `stockid` (`stockid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-
-CREATE TABLE IF NOT EXISTS `suppliercontacts` (
-  `supplierid` varchar(10) NOT NULL DEFAULT '',
-  `contact` varchar(30) NOT NULL DEFAULT '',
-  `position` varchar(30) NOT NULL DEFAULT '',
-  `tel` varchar(30) NOT NULL DEFAULT '',
-  `fax` varchar(30) NOT NULL DEFAULT '',
-  `mobile` varchar(30) NOT NULL DEFAULT '',
-  `email` varchar(55) NOT NULL DEFAULT '',
-  `ordercontact` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`supplierid`,`contact`),
-  KEY `Contact` (`contact`),
-  KEY `SupplierID` (`supplierid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `suppliercontacts`
-  ADD CONSTRAINT `suppliercontacts_ibfk_1` FOREIGN KEY (`supplierid`) REFERENCES `suppliers` (`supplierid`);
-
 -- Add a description for the script:
 UPDATE `scripts` SET `description` = 'Allows you to view all bank transactions for a selected date range, and the inquiry can be filtered by matched or unmatched transactions, or all transactions can be chosen' WHERE `scripts`.`script` = 'DailyBankTransactions.php';
 
