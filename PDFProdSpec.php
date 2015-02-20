79fed66b28c965d1d2f4892ba80fe9b6a1fe69a1
agaluski <agaluski@09516a42-71f9-43be-811f-9b821cf0d246>
2015-02-20 17:23:17 +0000

    Added ability for blank grouping in Product Spec and Cert printing
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7161 09516a42-71f9-43be-811f-9b821cf0d246

--- PDFProdSpec.php
+++ PDFProdSpec.php
@@ -123,11 +123,12 @@ $HeaderPrinted=0;
 $line_height=$FontSize*1.25;
 $RectHeight=12;
 $SectionHeading=0;
-$CurSection='';
+$CurSection='NULL';
 $SectionTitle='';
 $SectionTrailer='';
 
 $SectionsArray=array(array('PhysicalProperty',3, _('Technical Data Sheet Properties'), _('* Data herein is typical and not to be construed as specifications.'), array(260,110,135),array(_('Physical Property'),_('Value'),_('Test Method')),array('left','center','center')),
+					 array('',3, _('Header'), _('* Trailer'), array(260,110,135), array(_('Physical Property'),_('Value'),_('Test Method')),array('left','center','center')),
 					 array('Processing',2, _('Injection Molding Processing Guidelines'), _('* Desicant type dryer required.'), array(240,265),array(_('Setting'),_('Value')),array('left','center')),
 					 array('RegulatoryCompliance',2, _('Regulatory Compliance'), '', array(240,265),array(_('Regulatory Compliance'),_('Value')),array('left','center')));
 
@@ -152,7 +153,7 @@ while ($myrow=DB_fetch_array($result)){
 
 	if ($CurSection!=$myrow['groupby']) {
 		$SectionHeading=0;
-		if ($CurSection!='' AND $PrintTrailer==1) {
+		if ($CurSection!='NULL' AND $PrintTrailer==1) {
 			$pdf->line($XPos+1, $YPos+$RectHeight,$XPos+506, $YPos+$RectHeight);
 		}
 		$PrevTrailer=$SectionTrailer;
