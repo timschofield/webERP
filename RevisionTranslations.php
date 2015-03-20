bf3b4f6e7949325e053522c059be9d7d73a83e02
Pak Ricard <pakricard@users.noreply.github.com>
2015-03-20 16:10:18 +0800

    Reverse changes for RevisionTranslations.php

--- RevisionTranslations.php
+++ RevisionTranslations.php
@@ -1,18 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-include('includes/session.inc');
-$Title = _('Revision of Description Translations');
-include('includes/header.inc');
-include('includes/SQL_CommonFunctions.inc');
-
-//update database if update pressed
-if (isset($_POST['Submit'])){
-	for ($i=1;$i<count($_POST);$i++){ //loop through the returned translations
-
-		if (isset($_POST['Revised' . $i]) AND ($_POST['Revised' . $i] == '1')){
-			$sqlUpdate="UPDATE stockdescriptiontranslations 
-=======
 /* $Id: RevisionTranslations.php 7040 2014-12-27 15:15:29Z tehonu $*/
 /* This script is to review the item description translations. */
 
@@ -35,7 +21,6 @@ if(isset($_POST['Submit'])) {
 
 		if(isset($_POST['Revised' . $i]) AND ($_POST['Revised' . $i] == '1')) {
 			$sqlUpdate="UPDATE stockdescriptiontranslations
->>>>>>> SVN_development
 						SET needsrevision = '0',
 							descriptiontranslation = '". $_POST['DescriptionTranslation' .$i] ."',
 							longdescriptiontranslation = '". $_POST['LongDescriptionTranslation' .$i] ."'
@@ -53,17 +38,6 @@ echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />'
 
 echo '<table class="selection">
 		<tr>
-<<<<<<< HEAD
-			<th colspan="7">' . _('Translations to revise (max 50)') .'</th>
-		</tr>';
-
-$sql = "SELECT stockdescriptiontranslations.stockid,
-			stockmaster.description,
-			stockmaster.longdescription,
-			stockdescriptiontranslations.language_id,
-			stockdescriptiontranslations.descriptiontranslation,
-			stockdescriptiontranslations.longdescriptiontranslation
-=======
 			<th colspan="7">' . _('Translations to revise') .'</th>
 		</tr>';
 
@@ -73,44 +47,27 @@ $sql = "SELECT stockdescriptiontranslations.stockid,
 				stockdescriptiontranslations.language_id,
 				stockdescriptiontranslations.descriptiontranslation,
 				stockdescriptiontranslations.longdescriptiontranslation
->>>>>>> SVN_development
 		FROM stockdescriptiontranslations, stockmaster
 		WHERE stockdescriptiontranslations.stockid = stockmaster.stockid
 			AND stockdescriptiontranslations.needsrevision = '1'
 		ORDER BY stockdescriptiontranslations.stockid,
-<<<<<<< HEAD
-				stockdescriptiontranslations.language_id
-		LIMIT 1, 50";
-=======
 				stockdescriptiontranslations.language_id";
->>>>>>> SVN_development
 
 $result = DB_query($sql);
 
 echo '<tr>
 	<th>' . _('Code') . '</th>
 	<th>' . _('Language') . '</th>
-<<<<<<< HEAD
-	<th>' . _('Description') . '</th>
-	<th>' . _('Long Description') . '</th>
-=======
 	<th>' . _('Part Description (short)') . '</th>
 	<th>' . _('Part Description (long)') . '</th>
->>>>>>> SVN_development
 	<th>' . _('Revised?') . '</th>
 </tr>';
 
 $k=0; //row colour counter
 $i=1;
-<<<<<<< HEAD
-while ($myrow=DB_fetch_array($result))	{
-
-	if ($k==1){
-=======
 while($myrow=DB_fetch_array($result)) {
 
 	if($k==1) {
->>>>>>> SVN_development
 		echo '<tr class="EvenTableRows">';
 		$k=0;
 	} else {
@@ -118,15 +75,6 @@ while($myrow=DB_fetch_array($result)) {
 		$k=1;
 	}
 
-<<<<<<< HEAD
-	echo'<td>' . $myrow['stockid'] . '</td>
-		<td>'. $_SESSION['Language']. '</td>
-		<td>' . $myrow['description'] . '</td>
-		<td>' . $myrow['longdescription'] . '</td>
-		</tr>';
-	
-	if ($k==1){
-=======
 	echo '<td>' . $myrow['stockid'] . '</td>
 		<td>' . $_SESSION['Language']. '</td>
 		<td>' . $myrow['description'] . '</td>
@@ -134,7 +82,6 @@ while($myrow=DB_fetch_array($result)) {
 		<td>&nbsp;</td></tr>';// nl2br: Inserts HTML line breaks before all newlines in a string.
 
 	if($k==1) {
->>>>>>> SVN_development
 		echo '<tr class="EvenTableRows">';
 		$k=0;
 	} else {
@@ -142,23 +89,6 @@ while($myrow=DB_fetch_array($result)) {
 		$k=1;
 	}
 
-<<<<<<< HEAD
-	echo'<td></td>
-		<td>' . $myrow['language_id'] . '</td>';
-
-	echo '<td>
-			<input type="text" class="text" name="DescriptionTranslation' . $i .'" maxlength="50" size="52" value="'. $myrow['descriptiontranslation'] .'" />
-		</td>
-		<td>
-			<textarea name="LongDescriptionTranslation' . $i .'" cols="70" rows="5">'. $myrow['longdescriptiontranslation'] .'" </textarea></td>
-		</td>';
-
-	echo '<td><input type="checkbox" name="Revised' . $i.'" value="1" />
-		</td>
-		</tr>';
-	echo'<input type="hidden" value="' . $myrow['stockid'] . '" name="StockID' . $i . '" />
-		<input type="hidden" value="' . $myrow['language_id'] . '" name="LanguageID' . $i . '" />';
-=======
 	echo '<td>&nbsp;</td>
 		<td>' . $myrow['language_id'] . '</td>';
 
@@ -171,7 +101,6 @@ while($myrow=DB_fetch_array($result)) {
 			<input name="LanguageID' . $i . '" type="hidden" value="' . $myrow['language_id'] . '" />
 		</td>
 		</tr>';
->>>>>>> SVN_development
 	$i++;
 
 } //end of looping
@@ -184,8 +113,4 @@ echo '</table>
 	</form>';
 
 include('includes/footer.inc');
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> SVN_development
