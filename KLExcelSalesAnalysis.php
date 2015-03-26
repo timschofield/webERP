2193ae48c9768710dda546d374b2c332cf89bda1
Pak Ricard <pakricard@users.noreply.github.com>
2015-03-26 21:25:38 +0800

    Excel for Sales Analysis

---
+++ KLExcelSalesAnalysis.php
@@ -0,0 +1,208 @@
+<?php
+require_once ('Classes/PHPExcel.php');
+
+include('includes/session.inc');
+include('includes/SQL_CommonFunctions.inc');
+include('includes/KLDefines.php');
+include('includes/KLBoards.php');
+include('includes/KLGeneralFunctions.php');
+include('includes/KLCountriesForRetail.php');
+include('includes/WeberpOpenCartDefines.php');
+include('includes/OpenCartGeneralFunctions.php');
+include('includes/OpenCartConnectDB.php');
+
+if (!isset($_POST['FromDate'])){
+	$_POST['FromDate'] = Date($_SESSION['DefaultDateFormat']);
+}
+
+if (!isset($_POST['ToDate'])){
+	$_POST['ToDate'] = Date($_SESSION['DefaultDateFormat']);
+}
+
+if (isset($_POST['submit'])) {
+    submit($db, $_POST['Categories'], $_POST['FromDate'], $_POST['ToDate']);
+} else {
+    display($db);
+}
+
+//####_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT####
+function submit(&$db, $ListCategories, $FromDate, $ToDate) {
+
+	//initialise no input errors
+	$InputError = 0;
+
+	//first off validate inputs sensible
+
+	if ($InputError == 0){
+		$today = date('Y-m-d');
+		$FromDate = FormatDateForSQL($_POST['FromDate']);
+		$ToDate = FormatDateForSQL($_POST['ToDate']);
+
+		$SQL = "SELECT salesorderdetails.stkcode,
+					salesorders.debtorno,
+					SUM(salesorderdetails.qtyinvoiced) AS qtyinvoiced,
+					AVG(salesorderdetails.unitprice) AS averageprice
+				FROM salesorders, salesorderdetails, stockmaster
+				WHERE salesorderdetails.orderno = salesorders.orderno
+					AND salesorderdetails.stkcode = stockmaster.stockid
+					AND stockmaster.categoryid IN ('". implode("','",$_POST['Categories'])."')
+					AND salesorders.orddate >= '" . $FromDate . "'
+					AND salesorders.orddate <= '" . $ToDate . "'
+				GROUP BY salesorderdetails.stkcode,
+						salesorders.debtorno
+				ORDER BY salesorderdetails.stkcode, 
+						salesorders.debtorno";
+
+prnMsg($SQL);
+
+		$result = DB_query($SQL);
+		if (DB_num_rows($result) != 0){
+
+			// Create new PHPExcel object
+			$objPHPExcel = new PHPExcel();
+
+			// Set document properties
+			$objPHPExcel->getProperties()->setCreator("webERP")
+										 ->setLastModifiedBy("webERP")
+										 ->setTitle("Sales Analysis")
+										 ->setSubject("Sales Analysis")
+										 ->setDescription("Sales Analysis")
+										 ->setKeywords("")
+										 ->setCategory("");
+		
+			// Add title data
+			$objPHPExcel->setActiveSheetIndex(0);
+			$objPHPExcel->getActiveSheet()->setCellValue('A1', 'CODE');
+			$objPHPExcel->getActiveSheet()->setCellValue('B1', 'DESCRIPTION');
+			$objPHPExcel->getActiveSheet()->setCellValue('C1', 'CATEGORY');
+			$objPHPExcel->getActiveSheet()->setCellValue('D1', 'DOB_CATEGORY');
+			$objPHPExcel->getActiveSheet()->setCellValue('E1', 'QOH');
+//			$objPHPExcel->getActiveSheet()->setCellValue('F1', 'TOP_ITEM');
+//			$objPHPExcel->getActiveSheet()->setCellValue('G1', 'PCS_SOLD');
+//			$objPHPExcel->getActiveSheet()->setCellValue('H1', 'VALUE_SOLD');
+			$objPHPExcel->getActiveSheet()->setCellValue('I1', 'STANDARD_COST');
+			$objPHPExcel->getActiveSheet()->setCellValue('J1', 'RETAILPRICE');
+			$objPHPExcel->getActiveSheet()->setCellValue('K1', 'PRICEFACTOR');
+ 
+			// Add data
+			$i = 2;
+			while ($myrow = DB_fetch_array($result)) {
+				$objPHPExcel->setActiveSheetIndex(0);
+				$objPHPExcel->getActiveSheet()->setCellValue('A'.$i, $myrow['stockid']);
+				$objPHPExcel->getActiveSheet()->setCellValue('B'.$i, $myrow['description']);
+				$objPHPExcel->getActiveSheet()->setCellValue('C'.$i, $myrow['categoryid']);
+				$objPHPExcel->getActiveSheet()->setCellValue('D'.$i, ConvertSQLDate($myrow['lastcategoryupdate']));
+				$objPHPExcel->getActiveSheet()->setCellValue('E'.$i, $myrow['qoh']);
+//				$objPHPExcel->getActiveSheet()->setCellValue('F'.$i, positionTopSalesItem($myrow['stockid'], 99999, $DaysTopSales, $db));
+//				$objPHPExcel->getActiveSheet()->setCellValue('G'.$i, round($myrow['totalinvoiced'],0));
+//				$objPHPExcel->getActiveSheet()->setCellValue('H'.$i, round($myrow['valuesales'],0));
+				$objPHPExcel->getActiveSheet()->setCellValue('I'.$i, round($myrow['standardcost'],0));
+				$objPHPExcel->getActiveSheet()->setCellValue('J'.$i, $myrow['retailprice']);
+				$objPHPExcel->getActiveSheet()->setCellValue('K'.$i, round(($myrow['retailprice']/$myrow['standardcost']),2));
+				
+				$i++;
+			}
+			
+			// Freeze panes
+			$objPHPExcel->getActiveSheet()->freezePane('A2');
+		
+			// Auto Size columns
+			foreach(range('A','F') as $columnID) {
+				$objPHPExcel->getActiveSheet()->getColumnDimension($columnID)
+					->setAutoSize(true);
+			}
+			
+			// Rename worksheet
+			$objPHPExcel->getActiveSheet()->setTitle('Price Analysis');
+
+			// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+			$objPHPExcel->setActiveSheetIndex(0);
+
+			// Redirect output to a client’s web browser (Excel2007)
+			header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+			$File = 'KL-SalesAnalysis-' . Date('Y-m-d'). '.xlsx';
+			header('Content-Disposition: attachment;filename="' . $File . '"');
+			header('Cache-Control: max-age=0');
+			// If you're serving to IE 9, then the following may be needed
+			header('Cache-Control: max-age=1');
+
+			// If you're serving to IE over SSL, then the following may be needed
+			header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+			header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+			header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+			header ('Pragma: public'); // HTTP/1.0
+
+			$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+			$objWriter->save('php://output');
+
+		}else{
+			$Title = _('Excel file for Sales Analysis');
+			include('includes/header.inc');
+			prnMsg('No items selected to analyse');
+			include('includes/footer.inc');
+		}
+	}
+} // End of function submit()
+
+
+function display(&$db)  //####DISPLAY_DISPLAY_DISPLAY_DISPLAY_DISPLAY_DISPLAY_#####
+{
+// Display form fields. This function is called the first time
+// the page is called.
+	$Title = _('Excel file for Sales Analysis');
+
+	include('includes/header.inc');
+
+	echo '<form action="' . htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '" method="post">
+          <div>
+			<br/>';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+
+	echo '<p class="page_title_text">
+			<img src="' . $RootPath . '/css/' . $Theme . '/images/magnifier.png" title="' . _('Excel file for Sales Analysis') . '" alt="" />' . ' ' . _('Excel file for Price Analysis') . '
+		</p>';
+
+	echo '<table class="selection">
+			<tr>
+				<td>' . _('Select Inventory Categories') . ':</td>
+				<td><select autofocus="autofocus" required="required" minlength="1" size="12" name="Categories[]"multiple="multiple">';
+	$SQL = 'SELECT categoryid, categorydescription 
+			FROM stockcategory 
+			ORDER BY categorydescription';
+	$CatResult = DB_query($SQL);
+	while ($MyRow = DB_fetch_array($CatResult)) {
+		if (isset($_POST['Categories']) AND in_array($MyRow['categoryid'], $_POST['Categories'])) {
+			echo '<option selected="selected" value="' . $MyRow['categoryid'] . '">' . $MyRow['categorydescription'] .'</option>';
+		} else {
+			echo '<option value="' . $MyRow['categoryid'] . '">' . $MyRow['categorydescription'] . '</option>';
+		}
+	}
+	echo '</select>
+			</td>
+		</tr>';
+	
+	echo '<tr>
+			<td>' . _('From') . ':</td>
+			<td><input type="text" class="date" alt="' .$_SESSION['DefaultDateFormat'] .'" name="FromDate" size="10" maxlength="10" value="' . $_POST['FromDate'] . '" /></td>
+			<td>' . _('To') . ':</td>
+			<td><input type="text" class="date" alt="' .$_SESSION['DefaultDateFormat'] .'" name="ToDate" size="10" maxlength="10" value="' . $_POST['ToDate'] . '" /></td>
+		</tr>';
+
+
+	echo '</table>
+		<table>';
+
+	echo '<tr><td>&nbsp;</td></tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" name="submit" value="' . _('Create Prices Excel File') . '" /></td>
+		</tr>
+		</table>
+		<br />';
+	echo '</div>
+         </form>';
+	include('includes/footer.inc');
+
+} // End of function display()
+
+?>
