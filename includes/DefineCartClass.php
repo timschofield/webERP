7a13c57f06423026cdbe54395748df1e84e79f0d
vvs2012 <vvs2012@09516a42-71f9-43be-811f-9b821cf0d246>
2015-03-30 21:12:13 +0000

    Get all taxes for the tax group, even if calculationorder is left at default value 0.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7243 09516a42-71f9-43be-811f-9b821cf0d246

--- includes/DefineCartClass.php
+++ includes/DefineCartClass.php
@@ -310,15 +310,16 @@ Class Cart {
 
 		$ErrMsg = _('The taxes and rates for this item could not be retrieved because');
 		$GetTaxRatesResult = DB_query($sql,$ErrMsg);
-
+		$i=1;
 		while ($myrow = DB_fetch_array($GetTaxRatesResult)){
 
-			$this->LineItems[$LineNumber]->Taxes[$myrow['taxcalculationorder']] =   new Tax($myrow['taxcalculationorder'],
+			$this->LineItems[$LineNumber]->Taxes[$i] =   new Tax($myrow['taxcalculationorder'],
 																							$myrow['taxauthid'],
 																							$myrow['description'],
 																							$myrow['taxrate'],
 																							$myrow['taxontax'],
 																							$myrow['taxglcode']);
+			$i++;
 		}
 	} //end method GetExistingTaxes
 
@@ -350,15 +351,16 @@ Class Cart {
 		if (DB_num_rows($GetTaxRatesResult)==0){
 			prnMsg(_('It appears that taxes are not defined correctly for this customer tax group') ,'error');
 		} else {
-
+			$i=1;
 			while ($myrow = DB_fetch_array($GetTaxRatesResult)){
 
-				$this->LineItems[$LineNumber]->Taxes[$myrow['calculationorder']] = new Tax($myrow['calculationorder'],
+				$this->LineItems[$LineNumber]->Taxes[$i] = new Tax($myrow['calculationorder'],
 																							$myrow['taxauthid'],
 																							$myrow['description'],
 																							$myrow['taxrate'],
 																							$myrow['taxontax'],
 																							$myrow['taxglcode']);
+				$i++;
 			} //end loop around different taxes
 		} //end if there are some taxes defined
 	} //end method GetTaxes
@@ -397,15 +399,16 @@ Class Cart {
 
 		$ErrMsg = _('The taxes and rates for this item could not be retrieved because');
 		$GetTaxRatesResult = DB_query($SQL,$ErrMsg);
-
+		$i=1;
 		while ($myrow = DB_fetch_array($GetTaxRatesResult)){
 
-			$this->FreightTaxes[$myrow['calculationorder']] = new Tax($myrow['calculationorder'],
+			$this->FreightTaxes[$i] = new Tax($myrow['calculationorder'],
 																		$myrow['taxauthid'],
 																		$myrow['description'],
 																		$myrow['taxrate'],
 																		$myrow['taxontax'],
 																		$myrow['taxglcode']);
+			$i++;
 		}
 	} //end method GetFreightTaxes()
 
