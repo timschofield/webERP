f4a3b7b13f522daf676f29b1a0aa8e7da9d886b0
Pak Ricard <pakricard@gmail.com>
2015-04-13 11:50:56 +0800

    Change packaging stock warning to 75 days

--- KLControlBoard.php
+++ KLControlBoard.php
@@ -364,11 +364,11 @@ if ($KL_SystemAdmin){
 }
 
 if ($KL_SystemAdmin){
-	InsuficientStockForShopPackaging('SHPACK', 21, 80, 30, false, $RootPath, $db);
+	InsuficientStockForShopPackaging('SHPACK', 21, 75, 30, false, $RootPath, $db);
 }
 
 if ($KL_PurchasingTeam){
-	InsuficientStockForShopPackaging('SHPACK', 21, 80, 30, true, $RootPath, $db);
+	InsuficientStockForShopPackaging('SHPACK', 21, 75, 30, true, $RootPath, $db);
 }
 
 if ($KL_SystemAdmin 
