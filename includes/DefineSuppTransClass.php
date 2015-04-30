5cf600aafdfada13013f26b99212f118038930ec
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2015-04-30 04:30:19 +0000

    30/04/15 Exson: Fixed the properties initial bug and tidy code up by fix typo.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7280 09516a42-71f9-43be-811f-9b821cf0d246

--- includes/DefineSuppTransClass.php
+++ includes/DefineSuppTransClass.php
@@ -28,7 +28,7 @@ Class SuppTrans {
 	var $OvAmount;
 	var $OvGST;
 	var $GLCodesCounter=0;
-	var $ShiptsCounter=0;
+	var $ShiptCounter=0;
 	var $ContractsCounter=0;
 	var $AssetCounter=0;
 	var $TaxGroup;
@@ -36,7 +36,6 @@ Class SuppTrans {
 	var $TaxGroupDescription;
 	var $Taxes;
 	var $Hold;
-	var $ShiptCounter;
 
 	function SuppTrans(){
 	/*Constructor function initialises a new Supplier Transaction object */
@@ -46,7 +45,6 @@ Class SuppTrans {
 		$this->Contracts = array();
 		$this->Assets = array();
 		$this->Taxes = array();
-		$ShiptCounter = 0;
 	}
 
 	function GetTaxes () {
