12a83be4a629f06de2e1e02e4306921623d64f91
Pak Ricard <pakricard@users.noreply.github.com>
2015-05-03 08:22:45 +0800

    SPG Can do return transfers in webERP

--- KLShopTransfersSPG.php
+++ KLShopTransfersSPG.php
@@ -6,6 +6,90 @@ include('includes/KLGeneralFunctions.php');
 include('includes/KLDefines.php');
 
 $LocationName = GetLocationNameFromCode($_SESSION['UserStockLocation']);
+
+/********************************************************************************************************
+ITEMS STILL IN PROCESS BY TRANSFERS ON THE LAST X DAYS
+*********************************************************************************************************/
+$SQL = "SELECT reference,
+				stockid,
+				shipqty,
+				recqty,
+				shipdate,
+				shiploc,
+				recloc
+		FROM loctransfers
+		WHERE (shiploc = '". $_SESSION['UserStockLocation'] ."'
+				OR recloc = '". $_SESSION['UserStockLocation'] ."')
+			AND shipqty != recqty
+		ORDER BY reference ASC,
+				stockid ASC";
+
+$result = DB_query($SQL);
+
+echo '<p class="page_title_text" align="center"><strong>' . 'Transfers still in transit from / to ' . $LocationName .'</strong></p>';
+echo '<table class="selection">';
+$TableHeader = '<tr>
+					<th class="ascending">' . '# Transfer' . '</th>
+					<th class="ascending">' . _('Date') . '</th>
+					<th class="ascending">' . _('Item') . '</th>
+					<th class="ascending">' . _('From') . '</th>
+					<th class="ascending">' . _('Qty Send') . '</th>
+					<th class="ascending">' . _('To') . '</th>
+					<th class="ascending">' . _('Qty Received') . '</th>
+				</tr>';
+echo $TableHeader;
+$k = 0; //row colour counter
+$i = 1;
+$Transfer = -1;
+while ($myrow = DB_fetch_array($result)) {
+	$k = StartEvenOrOddRow($k);
+	$CodeLink = '<a href="' . $RootPath . '/KLStockMovementsSPG.php?StockID=' . $myrow['stockid'] . '&Location='. $_SESSION['UserStockLocation'] . '">' . $myrow['stockid'] . '</a>';
+	if ($Transfer != $myrow['reference']){
+		// The first item of the transfer
+		$Transfer = $myrow['reference'];
+		printf('<td class="number">%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td class="number">%s</td>
+				<td>%s</td>
+				<td class="number">%s</td>
+				</tr>', 
+				locale_number_format($myrow['reference'],0),
+				ConvertSQLDate($myrow['shipdate']),
+				$CodeLink, 
+				$myrow['shiploc'], 
+				locale_number_format($myrow['shipqty'],0),
+				$myrow['recloc'], 
+				locale_number_format($myrow['recqty'],0)
+				);
+	}else{
+		// the other items of the transfer
+		printf('<td class="number">%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td class="number">%s</td>
+				<td>%s</td>
+				<td class="number">%s</td>
+				</tr>', 
+				'',
+				'',
+				$CodeLink, 
+				$myrow['shiploc'], 
+				locale_number_format($myrow['shipqty'],0),
+				$myrow['recloc'], 
+				locale_number_format($myrow['recqty'],0)
+				);
+	}
+	$i++;
+}
+echo '</table>';
+
+/********************************************************************************************************
+ITEMS PROCESSED BY TRANSFERS ON THE LAST X DAYS
+*********************************************************************************************************/
+
 $StartDate = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-TRANSFER_LIST_DAYS_FOR_SPG));
 
 $SQL = "SELECT transno,
@@ -26,7 +110,7 @@ $SQL = "SELECT transno,
 				
 $result = DB_query($SQL);
 
-echo '<p class="page_title_text" align="center"><strong>' . 'Transfers from / to ' . $LocationName  . ' of last ' . TRANSFER_LIST_DAYS_FOR_SPG .' days</strong></p>';
+echo '<p class="page_title_text" align="center"><strong>' . 'Transfers already processed from / to ' . $LocationName  . ' of last ' . TRANSFER_LIST_DAYS_FOR_SPG .' days</strong></p>';
 echo '<table class="selection">';
 $TableHeader = '<tr>
 					<th class="ascending">' . '# Transfer' . '</th>
