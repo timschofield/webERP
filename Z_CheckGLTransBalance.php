98b4fa7fb008c1b84aad241907edb434544372b4
PakRicard <pakricard@gmail.com>
2015-06-13 21:29:22 +0800

    Improvement on decimal places

--- Z_CheckGLTransBalance.php
+++ Z_CheckGLTransBalance.php
@@ -17,18 +17,19 @@ $Header = '<tr>
 echo $Header;
 
 $sql = "SELECT gltrans.type,
-		systypes.typename,
-		gltrans.typeno,
-		periodno,
-		SUM(amount) AS nettot
-	FROM gltrans,
-		systypes
-	WHERE gltrans.type = systypes.typeid
-	GROUP BY gltrans.type,
-		systypes.typename,
-		typeno,
-		periodno
-	HAVING ABS(SUM(amount))>0.01";
+			systypes.typename,
+			gltrans.typeno,
+			periodno,
+			SUM(amount) AS nettot
+		FROM gltrans,
+			systypes
+		WHERE gltrans.type = systypes.typeid
+		GROUP BY gltrans.type,
+			systypes.typename,
+			typeno,
+			periodno
+		HAVING ABS(SUM(amount))>= " . 1/pow(10,$_SESSION['CompanyRecord']['decimalplaces']) . "
+		ORDER BY gltrans.counterindex";
 
 $OutOfWackResult = DB_query($sql);
 
@@ -47,7 +48,7 @@ while ($OutOfWackRow = DB_fetch_array($OutOfWackResult)){
 	<td><a href="' . $RootPath . '/GLTransInquiry.php?TypeID=' . $OutOfWackRow['type'] . '&TransNo=' . $OutOfWackRow['typeno'] . '">' . $OutOfWackRow['typename'] . '</a></td>
 	<td class="number">' . $OutOfWackRow['typeno'] . '</td>
 	<td class="number">' . $OutOfWackRow['periodno'] . '</td>
-	<td class="number">' . locale_number_format($OutOfWackRow['nettot'],3) . '</td>
+	<td class="number">' . locale_number_format($OutOfWackRow['nettot'],$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
 	</tr>';
 
 }
