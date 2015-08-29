138d9bca67f0f53e0f62fe5f3d6e75f4aa9d5291
PakRicard <pakricard@gmail.com>
2015-08-29 10:00:50 +0800

    Set up for Shop KA

--- sql/CopyRL from shop to shop.sql
+++ sql/CopyRL from shop to shop.sql
@@ -1,6 +1,2 @@
-UPDATE locstock AS loc1, locstock AS loc2
-	SET loc1.reorderlevel = loc2.reorderlevel
-WHERE  loc1.loccode = "TOKOB"
-	AND loc2.loccode = "TOKUB"
-	AND loc1.stockid = loc2.stockid;
+
 	
