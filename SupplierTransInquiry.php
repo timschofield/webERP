0775092b6fdde6b8448fb2dd26e989cf0818608e
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2015-12-24 10:12:57 +0000

    24/12/15 Exson: Add supplier no as a option for supplier transaction inquiry in SupplierTransInquiry.php.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7423 09516a42-71f9-43be-811f-9b821cf0d246

--- SupplierTransInquiry.php
+++ SupplierTransInquiry.php
@@ -48,10 +48,16 @@ if (!isset($_POST['FromDate'])){
 if (!isset($_POST['ToDate'])){
 	$_POST['ToDate'] = Date($_SESSION['DefaultDateFormat']);
 }
+if (!isset($_POST['SupplierNo'])) {
+	$_POST['SupplierNo'] = '';
+}
 echo '<td>' . _('From') . ':</td>
 		<td><input type="text" class="date" alt="' . $_SESSION['DefaultDateFormat'] . '" name="FromDate" maxlength="10" size="11" value="' . $_POST['FromDate'] . '" /></td>
 		<td>' . _('To') . ':</td>
 		<td><input type="text" class="date" alt="' . $_SESSION['DefaultDateFormat'] . '" name="ToDate" maxlength="10" size="11" value="' . $_POST['ToDate'] . '" /></td>
+		<td>' . _('Supplier No') . ':</td>
+		<td><input type="text" name="SupplierNo" size="11" maxlength="10" value="' . $_POST['SupplierNo'] . '" />
+		</td>
 	</tr>
 	</table>
 	<br />
