5d4468edf48280927931a385234491696d05f354
waynemcdougall <waynemcdougall@09516a42-71f9-43be-811f-9b821cf0d246>
2016-09-24 22:52:11 +0000

    Variable not renamed in r3146 - fix so date shows on Stock Price History Also correct comment spelling pirce to price
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7633 09516a42-71f9-43be-811f-9b821cf0d246

--- StockStatus.php
+++ StockStatus.php
@@ -301,7 +301,7 @@ if ($DebtorNo) { /* display recent pricing history for this debtor and this stoc
                   break; /* 10 price records is enough to display */
                 }
 	    	if ($myrow['trandate'] < FormatDateForSQL(DateAdd(date($_SESSION['DefaultDateFormat']),'y', -1))) {
-	    	  break; /* stop displaying pirce history more than a year old once we have at least one  to display */
+	    	  break; /* stop displaying price history more than a year old once we have at least one  to display */
    	        }
 	    }
 	    $LastPrice = $myrow['price'];
@@ -355,7 +355,7 @@ if ($DebtorNo) { /* display recent pricing history for this debtor and this stoc
 					<td class="number">%s</td>
 					<td class="number">%s%%</td>
 					</tr>',
-					$ph[0],
+					$PreviousPrice[0],
 					locale_number_format($PreviousPrice[1],$DecimalPlaces),
 					locale_number_format($PreviousPrice[2],$_SESSION['CompanyRecord']['decimalplaces']),
 					locale_number_format($PreviousPrice[3]*100,2));
