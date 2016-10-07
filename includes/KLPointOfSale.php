a7d43bb5edf0e0fc21c5992abb5521af3f69b93a
PakRicard <pakricard@gmail.com>
2016-10-07 08:14:31 +0800

    More on POS Printing

--- includes/KLPointOfSale.php
+++ includes/KLPointOfSale.php
@@ -1,5 +1,12 @@
 <?php
 
+/*USE needed to silent print ESCPOS*/    
+use Neodynamic\SDK\Web\WebClientPrint;
+use Neodynamic\SDK\Web\Utils;
+use Neodynamic\SDK\Web\DefaultPrinter;
+use Neodynamic\SDK\Web\InstalledPrinter;
+use Neodynamic\SDK\Web\ClientPrintJob;
+	
 function webERP_in_test(){
 	return (strpos($_SERVER['PHP_SELF'],"TEST"));
 }
@@ -640,12 +647,81 @@ function AccountDebtorDiscount($ReceiptNumber,
 }
 
 function KLPrintReceipt($identifier, $OrderNo){
-	$TextToPrint = KLPrintReceiptCreateText($identifier, $OrderNo);
+/*	$TextToPrint = KLPrintReceiptCreateText($identifier, $OrderNo);
 	$ShopTextToPrint = KLPrintReceiptShopText($identifier, $OrderNo);
 
 	// for test only
 	$TextToPrint = $TextToPrint . $ShopTextToPrint;
 	echo $TextToPrint;
+*/
+    include 'includes/PrintESCPOS/WebClientPrint.php';
+
+    // Process request
+    // Generate ClientPrintJob? only if clientPrint param is in the query string
+    $urlParts = parse_url($_SERVER['REQUEST_URI']);
+    
+    if (isset($urlParts['query'])){
+        $rawQuery = $urlParts['query'];
+        parse_str($rawQuery, $qs);
+        if(isset($qs[WebClientPrint::CLIENT_PRINT_JOB])){
+
+            $useDefaultPrinter = ($qs['useDefaultPrinter'] === 'checked');
+            $printerName = urldecode($qs['printerName']);
+
+            //Create ESC/POS commands for sample receipt
+            $esc = '0x1B'; //ESC byte in hex notation
+            $newLine = '0x0A'; //LF byte in hex notation
+
+            $cmds = $esc . "@"; //Initializes the printer (ESC @)
+            $cmds .= $esc . '!' . '0x38'; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
+            $cmds .= 'BEST DEAL STORES'; //text to print
+            $cmds .= $newLine . $newLine;
+            $cmds .= $esc . '!' . '0x00'; //Character font A selected (ESC ! 0)
+            $cmds .= 'COOKIES                   5.00'; 
+            $cmds .= $newLine;
+            $cmds .= 'MILK 65 Fl oz             3.78';
+            $cmds .= $newLine . $newLine;
+            $cmds .= 'SUBTOTAL                  8.78';
+            $cmds .= $newLine;
+            $cmds .= 'TAX 5%                    0.44';
+            $cmds .= $newLine;
+            $cmds .= 'TOTAL                     9.22';
+            $cmds .= $newLine;
+            $cmds .= 'CASH TEND                10.00';
+            $cmds .= $newLine;
+            $cmds .= 'CASH DUE                  0.78';
+            $cmds .= $newLine . $newLine;
+            $cmds .= $esc . '!' . '0x18'; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
+            $cmds .= '# ITEMS SOLD 2';
+            $cmds .= $esc . '!' . '0x00'; //Character font A selected (ESC ! 0)
+            $cmds .= $newLine . $newLine;
+            $cmds .= '11/03/13  19:53:17';
+			$cmds .= $newLine;
+			$cmds .= '0x1D0x560x410x00';
+			$cmds .= $newLine;
+		
+
+            //Create a ClientPrintJob obj that will be processed at the client side by the WCPP
+            $cpj = new ClientPrintJob();
+            //set ESC/POS commands to print...
+            $cpj->printerCommands = $cmds;
+            $cpj->formatHexValues = true;
+            //set client printer
+            if ($useDefaultPrinter || $printerName === 'null'){
+                $cpj->clientPrinter = new DefaultPrinter();
+            }else{
+                $cpj->clientPrinter = new InstalledPrinter($printerName);
+            }
+
+            //Send ClientPrintJob back to the client
+            ob_start();
+            ob_clean();
+            echo $cpj->sendToClient();
+            ob_end_flush();
+            exit();
+        }
+    }
+
 }
 
 function KLPrintReceiptCreateText($identifier, $OrderNo){
