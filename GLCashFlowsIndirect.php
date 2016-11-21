8a0a375afc97ec5404a2e94514a617290183e90a
rchacon <rchacon@09516a42-71f9-43be-811f-9b821cf0d246>
2016-11-21 00:08:18 +0000

    Update translation files.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7674 09516a42-71f9-43be-811f-9b821cf0d246

--- GLCashFlowsIndirect.php
+++ GLCashFlowsIndirect.php
@@ -63,7 +63,7 @@ if($_POST['PeriodFrom'] > $_POST['PeriodTo']) {
 	prnMsg(_('The beginning of the period should be before or equal to the end of the period. Please reselect the reporting period.'), 'error');
 }
 if($_POST['PeriodTo']-$_POST['PeriodFrom']+1 > 12) {
-	// The reporting period is greater then 12 months.
+	// The reporting period is greater than 12 months.
 	unset($_POST['PeriodFrom']);
 	unset($_POST['PeriodTo']);
 	prnMsg(_('The period should be 12 months or less in duration. Please select an alternative period range.'), 'error');
