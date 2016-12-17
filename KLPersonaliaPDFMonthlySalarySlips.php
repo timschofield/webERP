bc0574d416944f668c8cc28ee30797d79680f21a
Pak Ricard <pakricard@gmail.com>
2016-12-17 11:39:33 +0800

    Pint salary slips in PDF

--- KLPersonaliaPDFMonthlySalarySlips.php
+++ KLPersonaliaPDFMonthlySalarySlips.php
@@ -79,7 +79,7 @@ function submit($Title, $Company, $LastDateOfPeriod, &$db) {
 			require_once('includes/tcpdf/tcpdf.php');
 			
 			$pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
-			$CoreFileName = $Company . '-SalarySlips-' . $LastDateOfPeriod;
+			$CoreFileName = $Company . '-MonthlySalarySlips-' . substr($LastDateOfPeriod,0,7);
 			
 			// set PDF document information
 			$pdf->SetCreator('webERP');
@@ -96,17 +96,282 @@ function submit($Title, $Company, $LastDateOfPeriod, &$db) {
 			$pdf->setPrintFooter(false);
 			$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 			$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+			
+			$FontType = 'helvetica';
+			$FontBigSize = 12;
+			$FontNormalSize = 10;
+			$FontSmallSize = 8;
+			
+			$EmployeesByBank = 0;
+			$AmountByBank = 0;
+			$EmployeesByCheck = 0;
+			$AmountByCheck = 0;
+			$EmployeesByCash = 0;
+			$AmountByCash = 0;
 
 			while ($myrow = DB_fetch_array($result)) {
-				$pdf->AddPage();
-				// https://tcpdf.org/examples/example_005/
-				// https://tcpdf.org/docs/source_docs/classTCPDF/#aa81d4b585de305c054760ec983ed3ece
-				$pdf->MultiCell(100, 0, 'Nama Panggilan:', 0, 'R', 0, 0, '', '', true);
-				$pdf->MultiCell(100, 0, $myrow['codename'], 0, 'L', 0, 1, '', '', true);
-				$pdf->MultiCell(100, 0, 'Nama Lengkap:', 0, 'R', 0, 0, '', '', true);
-				$pdf->MultiCell(100, 0, $myrow['fullname'], 0, 'L', 0, 1, '', '', true);
+				
+				$GajiPokok = $myrow['upahpokok'] +
+						$myrow['tunjanganmakan'] +
+						$myrow['tunjangantransport'] +
+						$myrow['tunjanganjabatan'] +
+						$myrow['komisitetap'];
 
+				$TotalPenerimaan = $GajiPokok +
+								$myrow['tunjanganmasakerja'] +
+								$myrow['tunjangankendaraan'] +
+								$myrow['komisiretail'] +
+								$myrow['komisisupport'] +
+								$myrow['bonuspenjualan'] +
+								$myrow['lembur'] +
+								$myrow['thr'] +
+								$myrow['penerimaanlain'];
+						
+				$TotalPotongan = $myrow['potonganjht'] +
+								$myrow['potonganaskes'] +
+								$myrow['potonganpph21'] +
+								$myrow['potonganabsen'] +
+								$myrow['potonganlain2'];
+				
+				$TotalBawaPulang = $TotalPenerimaan + $TotalPotongan + $myrow['bulatan'];
+				// set information depending on payment method
+				if ($myrow['paymentmethod'] == 'Bank'){
+					$SalaryCopiesToPrint = 2;
+					$EmployeesByBank++;
+					$AmountByBank += $TotalBawaPulang;
+				}elseif ($myrow['paymentmethod'] == 'Check'){
+					$SalaryCopiesToPrint = 2;
+					$EmployeesByCheck++;
+					$AmountByCheck += $TotalBawaPulang;
+				}elseif ($myrow['paymentmethod'] == 'Cash'){
+					$SalaryCopiesToPrint = 1;
+					$EmployeesByCash++;
+					$AmountByCash += $TotalBawaPulang;
+				}
+				$CopiesPrinted = 0;
+				
+				while ($CopiesPrinted < $SalaryCopiesToPrint){
+					$pdf->AddPage();
+					// https://tcpdf.org/examples/example_005/
+					// https://tcpdf.org/docs/source_docs/classTCPDF/#aa81d4b585de305c054760ec983ed3ece
+					
+					
+					// Company header
+					if ($Company == 'PTBB'){
+						$pdf->SetFont($FontType, 'B', $FontBigSize);
+						$pdf->MultiCell(0, 0, 'PT. Bumi Biru', 0, 'L', 0, 1, '', '', true);
+						$pdf->SetFont($FontType, '', $FontSmallSize);
+						$pdf->MultiCell(0, 0, 'Jl. Kesambi 1, Kerobokan - Bali - Indonesia', 0, 'L', 0, 1, '', '', true);
+						$pdf->MultiCell(0, 0, 'Ph. +62 81 238 167 94', 0, 'L', 0, 1, '', '', true);
+					}elseif ($Company == 'PTADU'){
+						$pdf->SetFont($FontType, 'B', $FontBigSize);
+						$pdf->MultiCell(0, 0, 'PT. Angin Utara Dingin', 0, 'L', 0, 1, '', '', true);
+						$pdf->SetFont($FontType, '', $FontSmallSize);
+						$pdf->MultiCell(0, 0, 'Jl. Kesambi 1-B, Kerobokan - Bali - Indonesia', 0, 'L', 0, 1, '', '', true);
+						$pdf->MultiCell(0, 0, 'Ph. +62', 0, 'L', 0, 1, '', '', true);
+					}				
+					
+					$pdf->SetFont($FontType, '', $FontNormalSize);
+											// employee header
+					$WidthColumn1 = 120;
+					$WidthColumn2 = 0;
+					$pdf->MultiCell($WidthColumn1, 0, 'Nama Panggilan:', 0, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, $myrow['codename'], 0, 'L', 0, 1, '', '', true);
+					$pdf->MultiCell($WidthColumn1, 0, 'Nama Lengkap:', 0, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, $myrow['fullname'], 0, 'L', 0, 1, '', '', true);
+					$pdf->MultiCell($WidthColumn1, 0, 'Posisi:', 0, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, $myrow['position'], 0, 'L', 0, 1, '', '', true);
+					$pdf->MultiCell($WidthColumn1, 0, 'Slip Gaji Periode:', 0, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, ConvertSQLDate($myrow['salaryfrom']) . ' - ' . 
+													ConvertSQLDate($myrow['salaryto']), 0, 'L', 0, 1, '', '', true);
+					
+					$pdf->ln(5);
+					
+					// gaji details
+					$WidthColumn1 = 45;
+					$WidthColumn2 = 33;
+					$WidthColumn3 = 33;
+					$WidthColumn4 =  0;
+					$pdf->MultiCell($WidthColumn1, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, 'Penerimaan', 1, 'C', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, 'Potongan', 1, 'C', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, 'Catatan', 1, 'C', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Upah:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['upahpokok']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					
+					$pdf->MultiCell($WidthColumn1, 0, 'Tunjangan makan:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['tunjanganmakan']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					
+					$pdf->MultiCell($WidthColumn1, 0, 'Tunjangan transport:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['tunjangantransport']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					
+					$pdf->MultiCell($WidthColumn1, 0, 'Tunjangan jabatan:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['tunjanganjabatan']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Komisi tetap:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['komisitetap']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->SetFont($FontType, 'B', $FontNormalSize);
+					$pdf->MultiCell($WidthColumn1, 0, 'Gaji pokok:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, 'Rp. ' . locale_number_format($GajiPokok), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					$pdf->SetFont($FontType, '', $FontNormalSize);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Tunjangan pengalaman:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['tunjanganmasakerja']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Tunjangan operasional:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['tunjangankendaraan']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Komisi Senior:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['komisiretail']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Komisi Junior/Support:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['komisisupport']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Bonus:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['bonuspenjualan']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Lembur:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['lembur']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'THR:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['thr']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Penerimaan lain lain:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['penerimaanlain']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, $myrow['penerimaanlainnotes'], 1, 'L', 0, 1, '', '', true);
+
+					$pdf->SetFont($FontType, 'B', $FontNormalSize);
+					$pdf->MultiCell($WidthColumn1, 0, 'Total Penerimaan:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, 'Rp. ' . locale_number_format($TotalPenerimaan), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					$pdf->SetFont($FontType, '', $FontNormalSize);
+					
+					$pdf->MultiCell($WidthColumn1, 0, 'Potongan JHT:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, locale_number_format($myrow['potonganjht']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Potongan ASKES:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, locale_number_format($myrow['potonganaskes']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Potongan PPh21:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, locale_number_format($myrow['potonganpph21']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Potongan Absen:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, locale_number_format($myrow['potonganabsen']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Potongan lain lain:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, locale_number_format($myrow['potonganlain2']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, $myrow['potonganlain2notes'], 1, 'L', 0, 1, '', '', true);
+
+					$pdf->SetFont($FontType, 'B', $FontNormalSize);
+					$pdf->MultiCell($WidthColumn1, 0, 'Total Potongan:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, 'Rp. ' . locale_number_format($TotalPotongan), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					$pdf->SetFont($FontType, '', $FontNormalSize);
+
+					$pdf->MultiCell($WidthColumn1, 0, 'Bulatan:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, locale_number_format($myrow['bulatan']), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+
+					$pdf->SetFont($FontType, 'B', $FontNormalSize);
+					$pdf->MultiCell($WidthColumn1, 0, 'Total bawa pulang:', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn2, 0, 'Rp. ' . locale_number_format($TotalBawaPulang), 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn3, 0, '', 1, 'R', 0, 0, '', '', true);
+					$pdf->MultiCell($WidthColumn4, 0, '', 1, 'L', 0, 1, '', '', true);
+					$pdf->SetFont($FontType, '', $FontNormalSize);
+
+					// payment details
+					$pdf->ln(5);
+					if ($myrow['paymentmethod'] == 'Bank'){
+						$pdf->MultiCell($WidthColumn4, 0, 'Pembayaran lewat bank transfer ke bank ' . 
+														$myrow['bankcode'] . 
+														' rekening nomor ' . 
+														$myrow['bankaccount'], 0, 'L', 0, 1, '', '', true);
+					}elseif ($myrow['paymentmethod'] == 'Check'){
+						$pdf->MultiCell($WidthColumn4, 0, 'Pembayaran lewat cek dari Bank Danamon', 0, 'L', 0, 1, '', '', true);
+					}elseif ($myrow['paymentmethod'] == 'Cash'){
+						$pdf->MultiCell($WidthColumn4, 0, 'Pembayaran Tunai' , 0, 'L', 0, 1, '', '', true);
+					}
+					
+					// footer
+					$pdf->ln(5);
+					$pdf->MultiCell($WidthColumn4, 0, 'Saya telah menerima gaji sebesar jumlah tertera di atas pada tanggal: ' . 
+													$myrow['paymentday'], 0, 'L', 0, 1, '', '', true);
+
+					$pdf->ln(40);
+					$pdf->MultiCell($WidthColumn4, 0, 'Tanda tangan: ' . $myrow['fullname'], 0, 'l', 0, 1, '', '', true);
+				
+					// update copy counter
+					$CopiesPrinted++;
+				}
 			}
+			
+			// prepare page with totals
+			$pdf->AddPage();
+			// Company header
+			if ($Company == 'PTBB'){
+				$pdf->SetFont($FontType, 'B', $FontBigSize);
+				$pdf->MultiCell(0, 0, 'PT. Bumi Biru', 0, 'L', 0, 1, '', '', true);
+				$pdf->SetFont($FontType, '', $FontSmallSize);
+				$pdf->MultiCell(0, 0, 'Jl. Kesambi 1, Kerobokan - Bali - Indonesia', 0, 'L', 0, 1, '', '', true);
+				$pdf->MultiCell(0, 0, 'Ph. +62 81 238 167 94', 0, 'L', 0, 1, '', '', true);
+			}elseif ($Company == 'PTADU'){
+				$pdf->SetFont($FontType, 'B', $FontBigSize);
+				$pdf->MultiCell(0, 0, 'PT. Angin Utara Dingin', 0, 'L', 0, 1, '', '', true);
+				$pdf->SetFont($FontType, '', $FontSmallSize);
+				$pdf->MultiCell(0, 0, 'Jl. Kesambi 1-B, Kerobokan - Bali - Indonesia', 0, 'L', 0, 1, '', '', true);
+				$pdf->MultiCell(0, 0, 'Ph. +62', 0, 'L', 0, 1, '', '', true);
+			}
+			$pdf->SetFont($FontType, '', $FontBigSize);
+			$pdf->ln(5);
+			$pdf->MultiCell(0, 0, 'Salary totals for ' . ConvertSQLDate($LastDateOfPeriod), 0, 'L', 0, 1, '', '', true);
+			$pdf->ln(5);
+			$pdf->MultiCell(0, 0, 'Total Employees by Bank Transfer: ' .	locale_number_format($EmployeesByBank), 0, 'L', 0, 1, '', '', true);
+			$pdf->MultiCell(0, 0, 'Total Amount by Bank Transfer: ' .	locale_number_format($AmountByBank), 0, 'L', 0, 1, '', '', true);
+			$pdf->MultiCell(0, 0, 'Total Employees by Check : ' .	locale_number_format($EmployeesByCheck), 0, 'L', 0, 1, '', '', true);
+			$pdf->MultiCell(0, 0, 'Total Amount by Check: ' .	locale_number_format($AmountByCheck), 0, 'L', 0, 1, '', '', true);
+			$pdf->MultiCell(0, 0, 'Total Employees by Cash: ' .	locale_number_format($EmployeesByCash), 0, 'L', 0, 1, '', '', true);
+			$pdf->MultiCell(0, 0, 'Total Amount by Cash: ' .	locale_number_format($AmountByCash), 0, 'L', 0, 1, '', '', true);
 			
 			// download the pdf file
 			$FileName= $CoreFileName . '.pdf';
