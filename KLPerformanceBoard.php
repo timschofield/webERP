c4efa53d3cf22bebd4b5470e3faf528ebcfad91a
Pak Ricard <pakricard@gmail.com>
2017-01-02 09:06:29 +0800

    Added top retail sales days hall of fame

--- KLPerformanceBoard.php
+++ KLPerformanceBoard.php
@@ -109,6 +109,11 @@ if ($ProcessSection01){
 		GeneralCustomerBehaviour(90, $db);
 		$NumberOfTestExecuted++;
 	}
+	if ($KL_SystemAdmin 
+		OR $KL_BusinessDevelopmentManager){
+		DailySalesRecords(10, $db);
+		$NumberOfTestExecuted++;
+	}
 }
 
 /***************************************************************************************
@@ -1014,6 +1019,49 @@ function PettyCashStatus($currency, $db){
 		
 		echo '</table>
 				</div>';
+	}
+}
+
+function DailySalesRecords($Days, $db){
+
+	$SQL = "SELECT salesorders.orddate,
+				SUM(salesorderdetails.qtyinvoiced * (salesorderdetails.unitprice * (1 - salesorderdetails.discountpercent))) AS sales
+			FROM salesorders
+				INNER JOIN salesorderdetails ON
+						salesorders.orderno=salesorderdetails.orderno
+			WHERE salesorders.debtorno LIKE 'RETAIL%'
+			GROUP BY salesorders.orddate
+			ORDER BY SUM(salesorderdetails.qtyinvoiced * (salesorderdetails.unitprice * (1 - salesorderdetails.discountpercent))) DESC
+			LIMIT ". $Days . "";
+
+	$result = DB_query($SQL);
+	if (DB_num_rows($result) != 0){
+		echo '<p class="page_title_text" align="center"><strong>' . _('Top ') . $Days . _(' retail sales days') .'</strong></p>';
+		echo '<div>';
+		echo '<table class="selection">';
+		$TableHeader = '<tr>
+							<th class="ascending">' .  _('#') . '</th>
+							<th class="ascending">' .  _('Date') . '</th>
+							<th class="ascending">' . _('Sales') . '</th>
+						</tr>';
+		echo $TableHeader;
+		$k = 0; //row colour counter
+		$i = 1;
+		while (($myrow = DB_fetch_array($result)) AND ($i <= $Days)) {
+			$k = StartEvenOrOddRow($k);
+			printf('<td class="number">%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					</tr>', 
+					locale_number_format($i,0),
+					ConvertSQLDate($myrow['orddate']),
+					locale_number_format($myrow['sales'],0)
+					);
+			$i++;
+		}
+		echo '</table>
+				</div>
+				</form>';
 	}
 }
 
