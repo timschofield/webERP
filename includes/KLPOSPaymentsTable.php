198e2c05c55c77291ac6ebff5cef880647c3fb82
Pak Ricard <pakricard@gmail.com>
2017-01-13 16:17:19 +0800

    prepare new variables to control returns and vouchers

--- includes/KLPOSPaymentsTable.php
+++ includes/KLPOSPaymentsTable.php
@@ -22,59 +22,100 @@ if (!isset($_POST['AmountPaidCCBCA'])){
 if (!isset($_POST['AmountReturnedGoods'])){
 	$_POST['AmountReturnedGoods'] =0;
 }
+if (!isset($_POST['ReturnedGoodsOldInvoice'])){
+	$_POST['ReturnedGoodsOldInvoice'] ='';
+}
+if (!isset($_POST['ReturnedGoodsItems'])){
+	$_POST['ReturnedGoodsItems'] ='';
+}
 if (!isset($_POST['AmountVouchers'])){
 	$_POST['AmountVouchers'] =0;
+}
+if (!isset($_POST['VoucherCode'])){
+	$_POST['VoucherCode'] ='';
 }
 if (!isset($_POST['Comments'])){
 	$_POST['Comments'] ='';
 }
 
-
 echo '<table class="selection">';
-echo'<tr>';
-echo'<th colspan=5>' . _('Payment details') . '</th>'; 
-echo'</tr>';
-
 echo '<tr>';
-echo'<th colspan=2>' . _('Cash Payments') . '</th>'; 
-echo '<td></td>';
-echo'<th colspan=2>' . _('Credit Card Payments') . '</th>'; 
+echo '<th colspan=4>' . _('Payment details') . '</th>'; 
 echo '</tr>';
 
 echo '<tr>';
+echo '<th>' . _('Cash Payments') . '</th>'; 
+echo '<td></td>';
 echo '<td>' . _('Amount Paid Cash') . ':</td>
 	  <td><input type="text" class="number" name="AmountPaidCash" maxlength="12" size="12" value="' . $_POST['AmountPaidCash'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<th>' . _('Credit Card Payments') . '</th>'; 
 echo '<td></td>';
 echo '<td>' . _('Amount Paid CC EDC Danamon') . ':</td>
 	  <td><input type="text" class="number" name="AmountPaidCCDanamon" maxlength="12" size="12" value="' . $_POST['AmountPaidCCDanamon'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo'<th colspan=2>' . _('Returned / Vouchers') . '</th>'; 
+echo '<td></td>'; 
 echo '<td></td>';
 echo '<td>' . _('Amount Paid CC EDC Mandiri') . ':</td>
 	  <td><input type="text" class="number" name="AmountPaidCCMandiri" maxlength="12" size="12" value="' . $_POST['AmountPaidCCMandiri'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo '<td>' . _('Amount Returned Goods') . ':</td>
-	  <td><input type="text" class="number" name="AmountReturnedGoods" maxlength="12" size="12" value="' . $_POST['AmountReturnedGoods'] . '" /></td>';
+echo '<td></td>'; 
 echo '<td></td>';
 echo '<td>' . _('Amount Paid CC EDC BCA') . ':</td>
 	  <td><input type="text" class="number" name="AmountPaidCCBCA" maxlength="12" size="12" value="' . $_POST['AmountPaidCCBCA'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo '<td>' . _('Amount Voucher/Discounts') . ':</td>
-	  <td><input type="text" class="number" name="AmountVouchers" maxlength="12" size="12" value="' . $_POST['AmountVouchers'] . '" /></td>';
+echo '<td></td>'; 
 echo '<td></td>';
 echo '<td>' . _('Amount Paid AMEX EDC BCA') . ':</td>
 	  <td><input type="text" class="number" name="AmountPaidAmexBCA" maxlength="12" size="12" value="' . $_POST['AmountPaidAmexBCA'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo '<td>'. _('Comments') .':</td>
-	  <td colspan= 4><textarea name="Comments" cols="60" rows="3">' . stripcslashes($_SESSION['Items'.$identifier]->Comments) .'</textarea></td>';
+echo '<th>' . _('Returned Goods') . '</th>'; 
+echo '<td></td>';
+echo '<td>' . _('Amount Returned Goods') . ':</td>
+	  <td><input type="text" class="number" name="AmountReturnedGoods" maxlength="12" size="12" value="' . $_POST['AmountReturnedGoods'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<td></td>'; 
+echo '<td></td>';
+echo '<td>' . _('Invoice number') . ':</td>
+	  <td><input type="text" class="number" name="ReturnedGoodsOldInvoice" maxlength="12" size="12" value="' . $_POST['ReturnedGoodsOldInvoice'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<td></td>'; 
+echo '<td></td>';
+echo '<td>' . _('Items returned') . ':</td>
+	  <td><input type="text" class="number" name="ReturnedGoodsItems" maxlength="40" size="12" value="' . $_POST['ReturnedGoodsItems'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<th>' . _('Vouchers/Discounts') . '</th>'; 
+echo '<td></td>';
+echo '<td>' . _('Amount Voucher/Discount') . ':</td>
+	  <td><input type="text" class="number" name="AmountVouchers" maxlength="12" size="12" value="' . $_POST['AmountVouchers'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<td></td>'; 
+echo '<td></td>';
+echo '<td>' . _('Voucher/Discount Code') . ':</td>
+	  <td><input type="text" class="number" name="VoucherCode" maxlength="40" size="12" value="' . $_POST['VoucherCode'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<th>'. _('Comments') .':</th>
+	  <td colspan= 3><textarea name="Comments" cols="40" rows="3">' . stripcslashes($_SESSION['Items'.$identifier]->Comments) .'</textarea></td>';
 echo '</tr>';
 
 echo '</table>';
