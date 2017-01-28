62c60abc4a9691cca66ccb41928e4991074ebb4a
PakRicard <pakricard@gmail.com>
2017-01-28 09:49:35 +0800

    minor changes

--- sql/From REAL to TEST.sql
+++ sql/From REAL to TEST.sql
@@ -40,6 +40,6 @@ WHERE `group` = 'templatemela'
 	AND `key` = 'templatemela_showcontrolpanel';	
 	
 UPDATE oc_setting
-SET `value` = 'http://www.bumibiru.com/TEST/shop/captainkapal/'
+SET `value` = 'http://www.mertabali.com/captainkapal/'
 WHERE `group` = 'config'
 	AND `key` = 'entry_admin_foldername';	
