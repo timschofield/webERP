e3af685c4ca8805996dd5c367b25f5c609e4a5f0
Pak Ricard <pakricard@gmail.com>
2017-01-30 16:52:02 +0800

    Add date of returned goods and reorder fields on screen

--- includes/KLPOSPaymentsTable.php
+++ includes/KLPOSPaymentsTable.php
@@ -31,6 +31,9 @@ if (!isset($_POST['ReturnedGoodsItems'])){
 if (!isset($_POST['ReturnedGoodsReason'])){
 	$_POST['ReturnedGoodsReason'] =0;
 }
+if (!isset($_POST['ReturnDate'])){
+	$_POST['ReturnDate'] = Date($_SESSION['DefaultDateFormat']);
+}
 if (!isset($_POST['AmountVouchers'])){
 	$_POST['AmountVouchers'] =0;
 }
@@ -43,70 +46,52 @@ if (!isset($_POST['Comments'])){
 
 echo '<table class="selection">';
 echo '<tr>';
-echo '<th colspan=4>' . _('Payment details') . '</th>'; 
+echo '<th colspan=6>' . _('Payment details') . '</th>'; 
 echo '</tr>';
 
 echo '<tr>';
 echo '<th>' . _('Cash Payments') . '</th>'; 
-echo '<td></td>';
 echo '<td>' . _('Amount Paid Cash') . ':</td>
 	  <td><input type="text" class="number" name="AmountPaidCash" maxlength="12" size="12" value="' . $_POST['AmountPaidCash'] . '" /></td>';
-echo '</tr>';
-
-echo '<tr>';
-echo '<th>' . _('Credit Card Payments') . '</th>'; 
-echo '<td></td>';
-echo '<td>' . _('Amount Paid CC EDC Danamon') . ':</td>
-	  <td><input type="text" class="number" name="AmountPaidCCDanamon" maxlength="12" size="12" value="' . $_POST['AmountPaidCCDanamon'] . '" /></td>';
-echo '</tr>';
-
-echo '<tr>';
-echo '<td></td>'; 
-echo '<td></td>';
-echo '<td>' . _('Amount Paid CC EDC Mandiri') . ':</td>
-	  <td><input type="text" class="number" name="AmountPaidCCMandiri" maxlength="12" size="12" value="' . $_POST['AmountPaidCCMandiri'] . '" /></td>';
-echo '</tr>';
-
-echo '<tr>';
-echo '<td></td>'; 
-echo '<td></td>';
-echo '<td>' . _('Amount Paid CC EDC BCA') . ':</td>
-	  <td><input type="text" class="number" name="AmountPaidCCBCA" maxlength="12" size="12" value="' . $_POST['AmountPaidCCBCA'] . '" /></td>';
-echo '</tr>';
-
-echo '<tr>';
-echo '<td></td>'; 
-echo '<td></td>';
-echo '<td>' . _('Amount Paid AMEX EDC BCA') . ':</td>
-	  <td><input type="text" class="number" name="AmountPaidAmexBCA" maxlength="12" size="12" value="' . $_POST['AmountPaidAmexBCA'] . '" /></td>';
-echo '</tr>';
-
-echo '<tr>';
 echo '<th>' . _('Returned Goods') . '</th>'; 
-echo '<td></td>';
 echo '<td>' . _('Amount Returned Goods') . ':</td>
 	  <td><input type="text" class="number" name="AmountReturnedGoods" maxlength="12" size="12" value="' . $_POST['AmountReturnedGoods'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo '<td></td>'; 
+echo '<th>' . _('Credit Card Payments') . '</th>'; 
+echo '<td>' . _('Amount Paid CC EDC Danamon') . ':</td>
+	  <td><input type="text" class="number" name="AmountPaidCCDanamon" maxlength="12" size="12" value="' . $_POST['AmountPaidCCDanamon'] . '" /></td>';
 echo '<td></td>';
 echo '<td>' . _('Invoice number') . ':</td>
 	  <td><input type="text" class="number" name="ReturnedGoodsOldInvoice" maxlength="12" size="12" value="' . $_POST['ReturnedGoodsOldInvoice'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo '<td></td>'; 
+echo '<td></td>';
+echo '<td>' . _('Amount Paid CC EDC Mandiri') . ':</td>
+	  <td><input type="text" class="number" name="AmountPaidCCMandiri" maxlength="12" size="12" value="' . $_POST['AmountPaidCCMandiri'] . '" /></td>';
+echo '<td></td>';
+echo '<td>' . _('Invoice Date') . ':</td>';
+echo '<td><input type="text" class="date" alt="' .$_SESSION['DefaultDateFormat'] .'" name="ReturnDate" size="10" maxlength="10" value="' . $_POST['ReturnDate'] . '" /></td>';
+echo '</tr>';
+
+echo '<tr>';
+echo '<td></td>';
+echo '<td>' . _('Amount Paid CC EDC BCA') . ':</td>
+	  <td><input type="text" class="number" name="AmountPaidCCBCA" maxlength="12" size="12" value="' . $_POST['AmountPaidCCBCA'] . '" /></td>';
 echo '<td></td>';
 echo '<td>' . _('Items returned') . ':</td>
 	  <td><input type="text" class="number" name="ReturnedGoodsItems" maxlength="40" size="12" value="' . $_POST['ReturnedGoodsItems'] . '" /></td>';
 echo '</tr>';
 
-echo '<tr>
-		<td></td>
-		<td></td>
-		<td>' . _('Reason of return') . ':</td>
-		<td><select name="ReturnedGoodsReason">';
+echo '<tr>';
+echo '<td></td>';
+echo '<td>' . _('Amount Paid AMEX EDC BCA') . ':</td>
+	  <td><input type="text" class="number" name="AmountPaidAmexBCA" maxlength="12" size="12" value="' . $_POST['AmountPaidAmexBCA'] . '" /></td>';
+echo '<td></td>';
+echo '<td>' . _('Reason of return') . ':</td>
+	  <td><select name="ReturnedGoodsReason">';
 
 $SQL = "SELECT reasonid,
 				reasonname
@@ -121,18 +106,22 @@ while ($myrow=DB_fetch_array($result)){
 		echo '<option value="' . $myrow['reasonid'] . '">' . $myrow['reasonname'] . '</option>';
 	}
 }
-echo '</select></td></tr>';
-
+echo '</select></td>';
+echo '</tr>';
 
 echo '<tr>';
-echo '<th>' . _('Vouchers/Discounts') . '</th>'; 
 echo '<td></td>';
+echo '<td></td>';
+echo '<td></td>';
+echo '<th>' . _('Vouchers/Discounts') . '</th>'; 
 echo '<td>' . _('Amount Voucher/Discount') . ':</td>
 	  <td><input type="text" class="number" name="AmountVouchers" maxlength="12" size="12" value="' . $_POST['AmountVouchers'] . '" /></td>';
 echo '</tr>';
 
 echo '<tr>';
-echo '<td></td>'; 
+echo '<td></td>';
+echo '<td></td>';
+echo '<td></td>';
 echo '<td></td>';
 echo '<td>' . _('Voucher/Discount Code') . ':</td>
 	  <td><input type="text" class="number" name="VoucherCode" maxlength="40" size="12" value="' . $_POST['VoucherCode'] . '" /></td>';
@@ -140,7 +129,7 @@ echo '</tr>';
 
 echo '<tr>';
 echo '<th>'. _('Comments') .':</th>
-	  <td colspan= 3><textarea name="Comments" cols="40" rows="3">' . stripcslashes($_SESSION['Items'.$identifier]->Comments) .'</textarea></td>';
+	  <td colspan=5><textarea name="Comments" cols="100" rows="3">' . stripcslashes($_SESSION['Items'.$identifier]->Comments) .'</textarea></td>';
 echo '</tr>';
 
 echo '</table>';
