015420fb89d035734fbe731db2e93cacb45aa963
exsonqu <exsonqu@09516a42-71f9-43be-811f-9b821cf0d246>
2017-09-06 06:08:38 +0000

    17/09/06 Exson: Fixed the bug that invoice cannot be issued by same transaction multiple times in SuppTrans.php.
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7824 09516a42-71f9-43be-811f-9b821cf0d246

--- sql/mysql/upgrade4.14.1-4.14.2.sql
+++ sql/mysql/upgrade4.14.1-4.14.2.sql
@@ -2,4 +2,6 @@ ALTER table supptrans ADD chequeno varchar(16) NOT NULL DEFAULT '';
 ALTER table supptrans ADD void tinyint(1) NOT NULL DEFAULT 0;
 ALTER table banktrans ADD chequeno varchar(16) NOT NULL DEFAULT '';
 INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description`) VALUES ('Z_RemovePurchaseBackOrders.php',  '1',  'Removes all purchase order back orders');
+ALTER table supptrans DROP KEY `TypeTransNo`; 
+ALTER table supptrans ADD KEY `TypeTransNo`(`transno`,`type`); 
 
