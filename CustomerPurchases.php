ab00f2163f8633a17d80f8d0b042fea1bbb524fb
turbopt <turbopt@09516a42-71f9-43be-811f-9b821cf0d246>
2018-01-15 22:41:51 +0000

    CustomerPurchases.php: Adds Units and Discount columns and other minor changes so that it more closely matches output from OrderDetails.php. (Reported in forums: http://www.weberp.org/forum/showthread.php?tid=8040)
    git-svn-id: https://svn.code.sf.net/p/web-erp/code/trunk@7917 09516a42-71f9-43be-811f-9b821cf0d246

--- CustomerPurchases.php
+++ CustomerPurchases.php
@@ -37,6 +37,7 @@ echo '<p class="page_title_text"><img alt="" src="'.$RootPath.'/css/'.$Theme.
 
 $SQL = "SELECT stockmoves.stockid,
 			stockmaster.description,
+			stockmaster.units,
 			systypes.typename,
 			transno,
 			locations.locationname,
@@ -84,9 +85,11 @@ else {
 				<th>' . _('Transaction No.') . '</th>
 				<th>' . _('From Location') . '</th>
 				<th>' . _('Branch Code') . '</th>
-				<th>' . _('Price') . '</th>
 				<th>' . _('Quantity') . '</th>
-				<th>' . _('Amount of Sale') . '</th>
+				<th>' . _('Unit') . '</th>
+				<th>' . _('Price') . '</th>
+				<th>' . _('Discount') . '</th>
+				<th>' . _('Total') . '</th>
 				<th>' . _('Reference') . '</th>
 				<th>' . _('Narrative') . '</th>
 			</tr>';
@@ -100,8 +103,10 @@ else {
 				<td class="number">' . $StockMovesRow['transno'] . '</td>
 				<td>' . $StockMovesRow['locationname'] . '</td>
 				<td>' . $StockMovesRow['branchcode'] . '</td>
+				<td class="number">' . -$StockMovesRow['qty'] . '</td>
+				<td>' . $StockMovesRow['units'] . '</td>
 				<td class="number">' . locale_number_format($StockMovesRow['price'] * (1 - $StockMovesRow['discountpercent']), $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
-				<td class="number">' . locale_number_format(-$StockMovesRow['qty'], $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+				<td class="number">' . locale_number_format(($StockMovesRow['discountpercent'] * 100),2) . '%' . '</td>
 				<td class="number">' . locale_number_format((-$StockMovesRow['qty'] * $StockMovesRow['price'] * (1 - $StockMovesRow['discountpercent'])), $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
 				<td class="number">' . $StockMovesRow['reference'] . '</td>
 				<td>' . $StockMovesRow['narrative'] . '</td>
