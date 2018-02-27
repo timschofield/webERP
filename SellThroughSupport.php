bd3e0ec7705f21e35018271a3e7b3dec5864f6b6
Paul Thursby <pthursby2@gmail.com>
2018-02-27 11:09:14 +0000

    SellThroughSupport.php: Remove redundant hidden FormID input.

--- SellThroughSupport.php
+++ SellThroughSupport.php
@@ -154,7 +154,7 @@ if (isset($_POST['SearchSupplier'])) {
 	echo '<form action="' . htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '" method="post">
 			<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />
 			<table cellpadding="2" colspan="7" class="selection">';
-	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+
     $TableHeader = '<tr>
 						<th>' . _('Code') . '</th>
 	                	<th>' . _('Supplier Name') . '</th>
