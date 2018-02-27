5fc33c8826064f7a60e4ae9fd3175e87b3b808cf
Paul Thursby <pthursby2@gmail.com>
2018-02-27 11:34:31 +0000

    MaintenanceUserSchedule.php: Fix closing tag mismatch.

--- MaintenanceUserSchedule.php
+++ MaintenanceUserSchedule.php
@@ -49,7 +49,7 @@ echo '<table class="selection">
 		<th>' . _('Asset') . '</th>
 		<th>' . _('Description') . '</th>
 		<th>' . _('Last Completed') . '</th>
-		<th>' . _('Due By') . '</td>
+		<th>' . _('Due By') . '</th>
 		<th>' . _('Person') . '</th>
 		<th>' . _('Manager') . '</th>
 		<th>' . _('Now Complete') . '</th>
