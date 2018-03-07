53c7406459ec90df2b543ea66a805bb9172cf78c
Pak Ricard <pakricard@gmail.com>
2018-03-07 15:14:15 +0800

    petty cash for PTADU

---
+++ sql/PettyCash to PTADU.sql
@@ -0,0 +1,158 @@
+SET FOREIGN_KEY_CHECKS=0;
+UPDATE pcexpenses SET codeexpense = "CARGO-IMPORT-BB" WHERE codeexpense = "CARGO-IMPORT";
+UPDATE pctabexpenses SET codeexpense = "CARGO-IMPORT-BB" WHERE codeexpense = "CARGO-IMPORT";
+UPDATE pcashdetails SET codeexpense = "CARGO-IMPORT-BB" WHERE codeexpense = "CARGO-IMPORT";
+UPDATE pcexpenses SET codeexpense = "CARGO-LOCAL-BB" WHERE codeexpense = "CARGO-LOCAL";
+UPDATE pctabexpenses SET codeexpense = "CARGO-LOCAL-BB" WHERE codeexpense = "CARGO-LOCAL";
+UPDATE pcashdetails SET codeexpense = "CARGO-LOCAL-BB" WHERE codeexpense = "CARGO-LOCAL";
+UPDATE pcexpenses SET codeexpense = "IMPORT-DUTIES-BB" WHERE codeexpense = "IMPORT-DUTIES";
+UPDATE pctabexpenses SET codeexpense = "IMPORT-DUTIES-BB" WHERE codeexpense = "IMPORT-DUTIES";
+UPDATE pcashdetails SET codeexpense = "IMPORT-DUTIES-BB" WHERE codeexpense = "IMPORT-DUTIES";
+UPDATE pcexpenses SET codeexpense = "INTERNET-KANTOR-BB" WHERE codeexpense = "INTERNET-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "INTERNET-KANTOR-BB" WHERE codeexpense = "INTERNET-KANTOR";
+UPDATE pcashdetails SET codeexpense = "INTERNET-KANTOR-BB" WHERE codeexpense = "INTERNET-KANTOR";
+UPDATE pcexpenses SET codeexpense = "INTERVIEW-BB" WHERE codeexpense = "INTERVIEW";
+UPDATE pctabexpenses SET codeexpense = "INTERVIEW-BB" WHERE codeexpense = "INTERVIEW";
+UPDATE pcashdetails SET codeexpense = "INTERVIEW-BB" WHERE codeexpense = "INTERVIEW";
+UPDATE pcexpenses SET codeexpense = "JAMSOSTEK-BB" WHERE codeexpense = "JAMSOSTEK";
+UPDATE pctabexpenses SET codeexpense = "JAMSOSTEK-BB" WHERE codeexpense = "JAMSOSTEK";
+UPDATE pcashdetails SET codeexpense = "JAMSOSTEK-BB" WHERE codeexpense = "JAMSOSTEK";
+UPDATE pcexpenses SET codeexpense = "JASA-SERVICE-BB" WHERE codeexpense = "JASA-SERVICE";
+UPDATE pctabexpenses SET codeexpense = "JASA-SERVICE-BB" WHERE codeexpense = "JASA-SERVICE";
+UPDATE pcashdetails SET codeexpense = "JASA-SERVICE-BB" WHERE codeexpense = "JASA-SERVICE";
+UPDATE pcexpenses SET codeexpense = "JASA-SERVICE-2%-BB" WHERE codeexpense = "JASA-SERVICE-2%";
+UPDATE pctabexpenses SET codeexpense = "JASA-SERVICE-2%-BB" WHERE codeexpense = "JASA-SERVICE-2%";
+UPDATE pcashdetails SET codeexpense = "JASA-SERVICE-2%-BB" WHERE codeexpense = "JASA-SERVICE-2%";
+UPDATE pcexpenses SET codeexpense = "JASA-SERVICE-4%-BB" WHERE codeexpense = "JASA-SERVICE-4%";
+UPDATE pctabexpenses SET codeexpense = "JASA-SERVICE-4%-BB" WHERE codeexpense = "JASA-SERVICE-4%";
+UPDATE pcashdetails SET codeexpense = "JASA-SERVICE-4%-BB" WHERE codeexpense = "JASA-SERVICE-4%";
+UPDATE pcexpenses SET codeexpense = "KOMPUTER-HW-BB" WHERE codeexpense = "KOMPUTER-HARDWARE";
+UPDATE pctabexpenses SET codeexpense = "KOMPUTER-HW-BB" WHERE codeexpense = "KOMPUTER-HARDWARE";
+UPDATE pcashdetails SET codeexpense = "KOMPUTER-HW-BB" WHERE codeexpense = "KOMPUTER-HARDWARE";
+UPDATE pcexpenses SET codeexpense = "KOMPUTER-SERVIS-BB" WHERE codeexpense = "KOMPUTER-SERVIS";
+UPDATE pctabexpenses SET codeexpense = "KOMPUTER-SERVIS-BB" WHERE codeexpense = "KOMPUTER-SERVIS";
+UPDATE pcashdetails SET codeexpense = "KOMPUTER-SERVIS-BB" WHERE codeexpense = "KOMPUTER-SERVIS";
+UPDATE pcexpenses SET codeexpense = "KOMPUTER-WEB-BB" WHERE codeexpense = "KOMPUTER-WEBSERVICES";
+UPDATE pctabexpenses SET codeexpense = "KOMPUTER-WEB-BB" WHERE codeexpense = "KOMPUTER-WEBSERVICES";
+UPDATE pcashdetails SET codeexpense = "KOMPUTER-WEB-BB" WHERE codeexpense = "KOMPUTER-WEBSERVICES";
+UPDATE pcexpenses SET codeexpense = "KONSUL-PAJAK-BB" WHERE codeexpense = "KONSULTAN-PAJAK";
+UPDATE pctabexpenses SET codeexpense = "KONSUL-PAJAK-BB" WHERE codeexpense = "KONSULTAN-PAJAK";
+UPDATE pcashdetails SET codeexpense = "KONSUL-PAJAK-BB" WHERE codeexpense = "KONSULTAN-PAJAK";
+UPDATE pcexpenses SET codeexpense = "MAINT-KANTOR-BB" WHERE codeexpense = "MAINTENANCE-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "MAINT-KANTOR-BB" WHERE codeexpense = "MAINTENANCE-KANTOR";
+UPDATE pcashdetails SET codeexpense = "MAINT-KANTOR-BB" WHERE codeexpense = "MAINTENANCE-KANTOR";
+UPDATE pcexpenses SET codeexpense = "MAINT-MOBIL-SE-BB" WHERE codeexpense = "MAINTENANCE-MOBIL-SE";
+UPDATE pctabexpenses SET codeexpense = "MAINT-MOBIL-SE-BB" WHERE codeexpense = "MAINTENANCE-MOBIL-SE";
+UPDATE pcashdetails SET codeexpense = "MAINT-MOBIL-SE-BB" WHERE codeexpense = "MAINTENANCE-MOBIL-SE";
+UPDATE pcexpenses SET codeexpense = "OFFICE-MAT-BB" WHERE codeexpense = "OFFICE-MAT-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "OFFICE-MAT-BB" WHERE codeexpense = "OFFICE-MAT-KANTOR";
+UPDATE pcashdetails SET codeexpense = "OFFICE-MAT-BB" WHERE codeexpense = "OFFICE-MAT-KANTOR";
+UPDATE pcexpenses SET codeexpense = "PACKAGING-TOKO-BB" WHERE codeexpense = "PACKAGING-TOKO";
+UPDATE pctabexpenses SET codeexpense = "PACKAGING-TOKO-BB" WHERE codeexpense = "PACKAGING-TOKO";
+UPDATE pcashdetails SET codeexpense = "PACKAGING-TOKO-BB" WHERE codeexpense = "PACKAGING-TOKO";
+UPDATE pcexpenses SET codeexpense = "PARKIR-KANTOR-BB" WHERE codeexpense = "PARKIR-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "PARKIR-KANTOR-BB" WHERE codeexpense = "PARKIR-KANTOR";
+UPDATE pcashdetails SET codeexpense = "PARKIR-KANTOR-BB" WHERE codeexpense = "PARKIR-KANTOR";
+UPDATE pcexpenses SET codeexpense = "PEMBERSIH-BB" WHERE codeexpense = "PEMBERSIH-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "PEMBERSIH-BB" WHERE codeexpense = "PEMBERSIH-KANTOR";
+UPDATE pcashdetails SET codeexpense = "PEMBERSIH-BB" WHERE codeexpense = "PEMBERSIH-KANTOR";
+UPDATE pcexpenses SET codeexpense = "PERIZINAN-BB" WHERE codeexpense = "PERIZINAN-PT";
+UPDATE pctabexpenses SET codeexpense = "PERIZINAN-BB" WHERE codeexpense = "PERIZINAN-PT";
+UPDATE pcashdetails SET codeexpense = "PERIZINAN-BB" WHERE codeexpense = "PERIZINAN-PT";
+UPDATE pcexpenses SET codeexpense = "PERSO-TRAINING-BB" WHERE codeexpense = "PERSONALIA-TRAINING";
+UPDATE pctabexpenses SET codeexpense = "PERSO-TRAINING-BB" WHERE codeexpense = "PERSONALIA-TRAINING";
+UPDATE pcashdetails SET codeexpense = "PERSO-TRAINING-BB" WHERE codeexpense = "PERSONALIA-TRAINING";
+UPDATE pcexpenses SET codeexpense = "PERSO-IKLAN-BB" WHERE codeexpense = "PERSONALIA-IKLAN";
+UPDATE pctabexpenses SET codeexpense = "PERSO-IKLAN-BB" WHERE codeexpense = "PERSONALIA-IKLAN";
+UPDATE pcashdetails SET codeexpense = "PERSO-IKLAN-BB" WHERE codeexpense = "PERSONALIA-IKLAN";
+UPDATE pcexpenses SET codeexpense = "PERSO-GATHERING-BB" WHERE codeexpense = "PERSONALIA-GATHERING";
+UPDATE pctabexpenses SET codeexpense = "PERSO-GATHERING-BB" WHERE codeexpense = "PERSONALIA-GATHERING";
+UPDATE pcashdetails SET codeexpense = "PERSO-GATHERING-BB" WHERE codeexpense = "PERSONALIA-GATHERING";
+UPDATE pcexpenses SET codeexpense = "PPH21-BB" WHERE codeexpense = "PPH21";
+UPDATE pctabexpenses SET codeexpense = "PPH21-BB" WHERE codeexpense = "PPH21";
+UPDATE pcashdetails SET codeexpense = "PPH21-BB" WHERE codeexpense = "PPH21";
+UPDATE pcexpenses SET codeexpense = "PPH22-BB" WHERE codeexpense = "PPH22";
+UPDATE pctabexpenses SET codeexpense = "PPH22-BB" WHERE codeexpense = "PPH22";
+UPDATE pcashdetails SET codeexpense = "PPH22-BB" WHERE codeexpense = "PPH22";
+UPDATE pcexpenses SET codeexpense = "PPH23-BB" WHERE codeexpense = "PPH23";
+UPDATE pctabexpenses SET codeexpense = "PPH23-BB" WHERE codeexpense = "PPH23";
+UPDATE pcashdetails SET codeexpense = "PPH23-BB" WHERE codeexpense = "PPH23";
+
+UPDATE pcexpenses SET codeexpense = "PPH25-BB" WHERE codeexpense = "PPH25";
+UPDATE pctabexpenses SET codeexpense = "PPH25-BB" WHERE codeexpense = "PPH25";
+UPDATE pcashdetails SET codeexpense = "PPH25-BB" WHERE codeexpense = "PPH25";
+
+UPDATE pcexpenses SET codeexpense = "PPH4-BB" WHERE codeexpense = "PPH4";
+UPDATE pctabexpenses SET codeexpense = "PPH4-BB" WHERE codeexpense = "PPH4";
+UPDATE pcashdetails SET codeexpense = "PPH4-BB" WHERE codeexpense = "PPH4";
+
+UPDATE pcexpenses SET codeexpense = "PPN-BB" WHERE codeexpense = "PPN";
+UPDATE pctabexpenses SET codeexpense = "PPN-BB" WHERE codeexpense = "PPN";
+UPDATE pcashdetails SET codeexpense = "PPN-BB" WHERE codeexpense = "PPN";
+
+UPDATE pcexpenses SET codeexpense = "RENOVASI-KANTOR-BB" WHERE codeexpense = "RENOVASI-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "RENOVASI-KANTOR-BB" WHERE codeexpense = "RENOVASI-KANTOR";
+UPDATE pcashdetails SET codeexpense = "RENOVASI-KANTOR-BB" WHERE codeexpense = "RENOVASI-KANTOR";
+
+UPDATE pcexpenses SET codeexpense = "RENT-VEHICLE-BB" WHERE codeexpense = "RENT-PICKUP-TAXI";
+UPDATE pctabexpenses SET codeexpense = "RENT-VEHICLE-BB" WHERE codeexpense = "RENT-PICKUP-TAXI";
+UPDATE pcashdetails SET codeexpense = "RENT-VEHICLE-BB" WHERE codeexpense = "RENT-PICKUP-TAXI";
+
+UPDATE pcexpenses SET codeexpense = "SAMPLES-NO-ERP-BB" WHERE codeexpense = "SAMPLES-NO-WEBERP";
+UPDATE pctabexpenses SET codeexpense = "SAMPLES-NO-ERP-BB" WHERE codeexpense = "SAMPLES-NO-WEBERP";
+UPDATE pcashdetails SET codeexpense = "SAMPLES-NO-ERP-BB" WHERE codeexpense = "SAMPLES-NO-WEBERP";
+
+UPDATE pcexpenses SET codeexpense = "SERVIS-STOCK-BB" WHERE codeexpense = "SERVIS-STOCK";
+UPDATE pctabexpenses SET codeexpense = "SERVIS-STOCK-BB" WHERE codeexpense = "SERVIS-STOCK";
+UPDATE pcashdetails SET codeexpense = "SERVIS-STOCK-BB" WHERE codeexpense = "SERVIS-STOCK";
+
+UPDATE pcexpenses SET codeexpense = "TELPON-KANTOR-BB" WHERE codeexpense = "TELPON-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "TELPON-KANTOR-BB" WHERE codeexpense = "TELPON-KANTOR";
+UPDATE pcashdetails SET codeexpense = "TELPON-KANTOR-BB" WHERE codeexpense = "TELPON-KANTOR";
+
+UPDATE pcexpenses SET codeexpense = "TRIP-INDO-BB" WHERE codeexpense = "TRIP-INDONESIA";
+UPDATE pctabexpenses SET codeexpense = "TRIP-INDO-BB" WHERE codeexpense = "TRIP-INDONESIA";
+UPDATE pcashdetails SET codeexpense = "TRIP-INDO-BB" WHERE codeexpense = "TRIP-INDONESIA";
+
+UPDATE pcexpenses SET codeexpense = "TRIP-OUT-INDO-BB" WHERE codeexpense = "TRIP-OUT-INDONESIA";
+UPDATE pctabexpenses SET codeexpense = "TRIP-OUT-INDO-BB" WHERE codeexpense = "TRIP-OUT-INDONESIA";
+UPDATE pcashdetails SET codeexpense = "TRIP-OUT-INDO-BB" WHERE codeexpense = "TRIP-OUT-INDONESIA";
+
+UPDATE pcexpenses SET codeexpense = "WATER-PDAM-KANT-BB" WHERE codeexpense = "WATER-PDAM-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "WATER-PDAM-KANT-BB" WHERE codeexpense = "WATER-PDAM-KANTOR";
+UPDATE pcashdetails SET codeexpense = "WATER-PDAM-KANT-BB" WHERE codeexpense = "WATER-PDAM-KANTOR";
+
+INSERT INTO `pcexpenses` (`codeexpense`, `description`, `glaccount`, `tag`, `klretentionpph23`) VALUES
+('IMPORT-DUTIES-ADU', 'Import Duties (Bea Cukai) PT ADU', '612011940AD', 1, '0.00'),
+('INTERNET-KANTOR-ADU', 'Internet for kantor PT ADU', '614011100AD', 1, '0.00'),
+('JAMSOSTEK-ADU', 'Jamsostek payments PT ADU', '611011700AD', 0, '0.00'),
+('JASA-SERVICE-2%-ADU', 'Payments for services with 2% PPH23 PT ADU', '614012000AD', 0, '2.00'),
+('JASA-SERVICE-4%-ADU', 'Payments for services with 4% PPH23 PT ADU', '614012000AD', 0, '4.00'),
+('KOMPUTER-HW-ADU', 'Small Hardware, IT accessories PT ADU', '614012050AD', 1, '0.00'),
+('KOMPUTER-SERVIS-ADU', 'Service computer, software, printer, etc PT ADU', '613011200AD', 0, '4.00'),
+('KOMPUTER-WEB-ADU', 'Web services payments PT ADU', '614011200AD', 1, '0.00'),
+('KONSUL-PAJAK-ADU', 'Payment for consultant Pajak PT ADU', '614011800AD', 0, '0.00'),
+('MAINT-KANTOR-ADU', 'Maintenance Kantor PT ADU', '614012000AD', 1, '0.00'),
+('MAINT-MOBIL-SE-ADU', 'Maintenance Sepeda Motor - Mobil PT ADU', '614012850AD', 1, '0.00'),
+('OFFICE-MAT-ADU', 'Office material for PT ADU', '614012050AD', 0, '0.00'),
+('PACKAGING-ADU', 'Packaging material for PT ADU', '612011705AD', 0, '0.00'),
+('PARKIR-KANTOR-ADU', 'Payment parkir Kantor PT ADU', '614012900AD', 1, '0.00'),
+('PEMBERSIH-ADU', 'Pembersih Kantor PT ADU', '614011550AD', 1, '0.00'),
+('PERIZINAN-ADU', 'Payment Perizinan PT Angin Dingin Utara', '614012200AD', 0, '0.00'),
+('PPH21-ADU', 'Payment PPH21 PT ADU', '611012000AD', 0, '0.00'),
+('PPH22-ADU', 'Payment PPH22 PT ADU', '611012002AD', 0, '0.00'),
+('PPH23-ADU', 'Payment PPH23 PT ADU', '611012005AD', 0, '0.00'),
+('PPH25-ADU', 'Payment PPH25 PT ADU', '611012010AD', 0, '0.00'),
+('PPH4-ADU', 'Payment PPH4 PT ADU', '611012020AD', 0, '0.00'),
+('PPHFINAL1-IK', 'Payment Penhasilan Final 1% POIK', '800011000IK', 0, '0.00'),
+('PPN-ADU', 'Payment Tunjangan PPN PT ADU', '611012030AD', 0, '0.00'),
+('RENOVASI-KANTOR-ADU', 'Renovasi - Decorasi Kantor PT ADU', '614012550AD', 1, '0.00'),
+('RENT-VEHICLE-ADU', 'Rental of PickUp-Taxi-Car-Motorbike PT ADU', '614012850AD', 0, '0.00'),
+('SAMPLES-NO-ERP-ADU', 'Samples not in stock webERP PT ADU', '612011800AD', 0, '0.00'),
+('SERVIS-STOCK-ADU', 'Service (Maintenance) stock goods PT ADU', '613019000AD', 0, '0.00'),
+('TELPON-KANTOR-ADU', 'Phone payments for PT ADU', '614011100AD', 1, '0.00'),
+('TRIP-INDO-ADU', 'Trip expenses indide Indonesia PT ADU', '614013500AD', 0, '0.00'),
+('TRIP-OUT-INDO-ADU', 'Trip expenses outside Indonesia PT ADU', '614013600AD', 0, '0.00'),
+('WATER-PDAM-ADU', 'Water (PDAM) for PT ADU', '614011510AD', 1, '0.00');
+
+SET FOREIGN_KEY_CHECKS=1;
---
+++ sql/SALARIES TO PTADU NOT DONE YET!!!!.sql
@@ -0,0 +1,170 @@
+SET FOREIGN_KEY_CHECKS=0;
+UPDATE pcexpenses SET codeexpense = "CARGO-IMPORT-BB" WHERE pcexpenses = "CARGO-IMPORT";
+UPDATE pctabexpenses SET codeexpense = "CARGO-IMPORT-BB" WHERE pcexpenses = "CARGO-IMPORT";
+UPDATE pcashdetails SET codeexpense = "CARGO-IMPORT-BB" WHERE pcexpenses = "CARGO-IMPORT";
+UPDATE pcexpenses SET codeexpense = "CARGO-LOCAL-BB" WHERE pcexpenses = "CARGO-LOCAL";
+UPDATE pctabexpenses SET codeexpense = "CARGO-LOCAL-BB" WHERE pcexpenses = "CARGO-LOCAL";
+UPDATE pcashdetails SET codeexpense = "CARGO-LOCAL-BB" WHERE pcexpenses = "CARGO-LOCAL";
+UPDATE pcexpenses SET codeexpense = "IMPORT-DUTIES-BB" WHERE pcexpenses = "IMPORT-DUTIES";
+UPDATE pctabexpenses SET codeexpense = "IMPORT-DUTIES-BB" WHERE pcexpenses = "IMPORT-DUTIES";
+UPDATE pcashdetails SET codeexpense = "IMPORT-DUTIES-BB" WHERE pcexpenses = "IMPORT-DUTIES";
+UPDATE pcexpenses SET codeexpense = "INTERNET-KANTOR-BB" WHERE pcexpenses = "INTERNET-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "INTERNET-KANTOR-BB" WHERE pcexpenses = "INTERNET-KANTOR";
+UPDATE pcashdetails SET codeexpense = "INTERNET-KANTOR-BB" WHERE pcexpenses = "INTERNET-KANTOR";
+UPDATE pcexpenses SET codeexpense = "INTERVIEW-BB" WHERE pcexpenses = "INTERVIEW";
+UPDATE pctabexpenses SET codeexpense = "INTERVIEW-BB" WHERE pcexpenses = "INTERVIEW";
+UPDATE pcashdetails SET codeexpense = "INTERVIEW-BB" WHERE pcexpenses = "INTERVIEW";
+UPDATE pcexpenses SET codeexpense = "JAMSOSTEK-BB" WHERE pcexpenses = "JAMSOSTEK";
+UPDATE pctabexpenses SET codeexpense = "JAMSOSTEK-BB" WHERE pcexpenses = "JAMSOSTEK";
+UPDATE pcashdetails SET codeexpense = "JAMSOSTEK-BB" WHERE pcexpenses = "JAMSOSTEK";
+UPDATE pcexpenses SET codeexpense = "JASA-SERVICE-BB" WHERE pcexpenses = "JASA-SERVICE";
+UPDATE pctabexpenses SET codeexpense = "JASA-SERVICE-BB" WHERE pcexpenses = "JASA-SERVICE";
+UPDATE pcashdetails SET codeexpense = "JASA-SERVICE-BB" WHERE pcexpenses = "JASA-SERVICE";
+UPDATE pcexpenses SET codeexpense = "JASA-SERVICE-2%-BB" WHERE pcexpenses = "JASA-SERVICE-2%";
+UPDATE pctabexpenses SET codeexpense = "JASA-SERVICE-2%-BB" WHERE pcexpenses = "JASA-SERVICE-2%";
+UPDATE pcashdetails SET codeexpense = "JASA-SERVICE-2%-BB" WHERE pcexpenses = "JASA-SERVICE-2%";
+UPDATE pcexpenses SET codeexpense = "JASA-SERVICE-4%-BB" WHERE pcexpenses = "JASA-SERVICE-4%";
+UPDATE pctabexpenses SET codeexpense = "JASA-SERVICE-4%-BB" WHERE pcexpenses = "JASA-SERVICE-4%";
+UPDATE pcashdetails SET codeexpense = "JASA-SERVICE-4%-BB" WHERE pcexpenses = "JASA-SERVICE-4%";
+UPDATE pcexpenses SET codeexpense = "KOMPUTER-HW-BB" WHERE pcexpenses = "KOMPUTER-HARDWARE";
+UPDATE pctabexpenses SET codeexpense = "KOMPUTER-HW-BB" WHERE pcexpenses = "KOMPUTER-HARDWARE";
+UPDATE pcashdetails SET codeexpense = "KOMPUTER-HW-BB" WHERE pcexpenses = "KOMPUTER-HARDWARE";
+UPDATE pcexpenses SET codeexpense = "KOMPUTER-SERVIS-BB" WHERE pcexpenses = "KOMPUTER-SERVIS";
+UPDATE pctabexpenses SET codeexpense = "KOMPUTER-SERVIS-BB" WHERE pcexpenses = "KOMPUTER-SERVIS";
+UPDATE pcashdetails SET codeexpense = "KOMPUTER-SERVIS-BB" WHERE pcexpenses = "KOMPUTER-SERVIS";
+UPDATE pcexpenses SET codeexpense = "KOMPUTER-WEB-BB" WHERE pcexpenses = "KOMPUTER-WEBSERVICES";
+UPDATE pctabexpenses SET codeexpense = "KOMPUTER-WEB-BB" WHERE pcexpenses = "KOMPUTER-WEBSERVICES";
+UPDATE pcashdetails SET codeexpense = "KOMPUTER-WEB-BB" WHERE pcexpenses = "KOMPUTER-WEBSERVICES";
+UPDATE pcexpenses SET codeexpense = "KONSUL-PAJAK-BB" WHERE pcexpenses = "KONSULTAN-PAJAK";
+UPDATE pctabexpenses SET codeexpense = "KONSUL-PAJAK-BB" WHERE pcexpenses = "KONSULTAN-PAJAK";
+UPDATE pcashdetails SET codeexpense = "KONSUL-PAJAK-BB" WHERE pcexpenses = "KONSULTAN-PAJAK";
+UPDATE pcexpenses SET codeexpense = "MAINT-KANTOR-BB" WHERE pcexpenses = "MAINTENANCE-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "MAINT-KANTOR-BB" WHERE pcexpenses = "MAINTENANCE-KANTOR";
+UPDATE pcashdetails SET codeexpense = "MAINT-KANTOR-BB" WHERE pcexpenses = "MAINTENANCE-KANTOR";
+UPDATE pcexpenses SET codeexpense = "MAINT-MOBIL-SE-BB" WHERE pcexpenses = "MAINTENANCE-MOBIL-SE";
+UPDATE pctabexpenses SET codeexpense = "MAINT-MOBIL-SE-BB" WHERE pcexpenses = "MAINTENANCE-MOBIL-SE";
+UPDATE pcashdetails SET codeexpense = "MAINT-MOBIL-SE-BB" WHERE pcexpenses = "MAINTENANCE-MOBIL-SE";
+UPDATE pcexpenses SET codeexpense = "OFFICE-MAT-BB" WHERE pcexpenses = "OFFICE-MAT-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "OFFICE-MAT-BB" WHERE pcexpenses = "OFFICE-MAT-KANTOR";
+UPDATE pcashdetails SET codeexpense = "OFFICE-MAT-BB" WHERE pcexpenses = "OFFICE-MAT-KANTOR";
+UPDATE pcexpenses SET codeexpense = "PACKAGING-TOKO-BB" WHERE pcexpenses = "PACKAGING-TOKO";
+UPDATE pctabexpenses SET codeexpense = "PACKAGING-TOKO-BB" WHERE pcexpenses = "PACKAGING-TOKO";
+UPDATE pcashdetails SET codeexpense = "PACKAGING-TOKO-BB" WHERE pcexpenses = "PACKAGING-TOKO";
+UPDATE pcexpenses SET codeexpense = "PARKIR-KANTOR-BB" WHERE pcexpenses = "PARKIR-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "PARKIR-KANTOR-BB" WHERE pcexpenses = "PARKIR-KANTOR";
+UPDATE pcashdetails SET codeexpense = "PARKIR-KANTOR-BB" WHERE pcexpenses = "PARKIR-KANTOR";
+UPDATE pcexpenses SET codeexpense = "PEMBERSIH-BB" WHERE pcexpenses = "PEMBERSIH-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "PEMBERSIH-BB" WHERE pcexpenses = "PEMBERSIH-KANTOR";
+UPDATE pcashdetails SET codeexpense = "PEMBERSIH-BB" WHERE pcexpenses = "PEMBERSIH-KANTOR";
+UPDATE pcexpenses SET codeexpense = "PERIZINAN-BB" WHERE pcexpenses = "PERIZINAN-PT";
+UPDATE pctabexpenses SET codeexpense = "PERIZINAN-BB" WHERE pcexpenses = "PERIZINAN-PT";
+UPDATE pcashdetails SET codeexpense = "PERIZINAN-BB" WHERE pcexpenses = "PERIZINAN-PT";
+UPDATE pcexpenses SET codeexpense = "PERSO-TRAINING-BB" WHERE pcexpenses = "PERSONALIA-TRAINING";
+UPDATE pctabexpenses SET codeexpense = "PERSO-TRAINING-BB" WHERE pcexpenses = "PERSONALIA-TRAINING";
+UPDATE pcashdetails SET codeexpense = "PERSO-TRAINING-BB" WHERE pcexpenses = "PERSONALIA-TRAINING";
+UPDATE pcexpenses SET codeexpense = "PERSO-IKLAN-BB" WHERE pcexpenses = "PERSONALIA-IKLAN";
+UPDATE pctabexpenses SET codeexpense = "PERSO-IKLAN-BB" WHERE pcexpenses = "PERSONALIA-IKLAN";
+UPDATE pcashdetails SET codeexpense = "PERSO-IKLAN-BB" WHERE pcexpenses = "PERSONALIA-IKLAN";
+UPDATE pcexpenses SET codeexpense = "PERSO-GATHERING-BB" WHERE pcexpenses = "PERSONALIA-GATHERING";
+UPDATE pctabexpenses SET codeexpense = "PERSO-GATHERING-BB" WHERE pcexpenses = "PERSONALIA-GATHERING";
+UPDATE pcashdetails SET codeexpense = "PERSO-GATHERING-BB" WHERE pcexpenses = "PERSONALIA-GATHERING";
+UPDATE pcexpenses SET codeexpense = "PPH21-BB" WHERE pcexpenses = "PPH21";
+UPDATE pctabexpenses SET codeexpense = "PPH21-BB" WHERE pcexpenses = "PPH21";
+UPDATE pcashdetails SET codeexpense = "PPH21-BB" WHERE pcexpenses = "PPH21";
+UPDATE pcexpenses SET codeexpense = "PPH22-BB" WHERE pcexpenses = "PPH22";
+UPDATE pctabexpenses SET codeexpense = "PPH22-BB" WHERE pcexpenses = "PPH22";
+UPDATE pcashdetails SET codeexpense = "PPH22-BB" WHERE pcexpenses = "PPH22";
+UPDATE pcexpenses SET codeexpense = "PPH23-BB" WHERE pcexpenses = "PPH23";
+UPDATE pctabexpenses SET codeexpense = "PPH23-BB" WHERE pcexpenses = "PPH23";
+UPDATE pcashdetails SET codeexpense = "PPH23-BB" WHERE pcexpenses = "PPH23";
+
+UPDATE pcexpenses SET codeexpense = "PPH25-BB" WHERE pcexpenses = "PPH25";
+UPDATE pctabexpenses SET codeexpense = "PPH25-BB" WHERE pcexpenses = "PPH25";
+UPDATE pcashdetails SET codeexpense = "PPH25-BB" WHERE pcexpenses = "PPH25";
+
+UPDATE pcexpenses SET codeexpense = "PPH4-BB" WHERE pcexpenses = "PPH4";
+UPDATE pctabexpenses SET codeexpense = "PPH4-BB" WHERE pcexpenses = "PPH4";
+UPDATE pcashdetails SET codeexpense = "PPH4-BB" WHERE pcexpenses = "PPH4";
+
+UPDATE pcexpenses SET codeexpense = "PPN-BB" WHERE pcexpenses = "PPN";
+UPDATE pctabexpenses SET codeexpense = "PPN-BB" WHERE pcexpenses = "PPN";
+UPDATE pcashdetails SET codeexpense = "PPN-BB" WHERE pcexpenses = "PPN";
+
+UPDATE pcexpenses SET codeexpense = "RENOVASI-KANTOR-BB" WHERE pcexpenses = "RENOVASI-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "RENOVASI-KANTOR-BB" WHERE pcexpenses = "RENOVASI-KANTOR";
+UPDATE pcashdetails SET codeexpense = "RENOVASI-KANTOR-BB" WHERE pcexpenses = "RENOVASI-KANTOR";
+
+UPDATE pcexpenses SET codeexpense = "RENT-VEHICLE-BB" WHERE pcexpenses = "RENT-PICKUP-TAXI";
+UPDATE pctabexpenses SET codeexpense = "RENT-VEHICLE-BB" WHERE pcexpenses = "RENT-PICKUP-TAXI";
+UPDATE pcashdetails SET codeexpense = "RENT-VEHICLE-BB" WHERE pcexpenses = "RENT-PICKUP-TAXI";
+
+UPDATE pcexpenses SET codeexpense = "SAMPLES-NO-ERP-BB" WHERE pcexpenses = "SAMPLES-NO-WEBERP";
+UPDATE pctabexpenses SET codeexpense = "SAMPLES-NO-ERP-BB" WHERE pcexpenses = "SAMPLES-NO-WEBERP";
+UPDATE pcashdetails SET codeexpense = "SAMPLES-NO-ERP-BB" WHERE pcexpenses = "SAMPLES-NO-WEBERP";
+
+UPDATE pcexpenses SET codeexpense = "SERVIS-STOCK-BB" WHERE pcexpenses = "SERVIS-STOCK";
+UPDATE pctabexpenses SET codeexpense = "SERVIS-STOCK-BB" WHERE pcexpenses = "SERVIS-STOCK";
+UPDATE pcashdetails SET codeexpense = "SERVIS-STOCK-BB" WHERE pcexpenses = "SERVIS-STOCK";
+
+UPDATE pcexpenses SET codeexpense = "TELPON-KANTOR-BB" WHERE pcexpenses = "TELPON-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "TELPON-KANTOR-BB" WHERE pcexpenses = "TELPON-KANTOR";
+UPDATE pcashdetails SET codeexpense = "TELPON-KANTOR-BB" WHERE pcexpenses = "TELPON-KANTOR";
+
+UPDATE pcexpenses SET codeexpense = "TRIP-INDO-BB" WHERE pcexpenses = "TRIP-INDONESIA";
+UPDATE pctabexpenses SET codeexpense = "TRIP-INDO-BB" WHERE pcexpenses = "TRIP-INDONESIA";
+UPDATE pcashdetails SET codeexpense = "TRIP-INDO-BB" WHERE pcexpenses = "TRIP-INDONESIA";
+
+UPDATE pcexpenses SET codeexpense = "TRIP-OUT-INDO-BB" WHERE pcexpenses = "TRIP-OUT-INDONESIA";
+UPDATE pctabexpenses SET codeexpense = "TRIP-OUT-INDO-BB" WHERE pcexpenses = "TRIP-OUT-INDONESIA";
+UPDATE pcashdetails SET codeexpense = "TRIP-OUT-INDO-BB" WHERE pcexpenses = "TRIP-OUT-INDONESIA";
+
+UPDATE pcexpenses SET codeexpense = "WATER-PDAM-BB" WHERE pcexpenses = "WATER-PDAM-KANTOR";
+UPDATE pctabexpenses SET codeexpense = "WATER-PDAM-BB" WHERE pcexpenses = "WATER-PDAM-KANTOR";
+UPDATE pcashdetails SET codeexpense = "WATER-PDAM-BB" WHERE pcexpenses = "WATER-PDAM-KANTOR";
+
+SET FOREIGN_KEY_CHECKS=1;
+
+INSERT INTO `pcexpenses` (`codeexpense`, `description`, `glaccount`, `tag`, `klretentionpph23`) VALUES
+('IMPORT-DUTIES-ADU', 'Import Duties (Bea Cukai) PT ADU', '612011940AD', 1, '0.00'),
+('INTERNET-KANTOR-ADU', 'Internet for kantor PT ADU', '614011100AD', 1, '0.00'),
+('JAMSOSTEK-ADU', 'Jamsostek payments PT ADU', '611011700AD', 0, '0.00'),
+('JASA-SERVICE-2%-ADU', 'Payments for services with 2% PPH23 PT ADU', '614012000AD', 0, '2.00'),
+('JASA-SERVICE-4%-ADU', 'Payments for services with 4% PPH23 PT ADU', '614012000AD', 0, '4.00'),
+('KOMPUTER-HW-ADU', 'Small Hardware, IT accessories PT ADU', '614012050AD', 1, '0.00'),
+('KOMPUTER-SERVIS-ADU', 'Service computer, software, printer, etc PT ADU', '613011200AD', 0, '4.00'),
+('KOMPUTER-WEB-ADU', 'Web services payments PT ADU', '614011200AD', 1, '0.00'),
+('KONSUL-PAJAK-ADU', 'Payment for consultant Pajak PT ADU', '614011800AD', 0, '0.00'),
+('MAINT-KANTOR-ADU', 'Maintenance Kantor PT ADU', '614012000AD', 1, '0.00'),
+('MAINT-MOBIL-SE-ADU', 'Maintenance Sepeda Motor - Mobil PT ADU', '614012850AD', 1, '0.00'),
+('OFFICE-MAT-ADU', 'Office material for PT ADU', '614012050AD', 0, '0.00'),
+('PACKAGING-ADU', 'Packaging material for PT ADU', '612011705AD', 0, '0.00'),
+('PARKIR-KANTOR-ADU', 'Payment parkir Kantor PT ADU', '614012900AD', 1, '0.00'),
+('PEMBERSIH-ADU', 'Pembersih Kantor PT ADU', '614011550AD', 1, '0.00'),
+('PERIZINAN-ADU', 'Payment Perizinan PT Angin Dingin Utara', '614012200AD', 0, '0.00'),
+('PPH21-ADU', 'Payment PPH21 PT ADU', '611012000AD', 0, '0.00'),
+('PPH22-ADU', 'Payment PPH22 PT ADU', '611012002AD', 0, '0.00'),
+('PPH23-ADU', 'Payment PPH23 PT ADU', '611012005AD', 0, '0.00'),
+('PPH25-ADU', 'Payment PPH25 PT ADU', '611012010AD', 0, '0.00'),
+('PPH4-ADU', 'Payment PPH4 PT ADU', '611012020AD', 0, '0.00'),
+('PPHFINAL1-IK', 'Payment Penhasilan Final 1% POIK', '800011000IK', 0, '0.00'),
+('PPN-ADU', 'Payment Tunjangan PPN PT ADU', '611012030AD', 0, '0.00'),
+('RENOVASI-KANTOR-ADU', 'Renovasi - Decorasi Kantor PT ADU', '614012550AD', 1, '0.00'),
+('RENT-VEHICLE-ADU', 'Rental of PickUp-Taxi-Car-Motorbike PT ADU', '614012850AD', 0, '0.00'),
+
+('SAL-BENSIN', 'Salaries - Uang Bensin', '611011300PT', 0, '0.00'),
+('SAL-COMMISSION', 'Salaries - Commission - Bonus', '612011200PT', 0, '0.00'),
+('SAL-DED-JAMSOSTEK', 'Salaries - Jamsostek deduction', '611011700PT', 0, '0.00'),
+('SAL-DED-PPH21 ', 'Salaries - PPh21 deduction', '611012000PT', 0, '0.00'),
+('SAL-FIXMONTH', 'Salaries - Fixed salary payable', '611011100PT', 0, '0.00'),
+('SAL-LAINLAIN', 'Salaries - Lain-lain (payment - deductions)', '611011100PT', 0, '0.00'),
+('SAL-MAKAN', 'Salaries - Uang makan', '611011400PT', 0, '0.00'),
+('SAL-ROUNDING', 'Salaries - Rounding of payments', '700111300', 0, '0.00'),
+('SAL-SHIFTS', 'Salaries - Extra Shifts', '611011600PT', 0, '0.00'),
+('SAL-THR', 'Salaries - THR', '611011800PT', 0, '0.00'),
+
+('SAMPLES-NO-ERP-ADU', 'Samples not in stock webERP PT ADU', '612011800AD', 0, '0.00'),
+('SERVIS-STOCK-ADU', 'Service (Maintenance) stock goods PT ADU', '613019000AD', 0, '0.00'),
+('TELPON-KANTOR-ADU', 'Phone payments for PT ADU', '614011100AD', 1, '0.00'),
+('TRIP-INDO-ADU', 'Trip expenses indide Indonesia PT ADU', '614013500AD', 0, '0.00'),
+('TRIP-OUT-INDO-ADU', 'Trip expenses outside Indonesia PT ADU', '614013600AD', 0, '0.00'),
+('WATER-PDAM-ADU', 'Water (PDAM) for PT ADU', '614011510AD', 1, '0.00');
