cb6e103ab0236f5a7618a5fbeee84e7213a8ae90
Pak Ricard <pakricard@gmail.com>
2018-07-01 12:35:21 +0800

    SQL file to fix bug preventing balace sheet to get the proper chek total (manual SQL)

---
+++ sql/Fix Balance Check Total.sql
@@ -0,0 +1,17 @@
+INSERT INTO `gltrans` 
+	(`counterindex`, `type`, `typeno`, `chequeno`, `trandate`, `periodno`, `account`, `narrative`, `amount`, `posted`, `jobref`, `tag`) 
+VALUES 
+	(NULL, 
+	'0', 
+	'19016', 
+	'0', 
+	'2018-06-30', 
+	'0', 
+	'350510100', 
+	'Adjustment', 
+	'-61726167', 
+	'0', 
+	'', 
+	'0');
+	
+/* ONLY CHANGE DATE AND CHECK TOTAL WITH OPPOSITE SIGN */
