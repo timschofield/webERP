6136b5a96b564b1add42d2dd3bbe22936f00e476
Phil Daintree <33766934+PhilDaintree@users.noreply.github.com>
2018-07-01 14:14:03 +1200

    Add script to set period of GL transactions where the period has not been set correctly - I found that receipts created from webSHOP were not working correctly and this utility fixes  (#71)
    * New utility to correct periods where gltrans committed with 0 period in error - webSHOP bug
    
    * Change log entry

--- sql/mysql/upgrade4.15-4.15.1.sql
+++ sql/mysql/upgrade4.15-4.15.1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `suppliers` ADD COLUMN `defaultgl` VARCHAR(20) NOT NULL DEFAULT '1' AFTER `url`;
 ALTER TABLE `suppliers` ADD COLUMN `defaultshipper` INT(11) NOT NULL DEFAULT '0' AFTER `url`;
+INSERT INTO `scripts` (`script`, `pagesecurity`, `description`) VALUES ('Z_FixGLTransPeriods', '15', 'Fixes periods where GL transactions were not created correctly');
