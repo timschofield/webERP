d80abdb257ac4995518104de5aef6843808bc30f
Rafael Chacón <rafael.emilio.chacon@gmail.com>
2018-09-03 09:02:14 -0600

    Add new database version number
    Add the last query to Update the database version number.

--- sql/mysql/upgrade4.15-4.15.1.sql
+++ sql/mysql/upgrade4.15-4.15.1.sql
@@ -1,14 +1,8 @@
 ALTER TABLE `suppliers` ADD COLUMN `defaultgl` VARCHAR(20) NOT NULL DEFAULT '1' AFTER `url`;
 ALTER TABLE `suppliers` ADD COLUMN `defaultshipper` INT(11) NOT NULL DEFAULT '0' AFTER `url`;
 INSERT INTO `scripts` (`script`, `pagesecurity`, `description`) VALUES ('Z_FixGLTransPeriods', '15', 'Fixes periods where GL transactions were not created correctly');
-INSERT INTO `config` (`confname`, `confvalue`) VALUES ('LoginBackground', 'Login window background file');
 
 
-
-
-
-
--- THIS IS THE LAST SQL query --
--- Updates database version number:
+-- THIS IS THE LAST SQL QUERY. Updates database version number:
 UPDATE config SET confvalue='4.15.1' WHERE confname='VersionNumber';
 
