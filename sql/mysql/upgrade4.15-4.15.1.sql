8d94b89dc70b206b82281e702b89063b78189639
Phil Daintree <33766934+PhilDaintree@users.noreply.github.com>
2018-12-01 21:55:32 +1300

    SQL to change date and datetime field defaults (#114)
    * David Shaw: Alter default dates in all date and datetime fields

--- sql/mysql/upgrade4.15-4.15.1.sql
+++ sql/mysql/upgrade4.15-4.15.1.sql
@@ -67,7 +67,63 @@ CREATE TABLE `timesheets` (
   CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`employeeid`) REFERENCES `employees` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+-- change date defaults to acceptable default - could also use CURRENT_TIMESTAMP ??
+ALTER TABLE `assetmanager` CHANGE `datepurchased` `datepurchased` DATE NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE audittrail CHANGE `transactiondate` `transactiondate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE banktrans CHANGE `transdate` `transdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE bom CHANGE `effectiveafter` `effectiveafter`  date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE contracts CHANGE `requireddate` `requireddate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE custallocns CHANGE `datealloc` `datealloc` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE custnotes CHANGE `date` `date` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE debtorsmaster CHANGE `clientsince` `clientsince` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE debtortrans CHANGE `trandate` `trandate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE debtortypenotes CHANGE `date` `date` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE deliverynotes   CHANGE `deliverydate` `deliverydate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE fixedassets CHANGE `datepurchased` `datepurchased` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE fixedassets CHANGE `disposaldate` `disposaldate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE gltrans CHANGE `trandate` `trandate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE grns CHANGE `deliverydate` `deliverydate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE loctransfers CHANGE `shipdate` `shipdate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE loctransfers CHANGE `recdate` `recdate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE mrpdemands CHANGE `duedate` `duedate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE offers CHANGE `expirydate` `expirydate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE periods CHANGE `lastdate_in_period` `lastdate_in_period` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pickinglists CHANGE `pickinglistdate` `pickinglistdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pickinglists CHANGE `dateprinted` `dateprinted` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pickinglists CHANGE `deliverynotedate` `deliverynotedate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pickreq CHANGE `initdate` `initdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pickreq CHANGE `requestdate` `requestdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pickreq CHANGE `shipdate` `shipdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE pricematrix CHANGE `startdate` `startdate`  date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE prices CHANGE `startdate` `startdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE purchorderdetails CHANGE `deliverydate` `deliverydate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE purchorders CHANGE `orddate` `orddate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE purchorders CHANGE `revised` `revised` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE purchorders CHANGE `deliverydate` `deliverydate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE qasamples CHANGE `sampledate` `sampledate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE recurringsalesorders CHANGE `orddate` `orddate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE recurringsalesorders CHANGE `lastrecurrence` `lastrecurrence` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE recurringsalesorders CHANGE `stopdate` `stopdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE salesorderdetails CHANGE `actualdispatchdate` `actualdispatchdate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE salesorders CHANGE `orddate` `orddate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE salesorders CHANGE `deliverydate` `deliverydate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE salesorders CHANGE `confirmeddate` `confirmeddate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE salesorders CHANGE `datepackingslipprinted` `datepackingslipprinted` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE salesorders CHANGE `quotedate` `quotedate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE sampleresults CHANGE `testdate` `testdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE shipments CHANGE `eta` `eta` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE stockcheckfreeze CHANGE `stockcheckdate` `stockcheckdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE stockmaster CHANGE `lastcostupdate` `lastcostupdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE stockmoves CHANGE `trandate` `trandate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE stockrequest CHANGE `despatchdate` `despatchdate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE stockserialitems CHANGE `expirationdate` `expirationdate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE suppallocs CHANGE `datealloc` `datealloc` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE suppliers CHANGE `suppliersince` `suppliersince` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE supptrans CHANGE `trandate` `trandate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE supptrans CHANGE `duedate` `duedate` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE tenders CHANGE `requiredbydate` `requiredbydate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE workorders CHANGE `requiredby` `requiredby` date NOT NULL DEFAULT '1000-01-01';
+ALTER TABLE workorders CHANGE `startdate` `startdate` date NOT NULL DEFAULT '1000-01-01';
 
 -- THIS IS THE LAST SQL QUERY. Updates database version number:
 UPDATE config SET confvalue='4.15.1' WHERE confname='VersionNumber';
