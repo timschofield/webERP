4c551c5e2350ee73865ffb1e015371940bc5e0f9
Tim Schofield <tim@weberpafrica.com>
2019-05-04 17:11:16 +0100

    Make sure that a negative credit cannot be applied.
    Files changed in commit:
    Credit_Invoice.php
    On branch master
    
    per HDeriauFF on:
    http://www.weberp.org/forum/showthread.php?tid=8339&pid=15699#pid15699

--- Credit_Invoice.php
+++ Credit_Invoice.php
@@ -202,7 +202,7 @@ if (isset($_POST['Location'])) {
 }
 
 if (isset($_POST['ChargeFreightCost'])) {
-	$_SESSION['CreditItems' . $identifier]->FreightCost = filter_number_format($_POST['ChargeFreightCost']);
+	$_SESSION['CreditItems' . $identifier]->FreightCost = abs(filter_number_format($_POST['ChargeFreightCost']));
 	if (($TotalQtyCredited + abs($_POST['ChargeFreightCost'])) <= 0) {
 		prnMsg(_('There are no item quantity or freight charge input'), 'error');
 		if (isset($_POST['ProcessCredit'])) {
