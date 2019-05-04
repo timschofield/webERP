c04787c327a3c85c5075f4253eb3f27cd7e3aaf9
Tim Schofield <tim@weberpafrica.com>
2019-05-04 08:47:58 +0100

    Enable the shipping to be reduced to zero in case where the original invoice included a value for shipping costs.
    Files changed in commit:
    Credit_Invoice.php
    On branch master
    
    http://www.weberp.org/forum/showthread.php?tid=8339&pid=15697#pid15697

--- Credit_Invoice.php
+++ Credit_Invoice.php
@@ -201,7 +201,7 @@ if (isset($_POST['Location'])) {
 	}
 }
 
-if (isset($_POST['ChargeFreightCost']) and abs($_POST['ChargeFreightCost']) > 0) {
+if (isset($_POST['ChargeFreightCost'])) {
 	$_SESSION['CreditItems' . $identifier]->FreightCost = filter_number_format($_POST['ChargeFreightCost']);
 	if (($TotalQtyCredited + abs($_POST['ChargeFreightCost'])) <= 0) {
 		prnMsg(_('There are no item quantity or freight charge input'), 'error');
