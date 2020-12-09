39229ff2ca21fa2a346ce15f74c1c3e0a941de8d
PakRicard <pakricard@gmail.com>
2020-12-09 11:05:25 +0800

    New DB credentials for OC 2.3.0.2

--- config_OpenCart-TEST.php
+++ config_OpenCart-TEST.php
@@ -7,10 +7,10 @@ $Version = '2.3'; //Connection to DB OpenCart v2.3.0.2
 
 $Language ='en_GB';
 
-$opencart_db_user = 'kurakura_klv2';
-$opencart_db_pwd = 'IT2016it!!';
+$opencart_db_user = 'DBU_kl_shop';
+$opencart_db_pwd = '2e549bf390a028a9fRR55.2afd';
 $opencart_db_host = 'localhost';
-$opencart_db_name = 'kl_shop_oc3';
+$opencart_db_name = 'kl_shop_oc_23';
 $opencart_db_tableprefix ='oc_';
 
 
