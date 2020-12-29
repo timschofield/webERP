defdc4c5e98072412b72f9a808d7ffa9459bc7d0
PakRicard <pakricard@gmail.com>
2020-12-29 09:31:40 +0800

    Link to the right DB's

--- config_OpenCart-TEST.php
+++ config_OpenCart-TEST.php
@@ -3,15 +3,14 @@
 /*Configuration file for webERP-OpenCart Connector 
 Edit all this file to provide the information for the connector to communicate with webERP and OpenCart */
 
-$Version = '2.3'; //Connection to DB OpenCart v2.3.0.2
+$Version = '3.0'; //Connection to DB OpenCart v 3.0.3.2
 
 $Language ='en_GB';
 
 $opencart_db_user = 'DBU_kl_shop';
 $opencart_db_pwd = '2e549bf390a028a9fRR55.2afd';
 $opencart_db_host = 'localhost';
-$opencart_db_name = 'kl_shop_oc_23';
+$opencart_db_name = 'kl_shop_oc_30';
 $opencart_db_tableprefix ='oc_';
-
 
 ?>
