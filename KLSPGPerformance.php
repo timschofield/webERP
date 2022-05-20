5d242972c0c892259c88b192f39398a403e3c9a4
PakRicard <pakricard@gmail.com>
2022-05-20 13:58:07 +0800

    Added Monthy SPG performance report, based on weekly report

--- KLSPGPerformance.php
+++ KLSPGPerformance.php
@@ -21,6 +21,10 @@ if ($KL_SystemAdmin
 	OR $KL_OperationalManager 
 	OR $KL_BusinessDevelopmentManager 
 	OR $KL_ShopManager){
+
+	SPGPerformanceMonthy($db);
+	$NumberOfTestExecuted++;
+
 	SPGPerformanceWeekly($db);
 	$NumberOfTestExecuted++;
 
@@ -45,9 +49,8 @@ if ($KL_SystemAdmin
 	OR $KL_OperationalManager
 	OR $KL_ShopManager
 	OR $KL_BusinessDevelopmentManager){	
-// RICARD 2019-11-26
-// CANCELLED FOR PERFORMANCE REASONS
-//	RetailTypePayments("SPG",180, $db);
+
+	RetailTypePayments("SPG",90, $db);
 	$NumberOfTestExecuted++;
 // RICARD 2019-11-26
 // CANCELLED FOR PERFORMANCE REASONS
@@ -320,6 +323,287 @@ function SPGPerformanceByShop($Shop, $NumDaysA, $NumDaysB, $NumDaysC, $db){
 					locale_number_format_zero_blank($myrow['daysC'],0),
 					locale_number_format_zero_blank($myrow['salesC']/$myrow['daysC'],0)
 					);
+		}
+		echo '</table>
+				</div>';
+	}
+}
+
+function SPGPerformanceMonthy($db){
+
+	$YesterdayA  = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-1));
+	$Last30A      = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-30));
+	$Last60A     = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-60));
+	$StartMonthA = substr($YesterdayA,0,7). '-01';	
+
+	$YesterdayB  = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-31));
+	$Last30B      = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-60));
+	$Last60B     = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-90));
+	$StartMonthB = substr($YesterdayB,0,7). '-01';	
+
+	$YesterdayC  = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-61));
+	$Last30C      = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-90));
+	$Last60C     = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-120));
+	$StartMonthC = substr($YesterdayC,0,7). '-01';	
+	
+	$YesterdayD  = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-91));
+	$Last30D      = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-120));
+	$Last60D     = FormatDateForSQL(DateAdd(Date($_SESSION['DefaultDateFormat']),'d',-150));
+	$StartMonthD = substr($YesterdayD,0,7). '-01';	
+
+	$SQL = "SELECT locations.zone,
+					locations.loccode,
+					salesman.salesmancode,
+					salesman.salesmanname,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last30A . "'
+							AND salesorders.orddate <= '". $YesterdayA . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days30A,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last30A . "'
+							AND salesorders.orddate <= '" . $YesterdayA . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last30A,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last60A . "'
+							AND salesorders.orddate <= '". $YesterdayA . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days60A,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last60A . "'
+							AND salesorders.orddate <= '" . $YesterdayA . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last60A,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $StartMonthA . "'
+							AND salesorders.orddate <= '" . $YesterdayA . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS mtdA,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last30B . "'
+							AND salesorders.orddate <= '". $YesterdayB . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days30B,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last30B . "'
+							AND salesorders.orddate <= '" . $YesterdayB . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last30B,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last60B . "'
+							AND salesorders.orddate <= '". $YesterdayB . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days60B,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last60B . "'
+							AND salesorders.orddate <= '" . $YesterdayB . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last60B,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $StartMonthB . "'
+							AND salesorders.orddate <= '" . $YesterdayB . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS mtdB,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last30C . "'
+							AND salesorders.orddate <= '". $YesterdayC . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days30C,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last30C . "'
+							AND salesorders.orddate <= '" . $YesterdayC . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last30C,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last60C . "'
+							AND salesorders.orddate <= '". $YesterdayC . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days60C,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last60C . "'
+							AND salesorders.orddate <= '" . $YesterdayC . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last60C,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $StartMonthC . "'
+							AND salesorders.orddate <= '" . $YesterdayC . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS mtdC,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last30D . "'
+							AND salesorders.orddate <= '". $YesterdayD . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days30D,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last30D . "'
+							AND salesorders.orddate <= '" . $YesterdayD . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last30D,
+					(SELECT COUNT(DISTINCT(salesorders.orddate))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '". $Last60D . "'
+							AND salesorders.orddate <= '". $YesterdayD . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS days60D,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last60D . "'
+							AND salesorders.orddate <= '" . $YesterdayD . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS last60D,
+					(SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+						FROM salesorderdetails, salesorders
+						WHERE salesorderdetails.orderno = salesorders.orderno
+							AND salesorderdetails.completed = 1
+							AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $StartMonthD . "'
+							AND salesorders.orddate <= '" . $YesterdayD . "'
+							AND salesorders.salesperson = salesman.salesmancode) AS mtdD
+			FROM locations, www_users, salesman
+			WHERE locations.loccode = www_users.defaultlocation 
+				AND www_users.salesman = salesman.salesmancode
+				AND ((SELECT SUM(qtyinvoiced * (unitprice * (1 - discountpercent)))
+								FROM salesorderdetails, salesorders
+								WHERE salesorderdetails.orderno = salesorders.orderno
+									AND salesorderdetails.completed = 1
+									AND salesorders.fromstkloc = locations.loccode
+							AND salesorders.orddate >= '" . $Last60A . "'
+							AND salesorders.orddate <= '" . $YesterdayA . "'
+									AND salesorders.salesperson = salesman.salesmancode) > 0)
+			ORDER BY locations.zone,
+				locations.loccode,
+				salesman.salesmancode";
+
+	$result = DB_query($SQL);
+	if (DB_num_rows($result) != 0){
+		echo '<p class="page_title_text" align="center"><strong>' . "SPG Monthly performance". '</strong></p>';
+		echo '<div>';
+		echo '<table class="selection">';
+		$TableHeader = '<tr>
+							<th colspan="4">' . 'SPG' . '</th>
+							<th colspan="3">' . ConvertSQLDate($YesterdayD) . '</th>
+							<th colspan="3">' . ConvertSQLDate($YesterdayC) . '</th>
+							<th colspan="3">' . ConvertSQLDate($YesterdayB) . '</th>
+							<th colspan="3">' . ConvertSQLDate($YesterdayA) . '</th>
+						</tr>';
+		echo $TableHeader;
+		
+		$TableHeader = '<tr>
+							<th>' . 'Zone' . '</th>
+							<th>' . 'Shop' . '</th>
+							<th>' . 'SPG' . '</th>
+							<th>' . 'Name' . '</th>
+							<th>' . 'MTD' . '</th>
+							<th>' . 'Last 30d' . '</th>
+							<th>' . 'last 60d' . '</th>
+							<th>' . 'MTD' . '</th>
+							<th>' . 'Last 30d' . '</th>
+							<th>' . 'last 60d' . '</th>
+							<th>' . 'MTD' . '</th>
+							<th>' . 'Last 30d' . '</th>
+							<th>' . 'last 60d' . '</th>
+							<th>' . 'MTD' . '</th>
+							<th>' . 'Last 30d' . '</th>
+							<th>' . 'last 60d' . '</th>
+						</tr>';
+		$k = 0; //row colour counter
+		$lastshop = $myrow['loccode'];
+		while ($myrow = DB_fetch_array($result)) {
+			$k = StartEvenOrOddRow($k);
+			if ($lastshop != $myrow['loccode']){
+				echo $TableHeader;
+			}
+			printf('<td>%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					</tr>', 
+					$myrow['zone'],
+					$myrow['loccode'],
+					$myrow['salesmancode'],
+					$myrow['salesmanname'],
+					locale_number_format_zero_blank($myrow['mtdD'],0),
+					locale_number_format_zero_blank($myrow['last30D']/$myrow['days30D'],0),
+					locale_number_format_zero_blank($myrow['last60D']/$myrow['days60D'],0),
+					locale_number_format_zero_blank($myrow['mtdC'],0),
+					locale_number_format_zero_blank($myrow['last30C']/$myrow['days30C'],0),
+					locale_number_format_zero_blank($myrow['last60C']/$myrow['days60C'],0),
+					locale_number_format_zero_blank($myrow['mtdB'],0),
+					locale_number_format_zero_blank($myrow['last30B']/$myrow['days30B'],0),
+					locale_number_format_zero_blank($myrow['last60B']/$myrow['days60B'],0),
+					locale_number_format_zero_blank($myrow['mtdA'],0),
+					locale_number_format_zero_blank($myrow['last30A']/$myrow['days30A'],0),
+					locale_number_format_zero_blank($myrow['last60A']/$myrow['days60A'],0)
+					);
+			$lastshop = $myrow['loccode'];
 		}
 		echo '</table>
 				</div>';
