3fb1b3cf8c7bb5679bb07d94affacedb75374912
PakRicard <pakricard@gmail.com>
2022-05-26 11:41:43 +0800

    Revert "Cicik as urchasing LEader + Shop Support roles in control and performance boards"
    This reverts commit 85518adc10942afb2409b072a74b2eef9da7d29d.

--- includes/KLRoles.php
+++ includes/KLRoles.php
@@ -19,11 +19,11 @@ $KL_ITSupport = FALSE;
 
 if ($_SESSION['AccessLevel'] == 8){ // webERP System Administration (R)
 	$KL_SystemAdmin = TRUE;
-}else if ($_SESSION['AccessLevel'] == 14){ // Operational Manager (Ike)
+}else if ($_SESSION['AccessLevel'] == 14){ // Operational Manager
 	$KL_OperationalManager = TRUE;
 }else if ($_SESSION['AccessLevel'] == 26){ // Shop Support Team
 	$KL_ShopSupportTeam = TRUE;
-}else if ($_SESSION['AccessLevel'] == 23){ // Shop Support Leader (ria)
+}else if ($_SESSION['AccessLevel'] == 23){ // Shop Support Leader
 	$KL_ShopSupportTeam = TRUE;
 	$KL_ShopSupportLeader = TRUE;
 }else if ($_SESSION['AccessLevel'] == 15){ // PettyCash Only User (courier, etc...)
@@ -35,15 +35,14 @@ if ($_SESSION['AccessLevel'] == 8){ // webERP System Administration (R)
 	$KL_SPGSeniorOrSupport = TRUE;
 }else if ($_SESSION['AccessLevel'] == 22){ // Sales Team SPG Support
 	$KL_SPGJunior = TRUE;
-}else if ($_SESSION['AccessLevel'] == 19){ // Administration Leader (Revi)
+}else if ($_SESSION['AccessLevel'] == 19){ // Administration Leader
 	$KL_AdministrationTeam = TRUE;
 }else if ($_SESSION['AccessLevel'] == 27){ // Administration Team 
 	$KL_AdministrationTeam = TRUE;
 }else if ($_SESSION['AccessLevel'] == 21){ // Business Development manager (L)
 	$KL_BusinessDevelopmentManager = TRUE;
-}else if ($_SESSION['AccessLevel'] == 24){ // Purchasing Leader (Cicik). Added as Shop Support Team too, as she has 2 roles, from COVID
+}else if ($_SESSION['AccessLevel'] == 24){ // Purchasing Leader
 	$KL_PurchasingTeam = TRUE;
-	$KL_ShopSupportTeam = TRUE;             
 }else if ($_SESSION['AccessLevel'] == 28){ // Purchasing Team
 	$KL_PurchasingTeam = TRUE;
 }else if ($_SESSION['AccessLevel'] == 29){ // IT Hostgator Support (as much rights as possible so they run long scripts
@@ -52,7 +51,7 @@ if ($_SESSION['AccessLevel'] == 8){ // webERP System Administration (R)
 	$KL_ShopManager = TRUE;
 }else if ($_SESSION['AccessLevel'] == 32){ // It Support
 	$KL_ITSupport = TRUE;
-}else if ($_SESSION['AccessLevel'] == 36){ // Sales Team Online (Ceria)
+}else if ($_SESSION['AccessLevel'] == 36){ // Sales Team Online
 	$KL_SalesTeamOnline = TRUE;
 }
 
