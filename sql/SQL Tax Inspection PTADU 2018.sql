f60761c3cec5093af929a6bc6c4426ac11d7acb4
PakRicard <pakricard@gmail.com>
2022-08-22 19:05:59 +0800

    More on tax inspection

--- sql/SQL Tax Inspection PTADU 2018.sql
+++ sql/SQL Tax Inspection PTADU 2018.sql
@@ -17,8 +17,8 @@ SELECT bom.parent,
 						AND workorders.requiredby <= "2018-12-31"
 						)
 	AS qty_component_2018
-FROM stockmaster INNER JOIN bom
-ON stockmaster.stockid=bom.component
+FROM stockmaster 
+INNER JOIN bom ON stockmaster.stockid=bom.component
 WHERE bom.parent IN (SELECT woitems.stockid
 						FROM woitems, workorders
 						WHERE woitems.wo = workorders.wo
@@ -29,9 +29,32 @@ ORDER BY bom.parent,
 		bom.component
 		
 		
-(SELECT * 
-FROM woitems, workorders
+SELECT stockmaster.stockid,
+	stockmaster.description,
+	purchdata.suppliers_partno,
+	purchdata.suppliersuom
+FROM woitems, workorders, stockmaster, purchdata
 WHERE woitems.wo = workorders.wo
+	AND woitems.stockid = stockmaster.stockid
+	AND purchdata.stockid = stockmaster.stockid
 	AND workorders.requiredby >= "2018-01-01"
 	AND workorders.requiredby <= "2018-12-31"
-	)
+GROUP BY stockmaster.stockid, purchdata.suppliers_partno
+ORDER BY stockmaster.stockid,purchdata.suppliers_partno
+
+
+SELECT bom.component,
+	stockmaster.description,
+	purchdata.suppliers_partno,
+	purchdata.suppliersuom
+FROM stockmaster 
+INNER JOIN bom ON stockmaster.stockid=bom.component
+INNER JOIN purchdata ON bom.component=purchdata.stockid
+WHERE bom.parent IN (SELECT woitems.stockid
+						FROM woitems, workorders
+						WHERE woitems.wo = workorders.wo
+							AND workorders.requiredby >= "2018-01-01"
+							AND workorders.requiredby <= "2018-12-31"
+						)
+GROUP BY bom.component
+ORDER BY bom.component
