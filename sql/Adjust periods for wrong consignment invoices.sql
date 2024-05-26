41412820086034db28606ee2d7eba2ec3f8facc6
PakRicard <pakricard@gmail.com>
2024-05-26 10:43:04 +0800

    Adjust periods for wrong consignment invoices

---
+++ sql/Adjust periods for wrong consignment invoices.sql
@@ -0,0 +1,26 @@
+SELECT * 
+FROM gltrans
+WHERE periodno >= 178
+	AND trandate <= "2023-12-31";
+	
+
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 165 AND trandate <= "2022-11-30";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 166 AND trandate <= "2022-12-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 167 AND trandate <= "2023-01-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 168 AND trandate <= "2023-02-29";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 169 AND trandate <= "2023-03-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 170 AND trandate <= "2023-04-30";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 171 AND trandate <= "2023-05-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 172 AND trandate <= "2023-06-30";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 173 AND trandate <= "2023-07-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 174 AND trandate <= "2023-08-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 175 AND trandate <= "2023-09-30";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 176 AND trandate <= "2023-10-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 177 AND trandate <= "2023-11-30";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 178 AND trandate <= "2023-12-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 179 AND trandate <= "2024-01-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 180 AND trandate <= "2024-02-29";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 181 AND trandate <= "2024-03-31";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 182 AND trandate <= "2024-04-30";
+UPDATE gltrans SET periodno = periodno - 1 WHERE periodno = 183 AND trandate <= "2024-05-31";
+	
