d7d64cb26605c8b36f1b852f429c37dc3c1e5f15
Tim Schofield <tim.schofield1960@googlemail.com>
2024-06-20 21:30:55 +0100

    Improvements to new login screen (#229)
    * PDF Order, Quote, and Invoice formatting.
    Line item narrative now replicates entered formatting like carriage returns and line feeds.
    Our company, deliver to, and company to now use functions to facilitate easier localization.
    
    * New design for the login screen incorporating logos in
    company dropdown list, and JS scrolling, GIF animation for
    waiting, ability to hide/show password
    
    Changes to be committed:
      companies/weberpdemo/Companies.php
      companies/weberpdemo/logo.png
      css/descending.png
      css/eye.png
      css/eyeshut.png
      css/login.css
      css/waiting.gif
      css/weberp.png
      includes/ConnectDB.inc
      includes/Login.php
      includes/session.php
      javascripts/Login.js
    
    * Improvements to new login screen
    Author:    Brian Lloyd <brian9961@protonmail.com>
    Changes to be committed:
       css/ascending.png
       css/login.css
       css/weberpdemo.png
       includes/Login.php
    
    ---------
    
    Co-authored-by: Paul Harness <pharness@d-m-eng.com>
    Co-authored-by: Brian Lloyd <brian9961@protonmail.com>

--- includes/Login.php
+++ includes/Login.php
@@ -12,7 +12,7 @@ if ((isset($AllowDemoMode)) and ($AllowDemoMode == True) and (!isset($demo_text)
 	echo '<!DOCTYPE html>';
 	echo '<html>
 		<head>
-			<title>', $ProjectName, ' ', _('Login screen'), '</title>
+			<title>WebERP ', _('Login screen'), '</title>
 			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<link rel="shortcut icon" href="favicon.ico?v=2" type="image/x-icon" />
