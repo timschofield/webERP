69301f283db1676770483a9e2826d97c504451c0
Tim Schofield <tim.schofield1960@googlemail.com>
2024-09-16 19:07:57 +0100

    Use of deprecated PHP4 style class constructor is not supported since PHP 7 Changed for PHP8 compatibility  Changes to be committed: 	modified:   includes/RFC822.php

--- includes/RFC822.php
+++ includes/RFC822.php
@@ -105,7 +105,7 @@ class Mail_RFC822
      *
      * @return object Mail_RFC822 A new Mail_RFC822 object.
      */
-    function Mail_RFC822($address = null, $default_domain = null, $nest_groups = null, $validate = null, $limit = null)
+    function __construct($address = null, $default_domain = null, $nest_groups = null, $validate = null, $limit = null)
     {
         if (isset($address))        $this->address        = $address;
         if (isset($default_domain)) $this->default_domain = $default_domain;
