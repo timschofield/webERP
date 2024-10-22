ca77310dd229f1704d41ed3e3b412fea7d9b410f
PakRicard <pakricard@gmail.com>
2024-10-22 17:35:17 +0800

    1st working webClientPrint v6.0 silent printing (Only KLRetailPOSTest proof of concept)

--- includes/SilentPrinting.php
+++ includes/SilentPrinting.php
@@ -1,42 +1,20 @@
 <?php 
-    session_start();
-
-    include 'WebClientPrint.php';
-    use Neodynamic\SDK\Web\WebClientPrint;
+$imageSrc = $RootPath . '/css/' . $Theme . '/images/printer.png';
+$imageTitle = $textActionToPrint;
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>How to directly Print ESCPOS Commands without Preview or Printer Dialog</title>
-</head>
-<body>
-    <!-- Store User's SessionId -->
-    <input type="hidden" id="sid" name="sid" value="<?php echo session_id(); ?>" />
-    
-    <h1>How to directly Print ESCPOS Commands without Preview or Printer Dialog</h1>
-    <label class="checkbox">
-        <input type="checkbox" id="useDefaultPrinter" /> <strong>Use default printer</strong> or...
-    </label>
-    <div id="loadPrinters">
-    <br />
-    WebClientPrint can detect the installed printers in your machine.
-    <br />
-    <input type="button" onclick="javascript:jsWebClientPrint.getPrinters();" value="Load installed printers..." />
-                    
-    <br /><br />
-    </div>
-    <div id="installedPrinters" style="visibility:hidden">
-    <br />
-    <label for="installedPrinterName">Select an installed Printer:</label>
-    <select name="installedPrinterName" id="installedPrinterName"></select>
-    </div>
-            
-    <br /><br />
-    <input type="button" style="font-size:18px" onclick="javascript:jsWebClientPrint.print('useDefaultPrinter=' + $('#useDefaultPrinter').attr('checked') + '&printerName=' + $('#installedPrinterName').val());" value="Print Label..." />
-        
-    <script type="text/javascript">
-        var wcppGetPrintersTimeout_ms = 60000; //60 sec
+<img src="<?php echo $imageSrc; ?>" title="<?php echo $imageTitle; ?>" alt="" />
+<a href="#" onclick="javascript:jsWebClientPrint.print('useDefaultPrinter=1' + '&printerName=' + encodeURIComponent($('#installedPrinterName').val()) + '&identifier=<?php echo urlencode($identifier); ?>');">
+<?= $textActionToPrint; ?>
+</a>
+
+<?php 
+use Neodynamic\SDK\Web\WebClientPrint;
+?>
+
+<script type="text/javascript">
+	
+	var wcppGetPrintersTimeout_ms = 60000; //60 sec
         var wcppGetPrintersTimeoutStep_ms = 500; //0.5 sec
 
         function wcpGetPrintersOnSuccess(){
@@ -65,7 +43,7 @@
     <!-- Add Reference to jQuery at Google CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
 
-    <?php
+	<?php
     //Get Absolute URL of this page
     $currentAbsoluteURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
     $currentAbsoluteURL .= $_SERVER["SERVER_NAME"];
@@ -76,15 +54,11 @@
     $currentAbsoluteURL .= $_SERVER["REQUEST_URI"];
     
     //WebClientPrinController.php is at the same page level as WebClientPrint.php
-    $webClientPrintControllerAbsoluteURL = substr($currentAbsoluteURL, 0, strrpos($currentAbsoluteURL, '/')).'/WebClientPrintController.php';
+    $webClientPrintControllerAbsoluteURL = substr($currentAbsoluteURL, 0, strrpos($currentAbsoluteURL, '/')).'/includes/WebClientPrint/WebClientPrintController.php';
     
     //PrintESCPOSController.php is at the same page level as WebClientPrint.php
-    $printESCPOSControllerAbsoluteURL = substr($currentAbsoluteURL, 0, strrpos($currentAbsoluteURL, '/')).'/PrintESCPOSController.php';
+    $printESCPOSControllerAbsoluteURL = substr($currentAbsoluteURL, 0, strrpos($currentAbsoluteURL, '/')).'/includes/WebClientPrint/PrintESCPOSController.php';
     
     //Specify the ABSOLUTE URL to the WebClientPrintController.php and to the file that will create the ClientPrintJob object
     echo WebClientPrint::createScript($webClientPrintControllerAbsoluteURL, $printESCPOSControllerAbsoluteURL, session_id());
     ?>
-       
-
-</body>
-</html>
