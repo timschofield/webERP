4d8b8c098d0b30e7c1aac035b6d2efc091a3e882
Tim Schofield <tim.schofield1960@googlemail.com>
2024-10-25 21:14:51 +0100

    Correct the Collate type in table files
     Files changed in commit:
     install/tables/jnltmpldetails.sql
     install/tables/jnltmplheader.sql

--- install/tables/jnltmpldetails.sql
+++ install/tables/jnltmpldetails.sql
@@ -6,4 +6,4 @@ CREATE TABLE `jnltmpldetails` (
   `amount` double NOT NULL DEFAULT '0',
   `narrative` varchar(200) NOT NULL DEFAULT '',
   PRIMARY KEY (`templateid`,`linenumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
