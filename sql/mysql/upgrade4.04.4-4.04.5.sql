807bbe2269f55eb9148cc282ee9c23897ed54a7c
PakRicard <pakricard@gmail.com>
2024-11-13 11:21:36 +0800

    Remove files not used

--- sql/mysql/upgrade4.04.4-4.04.5.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `custcontacts` ADD `email` VARCHAR( 55 ) NOT NULL;
-INSERT INTO config (confname, confvalue) VALUES ('WorkingDaysWeek','5');
-INSERT INTO `scripts` (`script` ,`pagesecurity` ,`description`) VALUES ('PDFQuotationPortrait.php', '2', 'Quotation printout in portrait');
-UPDATE config SET confvalue='4.04.5' WHERE confname='VersionNumber'; 
--- sql/mysql/upgrade4.04.5-4.05.sql
+++
@@ -1,1 +0,0 @@
-UPDATE config SET confvalue='4.05' WHERE confname='VersionNumber'; 
