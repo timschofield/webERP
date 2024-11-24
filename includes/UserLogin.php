cbe4120e6d5ff160d27791e59574d90b273a081e
Tim Schofield <tim.schofield1960@googlemail.com>
2024-11-24 08:32:56 +0000

    Strict mode MySql doesn't allow the empty string to be passed as a value In the case where the ECB doesn't maintain a value for that currency then default to 1.
     Files changed in commit:
     includes/UserLogin.php

--- includes/UserLogin.php
+++ includes/UserLogin.php
@@ -205,7 +205,11 @@ function userLogin($Name, $Password, $SysAdminEmail = '') {
 								while ($CurrencyRow = DB_fetch_row($CurrenciesResult)){
 									if ($CurrencyRow[0]!=$_SESSION['CompanyRecord']['currencydefault']){
 
-										$UpdateCurrRateResult = DB_query("UPDATE currencies SET rate='" . GetCurrencyRate($CurrencyRow[0],$CurrencyRates) . "'
+										$Rate = GetCurrencyRate($CurrencyRow[0],$CurrencyRates);
+										if ($Rate == '') {
+											$Rate = 1;
+										}
+										$UpdateCurrRateResult = DB_query("UPDATE currencies SET rate='" . $Rate . "'
 																			WHERE currabrev='" . $CurrencyRow[0] . "'");
 									}
 								}
