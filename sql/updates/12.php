d7ac92b9d920ee816ad2002202379f2dcc8c6a1c
PakRicard <pakricard@gmail.com>
2024-12-16 17:21:55 +0800

    Improves check when error, as reported by Tim in https://github.com/timschofield/webERP/issues/304#issuecomment-2544987003

--- sql/updates/12.php
+++ sql/updates/12.php
@@ -4,7 +4,7 @@ $SQL = "UPDATE pricematrix SET enddate='9999-12-31' WHERE enddate='0000-00-00'";
 $ErrMsg = _('There is a problem setting the default pricematrix enddate to 9999-12-31');
 $Result = DB_query($SQL, $ErrMsg);
 
-if (DB_error_no() == 0) {
+if ($_SESSION['Updates']['Errors'] == 0) {
 	UpdateDBNo(basename(__FILE__, '.php'), _('Default end date for pricematrix set to 9999-12-31'));
 }
 
