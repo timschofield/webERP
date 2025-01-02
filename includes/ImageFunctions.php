6f5301da46a2315ae88f4b43f2086735a4143688
PakRicard <pakricard@gmail.com>
2025-01-02 15:27:20 +0800

    Amends the ameded commit.

--- includes/ImageFunctions.php
+++ includes/ImageFunctions.php
@@ -1,19 +1,18 @@
 <?php
 
 function GetImageLink($ImageFile, $Item, $Width, $Height, $AltText, $Class = "") {
-
     if (extension_loaded('gd') 
 		and function_exists('gd_info') 
 		and file_exists($ImageFile) 
 		and isset($Item) 
 		and !empty($Item)) {
         if ($_SESSION['ShowStockidOnImages'] == '0'){
-            $ImageLink = '<img src="GetStockImage.php?automake=1' . 
+			$ImageLink = '<img src="GetStockImage.php?automake=1' . 
                                                     '&textcolor=FFFFFF' .
                                                     '&bgcolor=CCCCCC' .
                                                     '&amp;width=' . $Width . 
                                                     '&amp;height=' . $Height .
-                                                    '&$Item=' . urlencode($Item) .
+                                                    '&amp;StockID=' . urlencode($Item) .
                                                     '" alt="" />';
         } else {
             $ImageLink = '<img src="GetStockImage.php?automake=1' . 
@@ -22,9 +21,9 @@ function GetImageLink($ImageFile, $Item, $Width, $Height, $AltText, $Class = "")
                                                     '&amp;text='. $Item .
                                                     '&amp;width=' . $Width . 
                                                     '&amp;height=' . $Height .
-                                                    '&$Item=' . urlencode($Item) .
+                                                    '&amp;StockID=' . urlencode($Item) .
                                                     '" alt="" />';
-}
+		}
     } else if (file_exists($ImageFile)) {
         $ImageLink = '<img class="StockImage" src="' . $ImageFile . '" />';
     } else {
