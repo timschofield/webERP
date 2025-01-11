37172a70efad3ff2508eeec666237bead578c7b8
PakRicard <pakricard@gmail.com>
2025-01-11 17:02:28 +0800

    Update KL ADU Version for development

--- KLConfig.php
+++ KLConfig.php
@@ -6,7 +6,7 @@
 *
 *******************************************************************************/
 /* KL RICARD Configuration file for specific KL code */
-$KLCodeVersion = "011";
+$KLCodeVersion = "012";
 
 // let's setup all the variables depending on the environment
 if (strpos(strtoupper($_SERVER['HTTP_HOST']),"LOCAL-TEST")!== false){
