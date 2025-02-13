4e6a4f6f650722b0fb4b472793bad73ca1ff95a8
Tim Schofield <tim.schofield1960@googlemail.com>
2025-02-13 11:27:31 +0000

    Exit more gracefully when in demo mode
     Files changed in commit:
     WWW_Users.php

--- WWW_Users.php
+++ WWW_Users.php
@@ -26,6 +26,7 @@ echo '<p class="page_title_text"><img alt="" src="', $RootPath, '/css/', $Theme,
 
 if($AllowDemoMode) {
 	prnMsg(_('The the system is in demo mode and the security model administration is disabled'), 'warn');
+	include('includes/footer.php');
 	exit;
 }
 $ModuleList = array(
