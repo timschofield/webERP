a1467a7d980dcff145cd4a8b51168404c5e1d822
PakRicard <pakricard@gmail.com>
2025-02-24 17:39:21 +0800

    SQL tests  for items in location X for more than Y days

---
+++ KLsql/items in location X for more than Y days.sql
@@ -0,0 +1,19 @@
+SELECT locstock.stockid
+,	locstock.quantity
+FROM locstock
+WHERE locstock.quantity > 0
+	AND locstock.loccode = "KANTO"
+	AND EXISTS (SELECT *
+				FROM stockmoves
+				WHERE locstock.stockid = stockmoves.stockid
+					AND locstock.loccode = stockmoves.loccode
+					AND stockmoves.trandate >= "2025-02-01"
+					AND stockmoves.qty > 0
+				)
+	AND NOT EXISTS (SELECT *
+					FROM stockmoves
+					WHERE locstock.stockid = stockmoves.stockid
+						AND locstock.loccode = stockmoves.loccode
+					AND stockmoves.trandate >= "2025-02-01"
+						AND stockmoves.qty < 0
+					)
