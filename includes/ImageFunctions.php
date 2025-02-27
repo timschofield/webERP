5c9220c237384543e5f22158466e79265622cc85
Tim Schofield <tim.schofield1960@googlemail.com>
2025-02-27 19:47:24 +0000

    Add css class to the <img elements
    Files changed in commit:
    includes/ImageFunctions.php

--- includes/ImageFunctions.php
+++ includes/ImageFunctions.php
@@ -1,32 +1,32 @@
 <?php
 
 function GetImageLink($ImageFile, $Item, $Width, $Height, $AltText, $Class = "") {
-    if (extension_loaded('gd') 
-		and function_exists('gd_info') 
-		and file_exists($ImageFile) 
-		and isset($Item) 
+    if (extension_loaded('gd')
+		and function_exists('gd_info')
+		and file_exists($ImageFile)
+		and isset($Item)
 		and !empty($Item)) {
         if ($_SESSION['ShowStockidOnImages'] == '0'){
-			$ImageLink = '<img src="GetStockImage.php?automake=1' . 
+			$ImageLink = '<img class="StockImage" src="GetStockImage.php?automake=1' .
                                                     '&textcolor=FFFFFF' .
                                                     '&bgcolor=CCCCCC' .
-                                                    '&width=' . $Width . 
+                                                    '&width=' . $Width .
                                                     '&height=' . $Height .
                                                     '&StockID=' . urlencode($Item) .
 													'" alt="' . htmlspecialchars($AltText, ENT_QUOTES) . '" />';
         } else {
-            $ImageLink = '<img src="GetStockImage.php?automake=1' . 
+            $ImageLink = '<img class="StockImage" src="GetStockImage.php?automake=1' .
                                                     '&textcolor=FFFFFF' .
                                                     '&bgcolor=CCCCCC' .
                                                     '&text='. $Item .
-                                                    '&width=' . $Width . 
+                                                    '&width=' . $Width .
                                                     '&height=' . $Height .
                                                     '&StockID=' . urlencode($Item) .
 													'" alt="' . htmlspecialchars($AltText, ENT_QUOTES) . '" />';
 		}
     } else if (file_exists($ImageFile)) {
-        $ImageLink = '<img class="StockImage" src="' . $ImageFile . 
-                                            '" width="' . $Width . 
+        $ImageLink = '<img class="StockImage" src="' . $ImageFile .
+                                            '" width="' . $Width .
                                             '" height="' . $Height . '" />';
     } else {
         $ImageLink = _('No Image');
