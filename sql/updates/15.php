785740edb6314995abbc51fe8dc8e6e117251112
Tim Schofield <tim.schofield1960@googlemail.com>
2025-03-01 21:48:57 +0000

    Include menu item for LoggedInUsers.php as per - https://github.com/timschofield/webERP/issues/363 Change the menu item caption for the GL reports by tag item
    Files changed in commit:
    sql/updates/15.php

---
+++ sql/updates/15.php
@@ -0,0 +1,12 @@
+<?php
+
+RemoveMenuItem('GL', 'Reports', 'Tag Reports', '/GLTagProfit_Loss.php');
+NewMenuItem('GL', 'Reports', _('Income and Expenditure by Tag'), '/GLTagProfit_Loss.php', 17);
+
+NewMenuItem('system', 'Maintenance', _('Logged in users'), '/LoggedInUsers.php', 8);
+
+if ($_SESSION['Updates']['Errors'] == 0) {
+	UpdateDBNo(basename(__FILE__, '.php'), _('Change Menu Items'));
+}
+
+?>
