57ba7158a0ddcaa2556453ff7264276196b71304
Tim Schofield <tim.schofield1960@googlemail.com>
2025-04-25 11:53:11 +0100

    Bring the installer up to date with latest database changes Remove chartdetails structure Remove chartdetails data from demo data Remove redundant menuitems from both demo and live installs Create triggers as part of installation Remove field 'posted' from gltrans table Correct transaction not balancing in gltrans demo data
    Files changed in commit:
    install/demo.sql
    install/pages/page_6.php
    install/sql/menuitems.sql
    install/tables/chartdetails.sql
    install/tables/gltrans.sql

--- install/pages/page_6.php
+++ install/pages/page_6.php
@@ -486,6 +486,8 @@ function UploadData($Demo, $AdminPassword, $AdminUser, $Email, $Language, $CoA, 
 	}
 }
 
+CreateGLTriggers();
+
 UploadData($_SESSION['Installer']['Demo'],
 			$_SESSION['Installer']['AdminPassword'],
 			$_SESSION['Installer']['AdminUser'],
@@ -555,6 +557,47 @@ function PopulateSQLDataBySQL($File) {
 
 	} //end of for loop around the lines of the sql script
 
+}
+
+function CreateGLTriggers() {
+
+$SQL = "CREATE TRIGGER gltrans_after_insert AFTER INSERT ON gltrans FOR EACH ROW
+		BEGIN
+			INSERT INTO gltotals (account, period, amount)
+			VALUES (NEW.account, NEW.periodno, NEW.amount)
+			ON DUPLICATE KEY UPDATE amount = amount + NEW.amount;
+		END";
+$Result = DB_query($SQL);
+
+$SQL = "CREATE TRIGGER `gltrans_after_update` AFTER UPDATE ON `gltrans` FOR EACH ROW
+		BEGIN
+			IF NEW.account <> OLD.account OR NEW.periodno <> OLD.periodno THEN
+				-- Handle account or period changes.
+				-- Deduct the old amount from the old account/period.
+				UPDATE gltotals
+				SET amount = amount - OLD.amount
+				WHERE account = OLD.account AND period = OLD.periodno;
+
+				-- Add the new amount to the new account/period.
+				INSERT INTO gltotals (account, period, amount)
+				VALUES (NEW.account, NEW.periodno, NEW.amount)
+				ON DUPLICATE KEY UPDATE amount = amount + NEW.amount;
+			ELSE
+				-- Just update the amount if account and period are the same.
+				UPDATE gltotals
+				SET amount = amount - OLD.amount + NEW.amount
+				WHERE account = NEW.account AND period = NEW.periodno;
+			END IF;
+		END";
+$Result = DB_query($SQL);
+
+$SQL = "CREATE TRIGGER `gltrans_after_delete` AFTER DELETE ON `gltrans` FOR EACH ROW
+		BEGIN
+			UPDATE gltotals
+			SET amount = amount - OLD.amount
+			WHERE account = OLD.account AND period = OLD.periodno;
+		END";
+$Result = DB_query($SQL);
 }
 
 ?>
