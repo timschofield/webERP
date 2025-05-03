0a82eb4cce44b0e36e7b8304e9c8186d9cb0c6c6
PakRicard <pakricard@gmail.com>
2025-05-03 10:41:57 +0800

    Disable limit of 30 items changing price at a given time.

--- KLUpdateStandardCostCountry.php
+++ KLUpdateStandardCostCountry.php
@@ -9,10 +9,10 @@ include('includes/KLBoards.php');
 include ('includes/KLGeneralFunctions.php');
 include ('includes/KLUIGeneralFunctions.php');
 
-	WrongStandardCost("Indonesia"  , "", STANDARD_COST_FACTOR_INDONESIA, 0.04, "UPDATEALL", $RootPath);
-	WrongStandardCost("Thailand"   , "", STANDARD_COST_FACTOR_FOREIGN, 0.04, "UPDATEALL", $RootPath);
-	WrongStandardCost("China"      , "", STANDARD_COST_FACTOR_FOREIGN, 0.04, "UPDATEALL", $RootPath);
-	WrongStandardCost("Hong Kong"  , "", STANDARD_COST_FACTOR_FOREIGN, 0.04, "UPDATEALL", $RootPath);
+	WrongStandardCost("Indonesia"  , "", STANDARD_COST_FACTOR_INDONESIA, 0.02, "UPDATEALL", $RootPath);
+	WrongStandardCost("Thailand"   , "", STANDARD_COST_FACTOR_FOREIGN, 0.02, "UPDATEALL", $RootPath);
+	WrongStandardCost("China"      , "", STANDARD_COST_FACTOR_FOREIGN, 0.02, "UPDATEALL", $RootPath);
+	WrongStandardCost("Hong Kong"  , "", STANDARD_COST_FACTOR_FOREIGN, 0.02, "UPDATEALL", $RootPath);
 //	WrongStandardCost("Catalonia"  , "", STANDARD_COST_FACTOR_FOREIGN, 0.10, "UPDATEALL", $RootPath);
 //	WrongStandardCost("Philippines", "", STANDARD_COST_FACTOR_FOREIGN, 0.04, "UPDATEALL", $RootPath);
 //	WrongStandardCost("India"      , "", STANDARD_COST_FACTOR_FOREIGN, 0.04, "UPDATEALL", $RootPath);
