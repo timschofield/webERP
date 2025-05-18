244603cfdf43a6bebb1f516638f7f62318e14c6d
Tim Schofield <tim.schofield1960@googlemail.com>
2025-05-18 10:54:35 +0100

    The reference field in the debtortrans table is only 20 characters long, while the customerref field in salesorders table is 50 chracters long. Change the debtortrans table to 50 chracters for consistency As reported here : https://github.com/timschofield/webERP/issues/420
    Files changed in commit:
    install/tables/debtortrans.sql
    sql/updates/21.php

---
+++ sql/updates/21.php
@@ -0,0 +1,7 @@
+<?php
+
+ChangeColumnType('reference', 'debtortrans', 'varchar(50)', ' NOT NULL ', '');
+
+UpdateDBNo(basename(__FILE__, '.php'), _('Change size of reference field to 50 characters for consistency'));
+
+?>
