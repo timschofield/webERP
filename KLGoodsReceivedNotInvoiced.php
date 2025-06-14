23b43d2adf4d139ba2d18890fa5d72a541e6aca7
PakRicard <pakricard@gmail.com>
2025-06-14 09:28:24 +0800

    Clean code

--- KLGoodsReceivedNotInvoiced.php
+++ KLGoodsReceivedNotInvoiced.php
@@ -1,9 +1,9 @@
 <?php
 
-include ('includes/session.php');
+include('includes/session.php');
 $Title = _('Goods Received But Not Invoiced Yet');
-include ('includes/header.php');
-include ('includes/KLGeneralFunctions.php');
+include('includes/header.php');
+include('includes/KLGeneralFunctions.php');
 include('includes/KLUIGeneralFunctions.php');
 
 $SQL = "SELECT grns.supplierid,
@@ -14,32 +14,32 @@ $SQL = "SELECT grns.supplierid,
 				grns.quantityinv,
 				purchorderdetails.unitprice,
 				suppliers.currcode,
-				(stockmaster.actualcost) AS standardcost,
+				stockmaster.actualcost AS standardcost,
 				currencies.rate,
 				currencies.decimalplaces
 		FROM grns 
 		INNER JOIN purchorderdetails
-			ON grns.podetailitem=purchorderdetails.podetailitem
+			ON grns.podetailitem = purchorderdetails.podetailitem
 		INNER JOIN suppliers
 			ON grns.supplierid = suppliers.supplierid
 		INNER JOIN stockmaster
 			ON grns.itemcode = stockmaster.stockid
 		INNER JOIN currencies
 			ON suppliers.currcode = currencies.currabrev
-		WHERE grns.qtyrecd - grns.quantityinv > 0
+		WHERE grns.qtyrecd > grns.quantityinv
 		ORDER BY grns.supplierid,
 			purchorderdetails.orderno,
 			grns.itemcode";
 $Result = DB_query($SQL);
 
-if (DB_num_rows($Result) != 0){
+if (DB_num_rows($Result) != 0) {
 	$TableTitleText = _('Goods Received but not invoiced Yet');
 	ShowTableTitle($TableTitleText);
 
 	echo '<div class="page_help_text">'
-	. _('Shows the list of Goods Received Not Yet Invoiced, both in supplier currency and home currency.'). '<br />'
-	. _('Total in home currency calculated at Purchasing Price converted at current exchange rate, not Standard cost!'). '<br />'
-	. '</div>';
+		. _('Shows the list of Goods Received Not Yet Invoiced, both in supplier currency and home currency.') . '<br />'
+		. _('Total in home currency calculated at Purchasing Price converted at current exchange rate, not Standard cost!') . '<br />'
+		. '</div>';
 
 	echo '<div>';
 	echo '<table class="selection">';
@@ -48,69 +48,68 @@ if (DB_num_rows($Result) != 0){
 							<th class="SortedColumn">' . _('Supplier') . '</th>
 							<th class="SortedColumn">' . _('PO#') . '</th>
 							<th class="SortedColumn">' . _('Item Code') . '</th>
-							<th class="SortedColumn">' . _('Date') . '</th>
+							<th>' . _('Date') . '</th>
 							<th>' . _('Qty Received') . '</th>
 							<th>' . _('Qty Invoiced') . '</th>
 							<th>' . _('Qty Pending') . '</th>
 							<th>' . _('Unit Price') . '</th>
-							<th>' .'' . '</th>
+							<th>' . '' . '</th>
 							<th>' . _('Line Total') . '</th>
 							<th>' . '' . '</th>
 							<th>' . _('IDR Total') . '</th>
 							<th>' . '' . '</th>
 							<th>' . _('Std Cost') . '</th>
-							<th>' . _('Total') . '</th>
 							<th>' . _('Std Cost Total') . '</th>
 							<th>' . '' . '</th>
 						</tr>
-					</thead><tbody>';
+					</thead>
+					<tbody>';
 	echo $TableHeader;
 	$TotalHomeCurrency = 0;
 	$TotalAtStandardCost = 0;
 	while ($MyRow = DB_fetch_array($Result)) {
 		$QtyPending = $MyRow['qtyrecd'] - $MyRow['quantityinv'];
-		$TotalHomeCurrency = $TotalHomeCurrency + ($QtyPending * $MyRow['unitprice'] / $MyRow['rate']);
+		$Rate = $MyRow['rate'] == 0 ? 1 : $MyRow['rate'];
+		$TotalHomeCurrency = $TotalHomeCurrency + ($QtyPending * $MyRow['unitprice'] / $Rate);
 		$TotalAtStandardCost = $TotalAtStandardCost + ($QtyPending * $MyRow['standardcost']);
 		echo '<tr class="striped_row">
 				<td>' . $MyRow['supplierid'] . '</td>
 				<td class="number">' . $MyRow['orderno'] . '</td>
 				<td>' . $MyRow['itemcode'] . '</td>
-				<td>' . ConvertSQLDate($MyRow['deliverydate']) . '</td>
+				<td class="date">' . ConvertSQLDate($MyRow['deliverydate']) . '</td>
 				<td class="number">' . $MyRow['qtyrecd'] . '</td>
 				<td class="number">' . $MyRow['quantityinv'] . '</td>
 				<td class="number">' . $QtyPending . '</td>
-				<td class="number">' . locale_number_format($MyRow['unitprice'],$MyRow['decimalplaces']) . '</td>
+				<td class="number">' . locale_number_format($MyRow['unitprice'], $MyRow['decimalplaces']) . '</td>
 				<td>' . $MyRow['currcode'] . '</td>
-				<td class="number">' . locale_number_format(($QtyPending * $MyRow['unitprice']),$MyRow['decimalplaces']) . '</td>
+				<td class="number">' . locale_number_format(($QtyPending * $MyRow['unitprice']), $MyRow['decimalplaces']) . '</td>
 				<td>' . $MyRow['currcode'] . '</td>
-				<td class="number">' . locale_number_format(($QtyPending * $MyRow['unitprice'] / $MyRow['rate']),$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+				<td class="number">' . locale_number_format(($QtyPending * $MyRow['unitprice'] / $Rate), $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
 				<td>' . $_SESSION['CompanyRecord']['currencydefault'] . '</td>
-				<td class="number">' . locale_number_format($MyRow['standardcost'],$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
-				<td class="number">' . locale_number_format(($QtyPending * $MyRow['standardcost']),$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
-				<td class="number">' . locale_number_format(($QtyPending * $MyRow['standardcost']),$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+				<td class="number">' . locale_number_format($MyRow['standardcost'], $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+				<td class="number">' . locale_number_format(($QtyPending * $MyRow['standardcost']), $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
 				<td>' . $_SESSION['CompanyRecord']['currencydefault'] . '</td>
-				</tr>';
+			</tr>';
 	}
 
 	echo '</tbody>
-			</tfooter>';
-	
+			<tfooter>';
+
 	echo '<tr class="striped_row">
 			<td colspan="10">' . '' . '</td>
 			<td>' . _('Total') . ':' . '</td>
-			<td class="number">' . locale_number_format($TotalHomeCurrency,$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+			<td class="number">' . locale_number_format($TotalHomeCurrency, $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
 			<td>' . $_SESSION['CompanyRecord']['currencydefault'] . '</td>
 			<td>' . '' . '</td>
-			<td>' . _('Std Cost') . ':' . '</td>
-			<td class="number">' . locale_number_format($TotalAtStandardCost,$_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+			<td class="number">' . locale_number_format($TotalAtStandardCost, $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
 			<td>' . $_SESSION['CompanyRecord']['currencydefault'] . '</td>
-			</tr>';
-	
+		</tr>';
+
 	echo '</tfooter>
 			</table>
 			</div>';
 }
 
-include ('includes/footer.php');
+include('includes/footer.php');
 
 ?>
