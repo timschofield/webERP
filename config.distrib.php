70f9d3f0c3fd90f928ddc87d72b62e7cf6ccba31
gggeek <giunta.gaetano@gmail.com>
2025-06-26 11:19:58 +0000

    improve wording in config.php, mentioning reverse proxies. better separate user vars from code which should not be changed

--- config.distrib.php
+++ config.distrib.php
@@ -16,17 +16,17 @@ $DefaultLanguage = 'en_GB.utf8';
 $DefaultTheme = 'xenos';
 
 // Whether to display the demo login and password or not on the login screen
-$AllowDemoMode = False;
+$AllowDemoMode = false;
 
 // Whether to display company logo in drop down list at login
-$ShowLogoAtLogin = True;
+$ShowLogoAtLogin = true;
 
-// email address of the system administrator
+// Email address of the system administrator
 $SysAdminEmail = 'admin@mydomain.com';
 
 // The timezone of the business - this allows the possibility of having
 // the web-server in a different timezone than the business but in general
-// should always be set correctly (and PHP used to complain if not set)
+// should always be set correctly
 //date_default_timezone_set('Europe/London');
 //date_default_timezone_set('America/Los_Angeles');
 //date_default_timezone_set('Asia/Shanghai');
@@ -67,11 +67,12 @@ $AllowCompanySelectionBox = 'ShowSelectionBox';
 // default in the login screen, otherwise the user must know the name of the company to log into).
 $DefaultDatabase = 'weberpdemo';
 
-// The maximum time that a login session can be idle before automatic logout
-// time is in seconds  3600 seconds in an hour
+// The maximum time that a login session can be idle before automatic logout.
+// Time is in seconds. 3600 seconds in an hour
 $SessionLifeTime = 3600;
 
-// The maximum time that a script can execute for before the web-server should terminate it
+// The maximum time that a script can execute for before the web-server should terminate it.
+// Time is in seconds
 $MaximumExecutionTime = 120;
 
 // Setting to 12 or 24 determines the format of the clock display at the end of all screens
@@ -79,9 +80,10 @@ $DefaultClock = 12;
 //$DefaultClock = 24;
 
 // Path to session files on server
+//
 // This can be useful for multi-host web servers where pages are serviced using load balancing servers, otherwise
-// the session can be lost when the load balancer picks a different server. To prevent this, this option tells the
-// server explicitly where to find the session file.
+// the session can be lost when the load balancer picks a different server. To prevent that, this option tells the
+// server explicitly where to find the session files (which should be on a shared-access filesystem in that scenario).
 //
 // This option is also useful when a server has multiple webERP installs (e.g. to serve multiple webERP sites using
 // customized or different releases of code) and those can be accessed by the same client browser (e.g. by a user of
@@ -90,23 +92,38 @@ $DefaultClock = 12;
 //
 // If there is only one installation of webERP on the web-server - which can be used with many company databases (and
 // there is no load balancing difficulties to circumvent), this can be left commented.
+//
+// Note that it is also possible to configure PHP to store sessions in database-like services, such as redis or memcached.
+// In that case, this setting won't have any effect.
+//
 //$SessionSavePath = '/tmp';
 
 // Set a specific session_name to avoid potential default session_name conflicts
 // with other apps using the same host.
 $SessionName = 'PHPSESSIDwebERPteam';
 
-// END OF USER CONFIGURABLE VARIABLES
-
 // $RootPath is used in most scripts to build links to assets and other scripts.
 // It is the path to the webERP directory as "seen" from the user's browser (excluding the hostname and port).
-// NOTE: In some windows installation the following line doesn't work and the administrator must set this to the
-// path of the installation manually
-// E.g. 1 if the files are under the webserver root directory, then rootpath ='';
-// E.g. 2 if the files are under webERP, then webERP is the rootpath (note no additional slashes are necessary).
-// NOTE: this value is outputted as-is as part of the html of the produced web pages, this is why we have to escape it
-// here with the htmlspecialchars call
+// NOTE: In some windows installations, or when the webs server sits behind a reverse proxy the default, calculated value
+// doesn't work and the administrator must set this to a fixed path manually.
+// NOTE: no trailing slashes are necessary.
+// E.g. if the files are under the webserver root directory
+//$RootPath = '';
+// E.g. 2 if the files are in the webERP folder inside the webserver root directory, then webERP is the rootpath
+//$RootPath = 'webERP';
 $RootPath = htmlspecialchars(dirname($_SERVER['PHP_SELF']), ENT_QUOTES, 'UTF-8');
+
+// Report all errors except E_NOTICE
+// (default value in php.ini for most installations, but it is forced here to be sure)
+// Note: turning on NOTICES will destroy things
+error_reporting(E_ALL && ~E_NOTICE);
+// For Development, use:
+//error_reporting(-1);
+
+// Installed companies
+// - see companies/dbname/Companies.php
+
+// END OF USER CONFIGURABLE VARIABLES
 
 if (isset($DirectoryLevelsDeep)) {
 	for ($i = 0; $i < $DirectoryLevelsDeep; $i++) {
@@ -114,16 +131,6 @@ if (isset($DirectoryLevelsDeep)) {
 	}
 }
 
-if ($RootPath == "/" or $RootPath == "\\") {
-	$RootPath = "";
+if ($RootPath === '/' or $RootPath === '\\') {
+	$RootPath = '';
 }
-
-// Report all errors except E_NOTICE
-// (default value in php.ini for most installations, but it is forced here to be sure)
-// Note: turning on NOTICES will destroy things
-error_reporting(E_ALL && ~E_NOTICE && E_WARNING);
-/* For Development Use */
-//error_reporting(-1);
-
-// Installed companies
-// - see companies/dbname/Companies.php
