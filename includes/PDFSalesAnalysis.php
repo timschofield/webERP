ae98aed6a7e413f1aa0a5e3ebdbf639fef7aba48
gggeek <giunta.gaetano@gmail.com>
2025-07-02 11:42:20 +0000

    remove leftover usage of $SERVER_NAME when building redirect target

--- includes/PDFSalesAnalysis.php
+++ includes/PDFSalesAnalysis.php
@@ -543,7 +543,7 @@ if (isset($_GET['ProduceCVSFile'])){
 	// gg: what was this line supposed to do ?
 	//echo '//' . getenv('SERVER_NAME') . $RootPath . '/' . $_SESSION['reports_dir'] .  '/SalesAnalysis.csv';
 	/// @todo this meta tag should be moved into the HTML HEAD, and thus be outputted within `header.php`
-	echo "<meta http-equiv='Refresh' content='0; url=//" . getenv('SERVER_NAME') . $RootPath . '/' . $_SESSION['reports_dir'] .  "/SalesAnalysis.csv'>";
+	echo "<meta http-equiv='Refresh' content='0; url=" . $RootPath . '/' . $_SESSION['reports_dir'] .  "/SalesAnalysis.csv'>";
 
 	 echo '<p>' . _('You should automatically be forwarded to the CSV Sales Analysis file when it is ready') . '. ' . _('If this does not happen') . ' <a href="' . $RootPath . '/' . $_SESSION['reports_dir'] . '/SalesAnalysis.csv">' . _('click here') . '</a> ' . _('to continue')  . '<br />';
 	 include('includes/footer.php');
