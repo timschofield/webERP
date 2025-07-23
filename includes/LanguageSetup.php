21b0e6d8d02063961bf5a03e78e96de251b0df9f
gggeek <giunta.gaetano@gmail.com>
2025-07-23 17:26:09 +0000

    WIP move to usage of polyfill-gettext over php-gettext

--- includes/LanguageSetup.php
+++ includes/LanguageSetup.php
@@ -3,13 +3,13 @@
 /* Set internal character encoding to UTF-8 */
 mb_internal_encoding('UTF-8');
 
-/* This file is included in session.php or PDFStarter.php or a report script that does not use PDFStarter.php
-to check for the existence of gettext function and setup the necessary environment to allow for automatic translation
+/* This file is included in session.php or PDFStarter.php or a report script that does not use PDFStarter.php to check
+   for the existence of gettext function and setting up the necessary environment to allow for automatic translation.
 
-Set language - defined in config.php or user variable when logging in (session.php)
-NB this language must also exist in the locale on the web-server
-normally the lower case two character language code underscore uppercase
-2 character country code does the trick  except for en !!*/
+   Set language - defined in config.php or user variable when logging in (session.php)
+   NB: this language must also exist in the locale on the web-server
+   Normally the lower case two character language code underscore uppercase 2 character country code does the trick,
+  except for en !! */
 
 /*
  * Improve language check to avoid potential LFI issue.
@@ -23,33 +23,36 @@ if (isset($_POST['Language']) && checkLanguageChoice($_POST['Language'])) {
 
 $Language = $_SESSION['Language'];
 
-//Check users' locale format via their language
-//Then pass this information to the js for number validation purpose
+// Check users' locale format via their language
+// Then pass this information to the js for number validation purpose
 
 $Collect = array(
 	'US'=>array('en_US.utf8','en_GB.utf8','ja_JP.utf8','hi_IN.utf8','mr_IN.utf8','sw_KE.utf8','tr_TR.utf8','vi_VN.utf8','zh_CN.utf8','zh_HK.utf8','zh_TW.utf8'),
 	'IN'=>array('en_IN.utf8','hi_IN.utf8','mr_IN.utf8'),
 	'EE'=>array('ar_EG.utf8','cz_CZ.utf8','fr_CA.utf8','fr_FR.utf8','hr_HR.utf8','pl_PL.utf8','ru_RU.utf8','sq_AL.utf8','sv_SE.utf8'),
 	'FR'=>array('ar_EG.utf8','cz_CZ.utf8','fr_CA.utf8','fr_FR.utf8','hr_HR.utf8','pl_PL.utf8','ru_RU.utf8','sq_AL.utf8','sv_SE.utf8'),
-	'GM'=>array('de_DE.utf8','el_GR.utf8','es_ES.utf8','fa_IR.utf8','id_ID.utf8','it_IT.utf8','ro_RO.utf8','lv_LV.utf8','nl_NL.utf8','pt_BR.utf8','pt_PT.utf8'));
+	'GM'=>array('de_DE.utf8','el_GR.utf8','es_ES.utf8','fa_IR.utf8','id_ID.utf8','it_IT.utf8','ro_RO.utf8','lv_LV.utf8','nl_NL.utf8','pt_BR.utf8','pt_PT.utf8')
+);
 
-foreach($Collect as $Key=>$Value) {
-	if(in_array($Language,$Value)) {
+foreach($Collect as $Key => $Value) {
+	if (in_array($Language, $Value)) {
 		$Lang = $Key;
 		$_SESSION['Lang'] = $Lang;
 	}
 }
 
-/*Since LanguagesArray requires the function _() to translate the language names - we must provide a substitute if it doesn't exist aready before we include includes/LanguagesArray.php
- * */
-if (!function_exists('gettext')) {
 /*
-	PHPGettext integration by Braian Gomez
-	http://www.vairux.com/
+ Since this file is always loaded after session.php has been loaded, and session.php sets up the composer autoloading,
+ when we get here the 'gettext' function is always defined. It can be either the function from the php native
+ extension, or the one from the polyfill-gettext php package.
+ Was:
+   Since LanguagesArray requires the function _() to translate the language names - we must provide a substitute if
+   it doesn't exist already before we include includes/LanguagesArray.php
+   PHPGettext integration by Braian Gomez - http://www.vairux.com/
 */
-	require_once($PathPrefix . 'includes/php-gettext/streams.php');
-	require_once($PathPrefix . 'includes/php-gettext/gettext.php');
-	if(isset($_SESSION['Language'])){
+/*
+if (!function_exists('gettext')) {
+	if (isset($_SESSION['Language'])) {
 		$Locale = $_SESSION['Language'];
 	} else {
 		$Locale = $DefaultLanguage;
@@ -58,7 +61,7 @@ if (!function_exists('gettext')) {
 	if (isset($PathPrefix)) {
 		$LangFile = $PathPrefix . 'locale/' . $Locale . '/LC_MESSAGES/messages.mo';
 	} else {
-		$LangFile = 'locale/' . $Locale . '/LC_MESSAGES/messages.mo';
+		$LangFile = __DIR__ . '/../locale/' . $Locale . '/LC_MESSAGES/messages.mo';
 	}
 
 	if (file_exists($LangFile)){
@@ -78,26 +81,30 @@ if (!function_exists('gettext')) {
 	}
 	include($PathPrefix . 'includes/LanguagesArray.php');
 } else {
+*/
 	include($PathPrefix . 'includes/LanguagesArray.php');
 
-	$LocaleSet = setlocale (LC_ALL, $_SESSION['Language'],$LanguagesArray[$_SESSION['Language']]['WindowsLocale']);
-	$LocaleSet = setlocale (LC_NUMERIC, 'C','en_GB.utf8','en_GB','en_US','english-us');
+	$LocaleSetAll = setlocale(LC_ALL, $_SESSION['Language'], $LanguagesArray[$_SESSION['Language']]['WindowsLocale']);
+	$LocaleSetNumeric = setlocale(LC_NUMERIC, 'C', 'en_GB.utf8', 'en_GB', 'en_US', 'english-us');
 
-	if (defined('LC_MESSAGES')){ //it's a unix/linux server
-		$LocaleSet = setlocale (LC_MESSAGES, $_SESSION['Language']);
+	// NB: this is always true now, because of polyfill-gettext. Was: "it's a unix/linux server"
+	if (defined('LC_MESSAGES')) {
+		$LocaleSetMessages = setlocale(LC_MESSAGES, $_SESSION['Language'], $LanguagesArray[$_SESSION['Language']]['WindowsLocale']);
 	}
-	//Turkish seems to be a special case
-	if ($_SESSION['Language']=='tr_TR.utf8') {
-		$Locale = setlocale(LC_CTYPE, 'C');
+	// Turkish seems to be a special case
+	if ($_SESSION['Language'] == 'tr_TR.utf8') {
+		$LocaleSetCtype = setlocale(LC_CTYPE, 'C');
 	}
 
 	// possibly even if locale fails the language will still switch by using Language instead of locale variable
+	/// @todo make this work better with polyfill gettext: besides the putenv call, if setlocale failed call PGettext\T::setlocale
 	putenv('LANG=' . $_SESSION['Language']);
 	putenv('LANGUAGE=' . $_SESSION['Language']);
+
 	bindtextdomain ('messages', $PathPrefix . 'locale');
 	textdomain ('messages');
 	bind_textdomain_codeset('messages', 'UTF-8');
-}
+/*}*/
 
 $DecimalPoint = $LanguagesArray[$_SESSION['Language']]['DecimalPoint'];
 $ThousandsSeparator = $LanguagesArray[$_SESSION['Language']]['ThousandsSeparator'];
