4d0eb843b1aa56606a36ee869b4aeeaf1780a9d8
gggeek <giunta.gaetano@gmail.com>
2025-07-25 17:07:26 +0000

    fix link to weberpo site in html creatot meta tag

--- AgedSuppliers.php
+++ AgedSuppliers.php
@@ -108,7 +108,7 @@ if (isset($_POST['PrintPDF']) or isset($_POST['View'])
 	}
 
 	$HTML .= '<meta name="author" content="WebERP " . $Version">
-					<meta name="Creator" content="webERP http://www.weberp.org">
+					<meta name="Creator" content="webERP https://www.weberp.org">
 				</head>
 				<body>
 				<div class="centre" id="ReportHeader">
