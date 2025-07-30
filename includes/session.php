a8d8211a004892ceed641e81371315ce0c8994b0
Tim Schofield <tim.schofield1960@googlemail.com>
2025-07-30 22:13:35 +0100

    Reverse incorrect call to exit() committed in https://github.com/timschofield/webERP/commit/a71e0140a79764adb33497b80459d5be26716094
    Files changed in commit:
    includes/session.php

--- includes/session.php
+++ includes/session.php
@@ -121,7 +121,6 @@ if (basename($_SERVER['SCRIPT_NAME']) == 'Logout.php') {
 	}
 
 	header('Location: ' . htmlspecialchars_decode($RootPath) . '/index.php'); //go back to the main index/login
-	exit();
 
 } elseif (isset($AllowAnyone)) { /* only do security checks if AllowAnyone is not true */
 	if (!isset($_SESSION['DatabaseName'])){
