fc11a751f7d146ae00dfac49df2ac3eb1d238510
gggeek <giunta.gaetano@gmail.com>
2025-08-06 11:10:46 +0000

    remove check for gettext extension from installer; move all installer images in one dir; installer code comments

--- install/pages/page_2.php
+++ install/pages/page_2.php
@@ -7,26 +7,19 @@ if (!empty($_SESSION['Installer']['TimeZone'])) {
 	date_default_timezone_set($_SESSION['Installer']['TimeZone']);
 }
 
-//Check if the cookie is allowed
+//Check if cookies are allowed
 if (false) {
 	$InputError = 1;
-	echo '<div class="error">' . _('Please set Cookies allowed in your web brower, otherwise webERP cannot run properly') . '</div>';
+	echo '<div class="error">' . _('Please set Cookies allowed in your web browser, otherwise webERP cannot run properly') . '</div>';
 } else {
 	echo '<div class="success">' . _('Cookies are properly enabled in your browser') . '</div>';
 }
 
-//Check if it's in safe model, safe mode has been deprecated at 5.3.0 and removed at 5.4
-//Please refer to here for more details http://hk2.php.net/manual/en/features.safe-mode.php
-if (ini_get('safe_mode')) {
-	$SafeModeWarning = _('You php is running in safe mode, it will force a maximum script execution time of 30 seconds') . ' ' . _('This can sometimes mean that the installation cannot be completed in time.') . ' ' . _('It is better to turn this function off');
-	$InputWarn = 1;
-	$WarnMsg.= '<p>' . _($_POST['SafeModeWarning']) . '</p>';
-}
-
-//It's time to check the php version. The version should be run greater than 5.1
-if (version_compare(PHP_VERSION, '5.1.0') < 0) {
+//It's time to check the php version. The version should be 8.1 or above
+/// @todo grab the version to check for from parsing composer.json
+if (version_compare(PHP_VERSION, '8.1.0') < 0) {
 	$InputError = 1;
-	echo '<div class="error">' . _('You PHP version should be greater than 5.1') . '</div>';
+	echo '<div class="error">' . _('You PHP version should be equal or greater than') . ' 8.1</div>';
 } else {
 	echo '<div class="success">' . _('Your PHP version is suitable for webERP') . '</div>';
 }
@@ -53,7 +46,9 @@ if (!is_writable($CompaniesDir)) {
 //get the list of installed extensions
 $Extensions = get_loaded_extensions();
 
-//First check the gd module
+/// @todo grab the list of required and recommended extensions from parsing composer.json
+
+//First check the gd extension
 if (!in_array('gd', $Extensions)) {
 	$InputError = 1;
 	echo '<div class="error">' . _('The GD extension should be installed in your PHP configuration') . '</div>';
@@ -61,23 +56,24 @@ if (!in_array('gd', $Extensions)) {
 	echo '<div class="success">' . _('The GD extension is correctly installed') . '</div>';
 }
 
-//Check the gettext module, it's a selectable
-if (!in_array('gettext', $Extensions)) {
+//The gettext extension is not checked anymore, as it's optional.
+/// @todo bring back the check for gettext, but make it a warning. Same for the other optional extensions
+/*if (!in_array('gettext', $Extensions)) {
 	$InputError = 1;
-	echo '<div class="error">' . _('The gettext extension is not availble in your PHP') . '</div>';
+	echo '<div class="error">' . _('The gettext extension is not available in your PHP') . '</div>';
 } else {
 	echo '<div class="success">' . _('The gettext extension is correctly installed') . '</div>';
-}
+}*/
 
-//Check the mbstring module, it must be exist
+//Check the mbstring extension, it must exist
 if (!in_array('mbstring', $Extensions)) {
 	$InputError = 1;
-	echo '<div class="error">' . _('The mbstring extension is not availble in your PHP') . '</div>';
+	echo '<div class="error">' . _('The mbstring extension is not available in your PHP') . '</div>';
 } else {
 	echo '<div class="success">' . _('The mbstring extension is correctly installed') . '</div>';
 }
 
-//Check the libxml module
+//Check the extension module
 if (!in_array('libxml', $Extensions)) {
 	$InputError = 1;
 	echo '<div class="error">' . _('The libxml extension is not available in your PHP') . '</div>';
@@ -85,10 +81,10 @@ if (!in_array('libxml', $Extensions)) {
 	echo '<div class="success">' . _('The libxml extension is correctly installed') . '</div>';
 }
 
-//Check that the DBMS driver is installed
+//Check that the extension used for DBMS connections is installed
 if (!in_array('mysqli', $Extensions)) {
 	$InputError = 1;
-	echo '<div class="error">' . _('You do not have the correct database extension installed for PHP') . '</div>';
+	echo '<div class="error">' . _('You do not have the correct database extension installed for PHP (mysqli)') . '</div>';
 } else {
-	echo '<div class="success">' . _('The database extension is installed') . '</div>';
+	echo '<div class="success">' . _('The database extension is correctly installed') . '</div>';
 }
