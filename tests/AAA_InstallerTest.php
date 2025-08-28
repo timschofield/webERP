3815ac53855b3aa503e91716b909ada292e910dd
gggeek <giunta.gaetano@gmail.com>
2025-08-28 16:04:49 +0000

    add phpunit env vars for db connection

--- tests/AAA_InstallerTest.php
+++ tests/AAA_InstallerTest.php
@@ -1,10 +1,23 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
 
 class AAA_InstallerTest extends TestCase {
 
+	/**
+	 * Runs the installer to create the database schema and fill it with data
+	 * @return void
+	 */
 	public function testInstallation() {
-
+		// sample code...
+		/*
+		$browser = new HttpBrowser(HttpClient::create());
+		$BaseUrl = $_ENV['TARGET_PROTOCOL'] . '://'. $_ENV['TARGET_HOSTNAME'] .
+			($_ENV['TARGET_PORT'] != '' ? (':' . ltrim($_ENV['TARGET_PORT'], ':')) : '') .
+			rtrim($_ENV['TARGET_BASE_URL'], '/');
+		$crawler = $browser->request('GET', $BaseUrl .  '/');
+		*/
 	}
 }
