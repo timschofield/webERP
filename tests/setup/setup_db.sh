7bee615aa08067a0b4c0ee5917c79d66902c2434
gggeek <giunta.gaetano@gmail.com>
2025-08-28 18:08:26 +0000

    simplify gh yaml

---
+++ tests/setup/setup_db.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+# @todo add support for 1st argument, being db type and 2nd eing version
+
+BASE_DIR="$(dirname -- "$(dirname -- "$(dirname -- "$(realpath "${BASH_SOURCE[0]}")")")")";
+
+cd "$BASE_DIR";
+
+# Debugging
+#echo '### /etc/mysql/'
+#sudo ls -la /etc/mysql/
+#echo
+#echo '### /etc/mysql/debian-start'
+#sudo cat /etc/mysql/debian-start
+#echo
+#echo '### /etc/mysql/debian.cnf'
+#sudo cat /etc/mysql/debian.cnf
+#echo
+#echo '### /etc/mysql/my.cnf'
+#sudo cat /etc/mysql/my.cnf
+#echo
+#echo '### /etc/mysql/my.cnf.fallback'
+#sudo cat /etc/mysql/my.cnf.fallback
+#echo
+#echo '### /etc/mysql/mysql.cnf'
+#sudo cat /etc/mysql/mysql.cnf
+#echo
+#sudo ls -la /etc/mysql/conf.d/
+#echo
+#sudo ls -la /etc/mysql/mysql.conf.d/
+
+# Start the service
+sudo systemctl start mysql.service
