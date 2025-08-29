2c529d05cdb6a88b68f2e0cb4c82b122603be6a9
gggeek <giunta.gaetano@gmail.com>
2025-08-29 16:42:44 +0000

    rename UserLogin.php to LoginFunctions.php, which is more in line with other files which only define functions

--- includes/session.php
+++ includes/session.php
@@ -155,7 +155,7 @@ if (basename($_SERVER['SCRIPT_NAME']) == 'Logout.php') {
 	include($PathPrefix . 'includes/GetConfig.php');
 
 } else {
-	include($PathPrefix . 'includes/UserLogin.php'); /* Login checking and setup. Includes GetConfig.php on successful logins */
+	include($PathPrefix . 'includes/LoginFunctions.pho'); /* Login checking and setup. Includes GetConfig.php on successful logins */
 
 	if (isset($_POST['UserNameEntryField']) and isset($_POST['Password'])) {
 		$rc = userLogin($_POST['UserNameEntryField'], $_POST['Password'], $SysAdminEmail);
