95f6e897f175eb6a4353e92b23e9ae1eccffced4
PakRicard <pakricard@gmail.com>
2025-08-30 10:44:46 +0800

    Implement changes of commit 59c4f8b22e94c08166e0c009976aef6dd60298c4  into KL files

--- includes/KLSessionCronJob.php
+++ includes/KLSessionCronJob.php
@@ -139,9 +139,10 @@ if (basename($_SERVER['SCRIPT_NAME']) == 'Logout.php') {
 	header('Location: ' . htmlspecialchars_decode($RootPath) . '/index.php'); //go back to the main index/login
 
 } elseif (isset($AllowCronJobToBeRun)) { /* only do security checks if AllowCronJobToBeRun is not true */
-	if (!isset($_SESSION['DatabaseName'])) {
-
+	if (!isset($_SESSION['AllowedPageSecurityTokens'])) {
 		$_SESSION['AllowedPageSecurityTokens'] = array();
+	}
+	if (!isset($_SESSION['DatabaseName'])) {
 		$_SESSION['DatabaseName'] = $DefaultDatabase;
 	}
 	$_SESSION['UserID'] = "CronJobKL";
