97c6235f5aa0e541ad42ee4c83f4ccca576feac2
Tim Schofield <tim.schofield1960@googlemail.com>
2025-08-31 19:32:04 +0100

    Change the prodspecgroups specification - File can only have the CREATE TABLE code in it
    Files changed in commit:
    install/sql/tables/prodspecgroups.sql

--- install/sql/tables/prodspecgroups.sql
+++ install/sql/tables/prodspecgroups.sql
@@ -1,18 +1,12 @@
 CREATE TABLE `prodspecgroups` (
-  `groupid` smallint(6) NOT NULL,
+  `groupid` smallint NOT NULL AUTO_INCREMENT,
   `groupname` char(50) DEFAULT NULL,
-  `groupbyNo` int(11) NOT NULL DEFAULT 1,
+  `groupbyNo` int NOT NULL DEFAULT '1',
   `headertitle` varchar(100) DEFAULT NULL,
   `trailertext` varchar(240) DEFAULT NULL,
   `labels` varchar(240) NOT NULL,
-  `numcols` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_general_ci;
-
-ALTER TABLE `prodspecgroups`
-  ADD PRIMARY KEY (`groupid`),
-  ADD UNIQUE KEY `groupname` (`groupname`),
-  ADD KEY `groupbyNo` (`groupbyNo`);
-
-ALTER TABLE `prodspecgroups`
-  MODIFY `groupid` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
+  `numcols` tinyint(1) NOT NULL,
+  PRIMARY KEY (`groupid`),
+  UNIQUE KEY `groupname` (`groupname`),
+  KEY `groupbyNo` (`groupbyNo`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3
