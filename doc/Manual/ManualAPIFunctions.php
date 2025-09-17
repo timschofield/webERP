459721d99965149b72cc1fc661232add09ccdb80
gggeek <giunta.gaetano@gmail.com>
2025-09-17 17:55:17 +0000

    even even more oops

--- doc/Manual/ManualAPIFunctions.php
+++ doc/Manual/ManualAPIFunctions.php
@@ -22,7 +22,7 @@ echo '</head>';
 echo '<body>';
 
 // avoid sending an xml-rpc request to self, interrogate directly the server
-$dispatchMap = include($PathPrefix . 'api/api_xml-rpc_definition.php');
+$dispatchMap = include($PathPrefix . 'api/includes/api_xml-rpc_definition.php');
 $server = new PhpXmlRpc\Server($dispatchMap, false);
 $response = PhpXmlRpc\Server::_xmlrpcs_listMethods($server);
 $answer = $response->value();
