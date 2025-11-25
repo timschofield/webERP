da63be4759b30a4178d41daae68cb18a08c3acf4
PakRicard <pakricard@gmail.com>
2025-11-26 07:44:10 +0800

    Update KL Code for development. 056

--- KLConfig.php
+++ KLConfig.php
@@ -6,7 +6,7 @@
 *
 *******************************************************************************/
 /* KL RICARD Configuration file for specific KL code */
-$KLCodeVersion = "055";
+$KLCodeVersion = "056";
 
 // let's setup all the variables depending on the environment
 if (URLWithoutScriptNameContains(".LOCAL")) {
