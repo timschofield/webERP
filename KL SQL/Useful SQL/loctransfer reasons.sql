2224c5fb0ae882880294a1439865adf2e1d7421a
PakRicard <pakricard@gmail.com>
2025-11-28 10:07:50 +0800

    Reason code was too long

--- KL SQL/Useful SQL/loctransfer reasons.sql
+++ KL SQL/Useful SQL/loctransfer reasons.sql
@@ -13,7 +13,7 @@ ALTER TABLE `klkpidescriptions` CHANGE `kpicode` `kpicode` VARCHAR(40) CHARACTER
 ALTER TABLE `klkpi` CHANGE `kpicode` `kpicode` VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 
 INSERT INTO `klkpidescriptions` (`kpicode`, `kpidescription`) VALUES ('TRANS-DISPATCH_OVERSTOCK-30-PCS', 'Manual Dispatch Overstock');
-INSERT INTO `klkpidescriptions` (`kpicode`, `kpidescription`) VALUES ('TRANS-DISPATCH_NEEDED_BY_RL-30-PCS', 'Manual Dispatch Needed by Reorder Level');
+INSERT INTO `klkpidescriptions` (`kpicode`, `kpidescription`) VALUES ('TRANS-DISPATCH_NEEDED-30-PCS', 'Manual Dispatch Needed by Reorder Level');
 INSERT INTO `klkpidescriptions` (`kpicode`, `kpidescription`) VALUES ('TRANS-SMART_NEEDED_BY_RL-30-PCS', 'Daily Cron Job From Kantor Needed in Shop by Reorder Level');
 INSERT INTO `klkpidescriptions` (`kpicode`, `kpidescription`) VALUES ('TRANS-SMART_RETURN_OVER-30-PCS', 'Daily Cron Job Return Overstock from Shop to Kantor');
 INSERT INTO `klkpidescriptions` (`kpicode`, `kpidescription`) VALUES ('TRANS-SERV__LAINLAIN-30-PCS', 'Servis Lain Lain');
