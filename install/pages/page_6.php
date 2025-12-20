17f63a71bd67de363a15fc4725a5f95796ea1da7
Tim Schofield <tim.schofield1960@googlemail.com>
2025-12-20 07:30:54 +0000

    Fixes https://github.com/timschofield/webERP/issues/797
    Files changed in commit:
    install/pages/page_6.php

--- install/pages/page_6.php
+++ install/pages/page_6.php
@@ -87,7 +87,7 @@ if (!CreateGLTriggers($Path_To_Root, $DBType)) {
 if (!UploadData($_SESSION['Installer']['Demo'],
 			$_SESSION['Installer']['AdminPassword'],
 			$_SESSION['Installer']['AdminUser'],
-			$_SESSION['Installer']['Email'],
+			$_SESSION['Installer']['AdminEmail'],
 			$_SESSION['Installer']['Language'],
 			$_SESSION['Installer']['CoA'],
 			$_SESSION['CompanyRecord']['coyname'],
@@ -95,7 +95,7 @@ if (!UploadData($_SESSION['Installer']['Demo'],
 			$Database,
 			$DBType)) {
 	return;
-};
+}
 
 /// @todo wouldn't it make more sense to have this be run as part of CreateCompanyFolder, or just after it?
 if (!CreateCompaniesFile($Path_To_Root, $Database, $_SESSION['CompanyRecord']['coyname'])) {
