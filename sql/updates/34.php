39d405d6de10e65010b34f7927c8b7f6e4e4ce61
PakRicard <pakricard@gmail.com>
2026-01-26 14:47:17 +0800

    SQl update not needed

--- sql/updates/34.php
+++ sql/updates/34.php
@@ -1,6 +1,7 @@
 <?php
 
-NewConfigValue('ItemDescriptionLanguages', ', ');
+// Not needed in KL environment
+// NewConfigValue('ItemDescriptionLanguages', ', ');
 
 if ($_SESSION['Updates']['Errors'] == 0) {
 	UpdateDBNo(basename(__FILE__, '.php'), __('Create config value for multi languages'));
