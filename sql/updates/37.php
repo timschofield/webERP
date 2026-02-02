6ca4768f85da21d1ba4f55b03f15089844db4df9
PakRicard <pakricard@gmail.com>
2026-02-02 15:58:48 +0800

    Changes not needed in KL DB

--- sql/updates/37.php
+++ sql/updates/37.php
@@ -1,6 +1,6 @@
 <?php
 
-CreateTable('auditscript',
+/*CreateTable('auditscript',
 "CREATE TABLE `auditscripts` (
   `executiondate` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
   `secondsrunning` decimal(10,5) NOT NULL DEFAULT 0.00000,
@@ -12,5 +12,5 @@ CreateTable('auditscript',
 )");
 
 NewMenuItem('system', 'Transactions', __('Scripts Audit Trail'), '/AuditScripts.php', 15);
-
+*/
 UpdateDBNo(basename(__FILE__, '.php'), __('Create auditscripts table to log automated script executions'));
