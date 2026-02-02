7078ae1237e4d99250b56fe4c055849873cfb020
PakRicard <pakricard@gmail.com>
2026-02-02 15:59:23 +0800

    Revert "Fixes https://github.com/timschofield/webERP/issues/824"
    This reverts commit dcd6f6e5cdd68d13737b1c1a54c4155fbf3083ed.

--- sql/updates/36.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-ChangeColumnSize('location', 'mrpparameters', 'VARCHAR(5)', ' NULL ', '', '5');
-
-UpdateDBNo(basename(__FILE__, '.php'), __('Update the size of the location field in mrpparameters to 5 chars'));
