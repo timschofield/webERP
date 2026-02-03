f620e338fe6805e904ece1e83e75b78410b03200
PakRicard <pakricard@gmail.com>
2026-02-03 09:30:29 +0800

    Add missing fields and related tables to customer and branch code changes

--- Z_ChangeCustomerCode.php
+++ Z_ChangeCustomerCode.php
@@ -41,139 +41,176 @@ if (isset($_POST['ProcessCustomerChange'])){
 	DB_Txn_Begin();
 
 	prnMsg(__('Inserting the new debtors master record'),'info');
-	$SQL = "INSERT INTO debtorsmaster (`debtorno`,
-									`name`,
-									`address1`,
-									`address2`,
-									`address3`,
-									`address4`,
-									`currcode`,
-									`salestype`,
-									`clientsince`,
-									`holdreason`,
-									`paymentterms`,
-									`discount`,
-									`discountcode`,
-									`pymtdiscount`,
-									`lastpaid`,
-									`lastpaiddate`,
-									`creditlimit`,
-									`invaddrbranch`,
-									`ediinvoices`,
-									`ediorders`,
-									`edireference`,
-									`editransport`,
-									`ediaddress`,
-									`ediserveruser`,
-									`ediserverpwd`,
-									`typeid`)
-					SELECT '" . $_POST['NewDebtorNo'] . "',
-									`name`,
-									`address1`,
-									`address2`,
-									`address3`,
-									`address4`,
-									`currcode`,
-									`salestype`,
-									`clientsince`,
-									`holdreason`,
-									`paymentterms`,
-									`discount`,
-									`discountcode`,
-									`pymtdiscount`,
-									`lastpaid`,
-									`lastpaiddate`,
-									`creditlimit`,
-									`invaddrbranch`,
-									`ediinvoices`,
-									`ediorders`,
-									`edireference`,
-									`editransport`,
-									`ediaddress`,
-									`ediserveruser`,
-									`ediserverpwd`,
-									`typeid`
-					FROM debtorsmaster
-					WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$SQL = "INSERT INTO debtorsmaster
+				(debtorno,
+				name,
+				address1,
+				address2,
+				address3,
+				address4,
+				address5,
+				address6,
+				currcode,
+				salestype,
+				clientsince,
+				holdreason,
+				paymentterms,
+				discount,
+				pymtdiscount,
+				lastpaid,
+				lastpaiddate,
+				creditlimit,
+				invaddrbranch,
+				discountcode,
+				ediinvoices,
+				ediorders,
+				edireference,
+				editransport,
+				ediaddress,
+				ediserveruser,
+				ediserverpwd,
+				taxref,
+				customerpoline,
+				typeid,
+				language_id)
+			SELECT
+				'" . $_POST['NewDebtorNo'] . "',
+				name,
+				address1,
+				address2,
+				address3,
+				address4,
+				address5,
+				address6,
+				currcode,
+				salestype,
+				clientsince,
+				holdreason,
+				paymentterms,
+				discount,
+				pymtdiscount,
+				lastpaid,
+				lastpaiddate,
+				creditlimit,
+				invaddrbranch,
+				discountcode,
+				ediinvoices,
+				ediorders,
+				edireference,
+				editransport,
+				ediaddress,
+				ediserveruser,
+				ediserverpwd,
+				taxref,
+				customerpoline,
+				typeid,
+				language_id
+			FROM debtorsmaster
+			WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
 	$ErrMsg = __('The SQL to insert the new debtors master record failed') . ', ' . __('the SQL statement was');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
 	prnMsg(__('Inserting new customer branch records'),'info');
-	$SQL = "INSERT INTO custbranch ( `branchcode`,
-									`debtorno`,
-									`brname`,
-									`braddress1`,
-									`braddress2`,
-									`braddress3`,
-									`braddress4`,
-									`braddress5`,
-									`braddress6`,
-									`estdeliverydays`,
-									`area`,
-									`salesman`,
-									`fwddate`,
-									`phoneno`,
-									`faxno`,
-									`contactname`,
-									`email`,
-									`defaultlocation`,
-									`taxgroupid`,
-									`disabletrans`,
-									`brpostaddr1`,
-									`brpostaddr2`,
-									`brpostaddr3`,
-									`brpostaddr4`,
-									`brpostaddr5`,
-									`brpostaddr6`,
-									`defaultshipvia`,
-									`specialinstructions`,
-									`custbranchcode`)
-							SELECT branchcode,
-								'" . $_POST['NewDebtorNo'] . "',
-									`brname`,
-									`braddress1`,
-									`braddress2`,
-									`braddress3`,
-									`braddress4`,
-									`braddress5`,
-									`braddress6`,
-									`estdeliverydays`,
-									`area`,
-									`salesman`,
-									`fwddate`,
-									`phoneno`,
-									`faxno`,
-									`contactname`,
-									`email`,
-									`defaultlocation`,
-									`taxgroupid`,
-									`disabletrans`,
-									`brpostaddr1`,
-									`brpostaddr2`,
-									`brpostaddr3`,
-									`brpostaddr4`,
-									`brpostaddr5`,
-									`brpostaddr6`,
-									`defaultshipvia`,
-									'',
-									`custbranchcode`
-								FROM custbranch
-								WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$SQL = "INSERT INTO custbranch
+				(branchcode,
+				debtorno,
+				brname,
+				braddress1,
+				braddress2,
+				braddress3,
+				braddress4,
+				braddress5,
+				braddress6,
+				lat,
+				lng,
+				estdeliverydays,
+				area,
+				salesman,
+				fwddate,
+				phoneno,
+				faxno,
+				contactname,
+				email,
+				defaultlocation,
+				taxgroupid,
+				defaultshipvia,
+				deliverblind,
+				disabletrans,
+				brpostaddr1,
+				brpostaddr2,
+				brpostaddr3,
+				brpostaddr4,
+				brpostaddr5,
+				brpostaddr6,
+				specialinstructions,
+				custbranchcode)
+			SELECT
+				branchcode,
+				'" . $_POST['NewDebtorNo'] . "',
+				brname,
+				braddress1,
+				braddress2,
+				braddress3,
+				braddress4,
+				braddress5,
+				braddress6,
+				lat,
+				lng,
+				estdeliverydays,
+				area,
+				salesman,
+				fwddate,
+				phoneno,
+				faxno,
+				contactname,
+				email,
+				defaultlocation,
+				taxgroupid,
+				defaultshipvia,
+				deliverblind,
+				disabletrans,
+				brpostaddr1,
+				brpostaddr2,
+				brpostaddr3,
+				brpostaddr4,
+				brpostaddr5,
+				brpostaddr6,
+				specialinstructions,
+				custbranchcode
+			FROM custbranch
+			WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
 
 	$ErrMsg = __('The SQL to insert new customer branch records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
+
+	prnMsg(__('Changing the customer code in contract header records'),'info');
+	$SQL = "UPDATE contracts SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update contract header records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+	
+	prnMsg(__('Changing customer contacts records'),'info');
+	$SQL = "UPDATE custcontacts SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update customer contacts records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+
+	prnMsg(__('Changing customer items records'),'info');
+	$SQL = "UPDATE custitem SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update customer items records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+
+	prnMsg(__('Changing customer notes records'),'info');
+	$SQL = "UPDATE custnotes SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update customer notes records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+
 	prnMsg(__('Changing debtor transaction records'),'info');
-
-	$SQL = "UPDATE debtortrans SET debtorNo='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
-
+	$SQL = "UPDATE debtortrans SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
 	$ErrMsg = __('The SQL to update debtor transaction records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
-	prnMsg(__('Changing sales analysis records'),'info');
-
-	$SQL = "UPDATE salesanalysis SET cust='" . $_POST['NewDebtorNo'] . "' WHERE cust='" . $_POST['OldDebtorNo'] . "'";
-
-	$ErrMsg = __('The SQL to update Sales Analysis records failed');
+	prnMsg(__('Changing location records'),'info');
+	$SQL = "UPDATE locations SET cashsalecustomer='" . $_POST['NewDebtorNo'] . "' WHERE cashsalecustomer='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update location records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
 	prnMsg(__('Changing order delivery differences records'),'info');
@@ -181,17 +218,29 @@ if (isset($_POST['ProcessCustomerChange'])){
 	$ErrMsg = __('The SQL to update order delivery differences records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
-
 	prnMsg(__('Changing pricing records'),'info');
 	$SQL = "UPDATE prices SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
-
 	$ErrMsg = __('The SQL to update the pricing records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+
+	prnMsg(__('Changing recurring sales orders records'),'info');
+	$SQL = "UPDATE recurringsalesorders SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update debtor transaction records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+
+	prnMsg(__('Changing sales analysis records'),'info');
+	$SQL = "UPDATE salesanalysis SET cust='" . $_POST['NewDebtorNo'] . "' WHERE cust='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update Sales Analysis records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
 	prnMsg(__('Changing sales orders records'),'info');
 	$SQL = "UPDATE salesorders SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
-
 	$ErrMsg = __('The SQL to update the sales order header records failed');
+	$Result = DB_query($SQL, $ErrMsg, '', true);
+
+	prnMsg(__('Changing sell through support records'),'info');
+	$SQL = "UPDATE sellthroughsupport SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
+	$ErrMsg = __('The SQL to update thesell through support records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
 	prnMsg( __('Changing stock movement records'),'info');
@@ -201,14 +250,7 @@ if (isset($_POST['ProcessCustomerChange'])){
 
 	prnMsg(__('Changing user default customer records'),'info');
 	$SQL = "UPDATE www_users SET customerid='" . $_POST['NewDebtorNo'] . "' WHERE customerid='" . $_POST['OldDebtorNo'] . "'";
-
 	$ErrMsg = __('The SQL to update the user records failed');
-	$Result = DB_query($SQL, $ErrMsg, '', true);
-
-	prnMsg(__('Changing the customer code in contract header records'),'info');
-	$SQL = "UPDATE contracts SET debtorno='" . $_POST['NewDebtorNo'] . "' WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
-
-	$ErrMsg = __('The SQL to update contract header records failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
 	DB_IgnoreForeignKeys();
@@ -218,16 +260,13 @@ if (isset($_POST['ProcessCustomerChange'])){
 	$ErrMsg = __('The SQL to delete the old CustBranch records for the old debtor record failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
-
 	prnMsg(__('Deleting the customer code from the DebtorsMaster table'),'info');
 	$SQL = "DELETE FROM debtorsmaster WHERE debtorno='" . $_POST['OldDebtorNo'] . "'";
-
 	$ErrMsg = __('The SQL to delete the old debtor record failed');
 	$Result = DB_query($SQL, $ErrMsg, '', true);
 
-
-	DB_Txn_Commit();
 	DB_ReinstateForeignKeys();
+	DB_Txn_Commit();
 
 }
 
@@ -237,11 +276,11 @@ echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />'
 echo '<fieldset>
 		<legend>', __('Customer to Change'), '</legend>
 	<field>
-		<label>' . __('Existing Debtor Code') . ':</label>
+		<label>' . __('Existing Customer Code') . ':</label>
 		<input type="text" name="OldDebtorNo" size="20" maxlength="20" />
 	</field>
 	<field>
-		<label> ' . __('New Debtor Code') . ':</label>
+		<label> ' . __('New Customer Code') . ':</label>
 		<input type="text" name="NewDebtorNo" size="20" maxlength="20" />
 	</field>
 	</fieldset>
