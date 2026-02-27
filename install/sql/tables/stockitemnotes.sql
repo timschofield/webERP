73c1963cbc57df6dce23dec40cdc5d9b0c938561
Tim Schofield <tim.schofield1960@googlemail.com>
2026-02-27 07:31:13 +0000

    Schema for new stockitemnotes table
    Files changed in commit:
    install/sql/tables/stockitemnotes.sql

---
+++ install/sql/tables/stockitemnotes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `stockitemnotes` (
+  `noteid` int NOT NULL AUTO_INCREMENT,
+  `stockid` varchar(64) NOT NULL DEFAULT '0',
+  `note` text NOT NULL,
+  `date` date NOT NULL DEFAULT '1000-01-01',
+  PRIMARY KEY (`noteid`),
+  KEY `stockitemnotes_ibfk_1` (`stockid`),
+  CONSTRAINT `stockitemnotes_ibfk_1` FOREIGN KEY (`stockid`) REFERENCES `stockmaster` (`stockid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
