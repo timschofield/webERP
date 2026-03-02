05fabdb8c933b194f23d708e90725c8ed048546f
PakRicard <pakricard@gmail.com>
2026-03-03 07:47:45 +0800

    Update KL Code for development. 067

--- KLConfig.php
+++ KLConfig.php
@@ -6,7 +6,7 @@
 *
 *******************************************************************************/
 /* KL RICARD Configuration file for specific KL code */
-$KLCodeVersion = "066";
+$KLCodeVersion = "067";
 
 // KL RICARD look for the secret values of sensitive variables and credentials $PTADU...
 include('KLConfig/KLCredentials.php');
