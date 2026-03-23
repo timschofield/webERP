c636efe4b3e6d240964d28260fc258dd0d10af0e
PakRicard <pakricard@gmail.com>
2026-03-23 10:06:12 +0800

    By default, do not show Forecasting module

--- sql/updates/49.php
+++ sql/updates/49.php
@@ -6,7 +6,7 @@ $Result = DB_query($SQL);
 while ($MyRow = DB_fetch_array($Result)) {
 	if (mb_strlen($MyRow['modulesallowed']) < 24) {
 		$StringLength = mb_strlen($MyRow['modulesallowed']);
-		$NewModulesAllowed = mb_substr($MyRow['modulesallowed'], 0, 12) . '1,' . mb_substr($MyRow['modulesallowed'], 12, ($StringLength - 11));
+		$NewModulesAllowed = mb_substr($MyRow['modulesallowed'], 0, 12) . '0,' . mb_substr($MyRow['modulesallowed'], 12, ($StringLength - 11));
 		UpdateField('www_users', 'modulesallowed', $NewModulesAllowed, 'userid="' . $MyRow['userid'] . '"');
 	}
 }
