2374a01b362b18e9abb82cef1901593369759d24
PakRicard <pakricard@gmail.com>
2026-06-17 21:09:43 +0800

    Standard security Token assigned

--- sql/updates/65.php
+++ sql/updates/65.php
@@ -1,7 +1,7 @@
 <?php
 
 NewMenuItem('hr', 'Transactions', __('My Appraisals as Reviewer'), '/HRMyAppraisalsAsReviewer.php', 10);
-NewScript('HRMyAppraisalsAsReviewer.php', 35000);
+NewScript('HRMyAppraisalsAsReviewer.php', 15);
 
 if ($_SESSION['Updates']['Errors'] == 0) {
 	UpdateDBNo(basename(__FILE__, '.php'), __('HR My Appraisals as Reviewer new script'));
