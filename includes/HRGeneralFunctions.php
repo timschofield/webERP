34d64b52432704a39ff202882353172786683d6c
PakRicard <pakricard@gmail.com>
2026-06-18 11:51:16 +0800

    Fixes Warning Parameter $XXX has no type information available.

--- includes/HRGeneralFunctions.php
+++ includes/HRGeneralFunctions.php
@@ -6,10 +6,10 @@
  * Returns a list separated by commas of the DepartmentId below the given one, including the $DepartmentId.
  * Recursively fetches all child departments.
  *
- * @param string $DepartmentId The ID of the starting department
+ * @param int|string $DepartmentId The ID of the starting department
  * @return string Comma-separated list of Department IDs
  */
-function GetDepartmentListBelow($DepartmentId) {
+function GetDepartmentListBelow(int|string $DepartmentId): string {
 	$DepartmentList = array($DepartmentId);
 	$SQL = "SELECT departmentid FROM departments WHERE parentdepartmentid='" . DB_escape_string($DepartmentId) . "'";
 	$Result = DB_query($SQL);
