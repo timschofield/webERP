216b798d3ab151ca71bd4b4e33798db2d15c798a
PakRicard <pakricard@gmail.com>
2026-07-04 11:06:45 +0800

    Add maintenance of teporary column usenewboxes. This commit should be reverted after the transition process

--- Locations.php
+++ Locations.php
@@ -123,7 +123,8 @@ if (isset($_POST['submit'])) {
 									alldisc50items = '" . $_POST['AllDisc50Items'] . "',
 									alldisc80items = '" . $_POST['AllDisc80Items'] . "',
 									allowinvoicing = '" . $_POST['AllowInvoicing'] . "',
-									departmentid = '" . $_POST['DepartmentID'] . "'
+									departmentid = '" . $_POST['DepartmentID'] . "',
+									usenewboxes = '" . $_POST['UseNewBoxes'] . "'
 						WHERE loccode = '" . $SelectedLocation . "'";
 
 		$ErrMsg = __('An error occurred updating the') . ' ' . $SelectedLocation . ' ' . __('location record because');
@@ -181,6 +182,7 @@ if (isset($_POST['submit'])) {
 		unset($_POST['AllDisc80Items']);
 		unset($_POST['AllowInvoicing']);
 		unset($_POST['DepartmentID']);
+		unset($_POST['UseNewBoxes']);
 
 
 	} elseif ($InputError !=1) {
@@ -242,7 +244,8 @@ if (isset($_POST['submit'])) {
 										alldisc50items,
 										alldisc80items,
 										allowinvoicing,
-										departmentid)
+										departmentid,
+										usenewboxes)
 						VALUES ('" . $_POST['LocCode'] . "',
 								'" . $_POST['LocationName'] . "',
 								'" . $_POST['DelAdd1'] ."',
@@ -285,7 +288,8 @@ if (isset($_POST['submit'])) {
 								'" . $_POST['AllDisc50Items'] . "',
 								'" . $_POST['AllDisc80Items'] . "',
 								'" . $_POST['AllowInvoicing'] . "',
-								'" . $_POST['DepartmentID'] . "')";
+								'" . $_POST['DepartmentID'] . "',
+								'" . $_POST['UseNewBoxes'] . "')";
 
 		$ErrMsg = __('An error occurred inserting the new location record because');
 		$Result = DB_query($SQL, $ErrMsg);
@@ -375,6 +379,7 @@ if (isset($_POST['submit'])) {
 		unset($_POST['AllDisc80Items']);
 		unset($_POST['AllowInvoicing']);
 		unset($_POST['DepartmentID']);
+		unset($_POST['UseNewBoxes']);
 	}
 
 	/* Go through the tax authorities for all Locations deleting or adding TaxAuthRates records as necessary */
@@ -560,7 +565,8 @@ or deletion of the records*/
 				packagingfrom,
 				rlfactorforpackaging,
 				rldaysforpackaging,
-				managed
+				managed,
+				usenewboxes
 			FROM locations
 			ORDER BY locationname";
 	$Result = DB_query($SQL);
@@ -586,6 +592,7 @@ or deletion of the records*/
 					<th class="SortedColumn">', __('Pack From'), '</th>
 					<th class="SortedColumn">', __('Pack Factor'), '</th>
 					<th class="SortedColumn">', __('Pack Days'), '</th>
+					<th class="SortedColumn">', __('New Boxes?'), '</th>
 					<th class="noPrint" colspan="2">&nbsp;</th>
 				</tr>
 			</thead>
@@ -620,6 +627,11 @@ or deletion of the records*/
 	} else {
 		$AvailableForOnline = __('No');
 	}
+	if ($MyRow['usenewboxes'] == 1) {
+		$UseNewBoxesText = __('Yes');
+	} else {
+		$UseNewBoxesText = __('No');
+	}
 
 	printf('<tr class="striped_row">
 			<td>%s</td>
@@ -637,6 +649,7 @@ or deletion of the records*/
 			<td>%s</td>
 			<td class="number">%s</td>
 			<td class="number">%s</td>
+			<td>%s</td>
 			<td class="noPrint"><a href="%sSelectedLocation=%s">' . __('Edit') . '</a></td>
 			<td class="noPrint"><a href="%sSelectedLocation=%s&amp;delete=1" onclick="return confirm(\'' . __('Are you sure you wish to delete this inventory location?') . '\');">' . __('Delete') . '</a></td>
 			</tr>',
@@ -655,6 +668,7 @@ or deletion of the records*/
 			$MyRow['packagingfrom'],
 			$MyRow['rlfactorforpackaging'],
 			$MyRow['rldaysforpackaging'],
+			$UseNewBoxesText,
 			htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '?', $MyRow['loccode'],
 			htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '?', $MyRow['loccode']);
 		}
@@ -719,7 +733,8 @@ if (!isset($_GET['delete'])) {
 					alldisc50items,
 					alldisc80items,
 					allowinvoicing,
-					departmentid
+					departmentid,
+					usenewboxes
 				FROM locations
 				WHERE loccode='" . $SelectedLocation . "'";
 
@@ -769,6 +784,7 @@ if (!isset($_GET['delete'])) {
 		$_POST['AllDisc80Items'] = $MyRow['alldisc80items'];
 		$_POST['AllowInvoicing'] = $MyRow['allowinvoicing'];
 		$_POST['DepartmentID'] = $MyRow['departmentid'];
+		$_POST['UseNewBoxes'] = $MyRow['usenewboxes'];
 
 		echo '<input type="hidden" name="SelectedLocation" value="' . $SelectedLocation . '" />';
 		echo '<input type="hidden" name="LocCode" value="' . $_POST['LocCode'] . '" />';
@@ -895,6 +911,9 @@ if (!isset($_GET['delete'])) {
 	}
 	if (!isset($_POST['DepartmentID'])) {
 		$_POST['DepartmentID'] = '';
+	}
+	if (!isset($_POST['UseNewBoxes'])) {
+		$_POST['UseNewBoxes'] = 0;
 	}
 	// KL RICARD END
 	echo '<field>
@@ -1175,6 +1194,21 @@ if (!isset($_GET['delete'])) {
 			<label for="RLFactorForPackaging">' . __('RL Factor for Packaging Transfers') . ':</label>
 			<input type="text" name="RLFactorForPackaging" class="number" title="' . __('Factor to Multiply Reorder Level for Packaging Transfers') . '" value="' . $_POST['RLFactorForPackaging'] . '" size="4" maxlength="4" />
 		</field>';
+
+	echo FieldToSelectFromTwoOptions(
+		'0',
+		__('No using new boxes'),
+		'1',
+		__('Yes using new boxes'),
+		'UseNewBoxes',
+		$_POST['UseNewBoxes'],
+		__('Using new boxes?'),
+		'',
+		'',
+		'',
+		true,
+		false
+	);
 
 	echo '</fieldset>';
 
