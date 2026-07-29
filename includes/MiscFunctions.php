7e347fbe15a3d1e1915479b7ae9df44eb0d0826c
Dale Scott <dale@dalescott.net>
2026-07-29 17:16:21 -0600

    Simplify name

--- includes/MiscFunctions.php
+++ includes/MiscFunctions.php
@@ -381,11 +381,11 @@ function wikiLink($WikiType, $WikiPageID) {
 	}
 
 	if ($_SESSION['WikiApp'] == __('WackoWiki')) {
-		echo '<a target="_blank" href="' . $WikiPath . $WikiType . $WikiPageID . '">' . __('Wiki ' . $WikiType . ' Knowledge Base') . ' </a>  <br />';
+		echo '<a target="_blank" href="' . $WikiPath . $WikiType . $WikiPageID . '">' . __('Wiki ' . $WikiType . ' Documents') . ' </a>  <br />';
 	} elseif ($_SESSION['WikiApp'] == __('MediaWiki')) {
-		echo '<a target="_blank" href="' . $WikiPath . 'index.php?title=' . $WikiType . '/' . $WikiPageID . '">' . __('Wiki ' . $WikiType . ' Knowledge Base') . '</a><br />';
+		echo '<a target="_blank" href="' . $WikiPath . 'index.php?title=' . $WikiType . '/' . $WikiPageID . '">' . __('Wiki ' . $WikiType . ' Documents') . '</a><br />';
 	} elseif ($_SESSION['WikiApp'] == __('DokuWiki')) {
-		echo '<a target="_blank" href="' . $WikiPath . '/doku.php?id=' . $WikiType . ':' . $WikiPageID . '">' . __('Wiki ' . $WikiType . ' Knowledge Base') . '</a><br />';
+		echo '<a target="_blank" href="' . $WikiPath . '/doku.php?id=' . $WikiType . ':' . $WikiPageID . '">' . __('Wiki ' . $WikiType . ' Documents') . '</a><br />';
 	}
 }
 
